628cf1cf2fcdb0a4714c538bad0ea484961a3245
Mukesh <mukesh.gramfactory.com>
2018-10-09 10:37:52 +0530

    changes

--- categories/api/v1/views.py
+++ categories/api/v1/views.py
@@ -8,8 +8,10 @@ from .serializers import CategorySerializer,CategoryDataSerializer
 from categories.models import Category,CategoryData,CategoryPosation
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
+from rest_framework.permissions import (AllowAny,IsAuthenticated)
 
 class GetAllSubCategoryListView(viewsets.ModelViewSet):
+    permission_classes = (AllowAny,)
     queryset = Category.objects.filter(category_parent=None)
     serializer_class = CategorySerializer
 
@@ -20,6 +22,7 @@ class GetAllSubCategoryListView(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 class GetCategoryListBySlot(APIView):
+    permission_classes = (AllowAny,)
 
     def get(self,*args,**kwargs):
         slot_name = self.kwargs.get("slot_name")
