c30a2c230050338c4fc2dc63c043cf763633165b
Jagjeet Singh <jagjeet@gramfactory.com>
2018-10-31 01:25:29 +0530

    added user and user-document apis

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
-from accounts.api.v1.views import UserDetail
+from accounts.api.v1.views import UserDetail, UserDocumentView
 urlpatterns = [
     path('user/', UserDetail.as_view(), name='user', ),
+    path('user-document/', UserDocumentView.as_view(), name='user-document', ),
+
 ]
