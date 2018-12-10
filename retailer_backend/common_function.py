4e7f7184aa4b0a0123850cad0e21d0a61ffd49fb
Mukesh <mukesh.gramfactory.com>
2018-12-10 12:46:36 +0530

    changes

--- retailer_backend/common_function.py
+++ retailer_backend/common_function.py
@@ -8,13 +8,15 @@ from rest_framework.response import Response
 def checkShop(shop_id):
     try:
         shop = Shop.objects.get(id=shop_id,status=True)
+        print(shop)
         return True
     except ObjectDoesNotExist:
         return False
 
 def checkShopMapping(shop_id):
     try:
-        parent_mapping = ParentRetailerMapping.objects.get(retailer=shop_id)
+        parent_mapping = ParentRetailerMapping.objects.get(retailer=shop_id,status=True)
+        print(parent_mapping)
         return True
     except ObjectDoesNotExist:
         return False
@@ -25,7 +27,6 @@ def checkNotShopAndMapping(shop_id):
     else:
         return True
 
-
 def getShop(shop_id):
     try:
         shop = Shop.objects.get(id=shop_id,status=True)
@@ -35,11 +36,8 @@ def getShop(shop_id):
 
 def getShopMapping(shop_id):
     try:
-        parent_mapping = ParentRetailerMapping.objects.get(retailer=shop_id)
+        parent_mapping = ParentRetailerMapping.objects.get(retailer=shop_id,status=True)
         return parent_mapping
     except ObjectDoesNotExist:
         return None
-
-def getShopMappingType(shop_id):
-    pass
 
