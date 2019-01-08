7ee1c842e076bba164d286b70a38d4a29f531be7
Mukesh <mukesh.gramfactory.com>
2019-01-08 18:06:04 +0530

    po_date cant be in past BACK-141

--- gram_to_brand/forms.py
+++ gram_to_brand/forms.py
@@ -85,3 +85,8 @@ class POGenerationForm(forms.ModelForm):
                     if not p.product_price == float(row[5]):
                         raise ValidationError("Row["+str(id+1)+"] | "+first_row[5]+":"+row[5]+" | Price does not matched with original product's brand to gram price")
             return self.cleaned_data
+
+        date = self.cleaned_data['po_validity_date']
+        if date < datetime.date.today():
+            raise forms.ValidationError("Po validity date cannot be in the past!")
+        return date
