241ccfaf40daa19d137e899d12d334c61474cae9
Raj Shekhar Singh <raj@gramfactory.com>
2019-01-10 17:23:15 +0530

    changes

--- gram_to_brand/templatetags/app_filters.py
+++ gram_to_brand/templatetags/app_filters.py
@@ -42,7 +42,7 @@ def shipped_qty(value, *args, **kwargs):
 @register.filter(name='inner_case_size')
 def inner_case_size(value, *args, **kwargs):
     result = int(value)*shipped_qty_list[0]
-    shipped_qty_list.clear()
+    
     return result
 
 @register.filter(name='price_to_retailer')
