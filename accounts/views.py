3f6db8e64810d15161765be86b4d30a564919139
Jagjeet Singh <jagjeet@gramfactory.com>
2019-01-15 18:24:09 +0530

    added terms and privacy policy

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def terms_and_conditions(request):
+    return render(request, 'accounts/terms_and_conditions.html')
 
-from allauth.account.signals import user_signed_up, email_confirmed
-@receiver(user_signed_up)
-def user_signed_up_(request, user, **kwargs):
-    print("user signed up")
+def privacy_policy(request):
+    return render(request, 'accounts/privacy_policy.html')
