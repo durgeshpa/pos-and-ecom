28cfaac183bfee647f8b4c43a916d187d23e8e3d
Jagjeet Singh <jagjeet@gramfactory.com>
2019-02-07 12:40:45 +0530

    autopopulate shipmentplanning for r/sp/gf

--- retailer_to_sp/forms.py
+++ retailer_to_sp/forms.py
@@ -1,21 +1,70 @@
-from django import forms
-from retailer_to_sp.models import CustomerCare, ReturnProductMapping
-from products.models import Product
-from django_select2.forms import Select2MultipleWidget,ModelSelect2Widget
 from dal import autocomplete
+from django_select2.forms import Select2MultipleWidget, ModelSelect2Widget
 
-class CustomerCareForm( forms.ModelForm ):
-    complaint_detail = forms.CharField( widget=forms.Textarea(attrs={'placeholder':'Enter your Message','cols': 50, 'rows': 8}) )
+from django import forms
+
+from retailer_to_sp.models import (
+    CustomerCare, ReturnProductMapping, OrderedProduct,
+    OrderedProductMapping
+)
+from products.models import Product
+
+
+class CustomerCareForm(forms.ModelForm):
+    complaint_detail = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Enter your Message',
+            'cols': 50, 'rows': 8})
+    )
+
     class Meta:
-        model =CustomerCare
-        fields='__all__'
+        model = CustomerCare
+        fields = '__all__'
+
 
 class ReturnProductMappingForm(forms.ModelForm):
     returned_product = forms.ModelChoiceField(
         queryset=Product.objects.all(),
-        widget=autocomplete.ModelSelect2(url='return-product-autocomplete',forward=('invoice_no',))
+        widget=autocomplete.ModelSelect2(
+            url='return-product-autocomplete',
+            forward=('invoice_no',)
+        )
      )
 
     class Meta:
-        model= ReturnProductMapping
-        fields= ('returned_product','total_returned_qty', 'reusable_qty', 'damaged_qty')
+        model = ReturnProductMapping
+        fields = (
+            'returned_product', 'total_returned_qty',
+            'reusable_qty', 'damaged_qty'
+        )
+
+
+class OrderedProductForm(forms.ModelForm):
+
+    class Meta:
+        model = OrderedProduct
+        fields = ['order', 'invoice_no', 'vehicle_no']
+
+    class Media:
+        js = (
+            'https://cdnjs.cloudflare.com/ajax/libs/select2/'
+            '4.0.6-rc.0/js/select2.min.js',
+            'admin/js/orderedproduct.js'
+        )
+        css = {
+            'all': (
+                'https://cdnjs.cloudflare.com/ajax/libs/select2/'
+                '4.0.6-rc.0/css/select2.min.css',
+            )
+        }
+
+
+class OrderedProductMappingForm(forms.ModelForm):
+    ordered_qty = forms.CharField(required=False)
+
+    class Meta:
+        model = OrderedProductMapping
+        fields = [
+            'product', 'ordered_qty', 'shipped_qty', 'delivered_qty',
+            'returned_qty', 'damaged_qty'
+        ]
