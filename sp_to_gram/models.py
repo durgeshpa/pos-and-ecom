697c2568b5f187ba7f0b89c08e1fcf8a6cdbc9b0
nripesh <nripesh@gramfactory.com>
2019-02-08 18:32:09 +0530

    shop id in credit note

--- sp_to_gram/models.py
+++ sp_to_gram/models.py
@@ -285,9 +285,9 @@ def create_brand_note_id(sender, instance=None, created=False, **kwargs):
 
 @receiver(post_save, sender=RetailerShipment)
 def create_credit_note(sender, instance=None, created=False, **kwargs):
-    if instance.rt_order_product_order_product_mapping.all().aggregate(Sum('returned_qty')).get('returned_qty__sum') > 0:
+    if instance.rt_order_product_order_product_mapping.last() and instance.rt_order_product_order_product_mapping.all().aggregate(Sum('returned_qty')).get('returned_qty__sum') > 0:
         invoice_prefix = instance.order.seller_shop.invoce_pattern.filter(status=ShopInvoicePattern.ACTIVE).last().pattern
-        last_credit_note = CreditNote.objects.last()
+        last_credit_note = CreditNote.objects.filter(shop=instance.order.seller_shop).last()
         if last_credit_note:
             note_id = int(getcredit_note_id(last_credit_note.credit_note_id, invoice_prefix))
             note_id += 1
@@ -300,6 +300,7 @@ def create_credit_note(sender, instance=None, created=False, **kwargs):
             credit_note = instance.credit_note.last()
         else:
             credit_note = CreditNote.objects.create(
+                shop = instance.order.seller_shop,
                 credit_note_id=brand_credit_note_pattern(note_id, invoice_prefix),
                 shipment = instance,
                 amount = 0,
