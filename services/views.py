a59466481e3156e96ef4043c936792f272d98d21
nripesh <nripesh@gramfactory.com>
2019-02-13 17:55:06 +0530

    image resize

--- services/views.py
+++ services/views.py
@@ -6,22 +6,23 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.http import HttpResponse
-
+from django.conf import settings
 # Create your views here.
 
 class ResizeImage(APIView):
     permission_classes = (AllowAny,)
-    def get(self,request, *args, **kwargs):
-    	img_url = request.GET.get('image_url')
-    	width = int(request.GET.get('width', '200'))
-    	height = request.GET.get('height', None)
-    	img_response = requests.get(img_url, stream=True)
-    	img_response.raw.decode_content = True
-    	image = Image.open(img_response.raw)
+    def get(self,request, image_path, image_name, *args, **kwargs):
+        path = "/".join(args)
+        img_url = "https://{}/{}/{}".format(getattr(settings, 'AWS_S3_CUSTOM_DOMAIN'), image_path,image_name, path)
+        width = int(request.GET.get('width', '600'))
+        height = request.GET.get('height', None)
+        img_response = requests.get(img_url, stream=True)
+        img_response.raw.decode_content = True
+        image = Image.open(img_response.raw)
 
-    	if not height:
-    		height = int(image.height * width/image.width)
-    	image = image.resize((width,height), PIL.Image.LANCZOS)
-    	response = HttpResponse(content_type="image/jpeg")
-    	image.save(response, "JPEG")
-    	return response
+        if not height:
+            height = int(image.height * width/image.width)
+        image = image.resize((width,height), PIL.Image.LANCZOS)
+        response = HttpResponse(content_type="image/jpeg")
+        image.save(response, "JPEG")
+        return response
