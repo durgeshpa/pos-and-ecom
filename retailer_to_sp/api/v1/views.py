8194b036b9647f8617d92ca0e39b7ad705045fbd
nripesh <nripesh@gramfactory.com>
2019-02-15 16:58:12 +0530

    invoice total sum fix

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -822,7 +822,7 @@ class DownloadInvoiceSP(APIView):
 
             sum_qty = sum_qty + int(m.product.product_inner_case_size) * int(m.shipped_qty)
             sum_amount += (int(m.product.product_inner_case_size) * int(m.shipped_qty) * m.get_shop_specific_products_prices_sp().price_to_retailer)
-            inline_sum_amount = (int(m.product.product_inner_case_size) * int(m.shipped_qty) * h.get_shop_specific_products_prices_sp().price_to_retailer)
+            inline_sum_amount = (int(m.product.product_inner_case_size) * int(m.shipped_qty) * m.get_shop_specific_products_prices_sp().price_to_retailer)
             
             for n in m.product.product_pro_tax.all():
 
