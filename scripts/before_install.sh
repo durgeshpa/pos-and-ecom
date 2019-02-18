9277b0153f33d33b74bc0e1206345bd9c985169b
nripesh <nripesh@gramfactory.com>
2019-02-18 16:46:31 +0530

    added before install cleanup fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
-if [ -d /home/ubuntu/sample-app ]; then
+if [ -d /home/ubuntu/project/retailer-backend ]; then
   sudo rm -R /home/ubuntu/project/retailer-backend
   mkdir /home/ubuntu/project/retailer-backend
 fi
