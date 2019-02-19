b96af10ae6b824593b5073c29f5f5687872fd9af
nripesh <nripesh@gramfactory.com>
2019-02-19 17:57:33 +0530

    add env file in deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
+cp /home/ubuntu/project/retailer-backend/.env /tmp/.env
 if [ -d /home/ubuntu/project/retailer-backend ]; then
   sudo rm -R /home/ubuntu/project/retailer-backend
   mkdir /home/ubuntu/project/retailer-backend
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
+cp /tmp/.env /home/ubuntu/project/retailer-backend/.env
 source /home/ubuntu/projects/prod/bin/activate
 cd /home/ubuntu/project/retailer-backend/
 python manage.py makemigrations
