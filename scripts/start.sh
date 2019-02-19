4725bafb21f0bc567057416b11d2c3005536ada7
nripesh <nripesh@gramfactory.com>
2019-02-19 18:42:13 +0530

    bug fix 4

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
+# cp -R /home/ubuntu/projects/tmp/retailer_backend /home/ubuntu/projects/retailer-backend
 cp /tmp/.env /home/ubuntu/project/retailer-backend/.env
-cp -R /home/ubuntu/projects/tmp/retailer-backend /home/ubuntu/projects/
 source /home/ubuntu/projects/prod/bin/activate
 cd /home/ubuntu/project/retailer-backend/
 python manage.py makemigrations
