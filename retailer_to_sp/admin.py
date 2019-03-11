ce15c310ed1eee601ae544f5d5fc016af62bb712
nripesh <nripesh@gramfactory.com>
2019-03-11 16:34:16 +0530

    search fix for orders

--- retailer_to_sp/admin.py
+++ retailer_to_sp/admin.py
@@ -251,8 +251,8 @@ class OrderAdmin(admin.ModelAdmin,ExportCsvMixin):
     search_fields = ('order_no', 'seller_shop__shop_name', 'buyer_shop__shop_name',
                     'order_status')
     list_display = ('order_no', 'seller_shop', 'buyer_shop', 'total_final_amount',
-                    'order_status', 'created_at', 'payment_mode', 'paid_amount', 'total_paid_amount', 'download_pick_list')
-    readonly_fields = ('payment_mode', 'paid_amount', 'total_paid_amount')
+                    'order_status', 'created_at', 'paid_amount', 'total_paid_amount', 'download_pick_list')
+    readonly_fields = ('paid_amount', 'total_paid_amount')
 
     def get_queryset(self, request):
         qs = super(OrderAdmin, self).get_queryset(request)
