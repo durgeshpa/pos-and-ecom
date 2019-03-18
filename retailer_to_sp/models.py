72090e2b2e8598a7334a71af14ff2190e26c62b7
nripesh <nripesh@gramfactory.com>
2019-03-18 17:55:43 +0530

    fix invoice amount

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -421,28 +421,13 @@ class OrderedProduct(models.Model):
             total_amount = []
             seller_shop = self.order.seller_shop
             ordered_products = self.rt_order_product_order_product_mapping.all()
-            try:
-                for product in ordered_products:
-                    if product.product:
-                        # product_price = ProductPrice.objects.filter(
-                        #     shop=seller_shop, product=product.product, status=True
-                        # ).last().price_to_retailer
-                        product_price = float(round(product.product.rt_cart_product_mapping.last().cart_product_price.price_to_retailer, 2))
-                        shipped_qty = float(product.shipped_qty)
-                        amount = shipped_qty * product_price
-                        total_amount.append(amount)
-                return str(round(sum(total_amount),2))
-            except:
-                for product in ordered_products:
-                    if product.product:
-                        product_price = ProductPrice.objects.filter(
-                            shop=seller_shop, product=product.product, status=True
-                        ).last().price_to_retailer
-                        shipped_qty = float(product.shipped_qty)
-                        amount = shipped_qty * product_price
-                        total_amount.append(amount)
-                return str(round(sum(total_amount),2))
-                return str("-")
+            for product in ordered_products:
+                if product.product:
+                    product_price = float(round(product.product.rt_cart_product_mapping.last().get_cart_product_price(seller_shop).price_to_retailer, 2))
+                    shipped_qty = float(product.shipped_qty)
+                    amount = shipped_qty * product_price
+                    total_amount.append(amount)
+            return str(round(sum(total_amount),2))
         return str("-")
 
     def save(self, *args, **kwargs):
