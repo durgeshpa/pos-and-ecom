d1ac122c7a0d0491dc8697db2fbb9f7bfe824c96
Raj Shekhar Singh <raj@gramfactory.com>
2019-03-26 19:35:53 +0530

    sales report

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url,include
-from .views import ResizeImage
+from .views import ResizeImage, SalesReport
 
 urlpatterns = [
     # URLs that do not require a session or valid token
     url(r'^resize-img/(?P<image_path>.*)/(?P<image_name>.*)', ResizeImage.as_view(), name='resize-image'),
+    url(r'^shop-sales-report/$',SalesReport.as_view() , name='shop-sales-report'),
 ]
