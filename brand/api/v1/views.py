eb338b9459c48810b2db15ec25a060cd387cc75c
nripesh <nripesh@gramfactory.com>
2019-04-23 16:27:22 +0530

    subbrand list opt

--- brand/api/v1/views.py
+++ brand/api/v1/views.py
@@ -66,10 +66,14 @@ class GetSubBrandsListView(APIView):
         brand = Brand.objects.get(pk=brand_id)
         if shop_id and shop_id != '-1' and Shop.objects.get(id=shop_id).retiler_mapping.exists():
             parent = ParentRetailerMapping.objects.get(retailer=shop_id).parent
-            grns = OrderedProductMapping.get_shop_stock(shop=parent).filter(available_qty__gt=0)
-            product_subbrands = [grn.product.product_brand for grn in grns if grn.product.product_brand.brand_parent == brand ]
-            product_subbrands = set(product_subbrands)
-            product_subbrands = list(product_subbrands)
+            grns = OrderedProductMapping.get_shop_stock(shop=parent, show_available=True)
+            product_subbrands = []
+            
+            for grn in grns:
+                if grn.product.product_brand in product_subbrands:
+                    continue
+                product_subbrands.append(grn.product.product_brand)
+
             brand_data_serializer = SubBrandSerializer(product_subbrands,many=True)
         else:
             product_subbrands = brand.brnd_parent.filter(active_status='active')
