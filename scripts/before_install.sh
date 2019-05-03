7ac803e46b339cf1874d8d225dbc03b9dcf350a3
nripesh <nripesh@gramfactory.com>
2019-05-03 17:14:20 +0530

    removed code from deploy, git pull

--- scripts/before_install.sh
+++
@@ -1,5 +0,0 @@
-if [ -d /home/ubuntu/project/retailer-backend ]; then
-  sudo rm -R /home/ubuntu/project/retailer-backend
-  mv /home/ubuntu/project/tmp/retailer-backend /home/ubuntu/project/retailer-backend
-  sudo rm -R /home/ubuntu/project/tmp
-fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 source /home/ubuntu/project/prod/bin/activate
 cd /home/ubuntu/project/retailer-backend/
+git pull origin staging
 python manage.py migrate --fake
 python manage.py makemigrations
 python manage.py migrate
