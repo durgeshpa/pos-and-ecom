ad651f566266b758d56af34a44ee32d467ea1269
nripesh <nripesh@gramfactory.com>
2019-05-03 15:58:49 +0530

    pipeline changes for staging

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 source /home/ubuntu/project/prod/bin/activate
 cd /home/ubuntu/project/retailer-backend/
-git pull origin master
 python manage.py migrate --fake
 python manage.py makemigrations
 python manage.py migrate
