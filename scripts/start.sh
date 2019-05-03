e111862cabb08aeff5739c8b44cd7513c2ef17b3
nripesh <nripesh@gramfactory.com>
2019-05-03 17:40:01 +0530

    removed code from deploy, git pull

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,7 @@
 source /home/ubuntu/project/prod/bin/activate
 cd /home/ubuntu/project/retailer-backend/
-git pull origin staging
+git pull
 python manage.py migrate --fake
 python manage.py makemigrations
 python manage.py migrate
-
 sudo supervisorctl restart all
