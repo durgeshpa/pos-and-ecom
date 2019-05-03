417dac2b6f669f34985754aaad21e0446a055a14
nripesh <nripesh@gramfactory.com>
2019-05-03 13:09:43 +0530

    pipeline fixes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 if [ -d /home/ubuntu/project/retailer-backend ]; then
   sudo rm -R /home/ubuntu/project/retailer-backend
-  mkdir /home/ubuntu/project/retailer-backend
+  mv /home/ubuntu/project/tmp/retailer-backend /home/ubuntu/project/retailer-backend
+  sudo rm -R /home/ubuntu/project/tmp
 fi
