eea93274cfdd64a3b1594485807d327e43590796
nripesh <nripesh@gramfactory.com>
2019-05-06 15:34:56 +0530

    git pull after migrate fake

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 source /home/ubuntu/project/prod/bin/activate
 cd /home/ubuntu/project/retailer-backend/
-git pull
 python manage.py migrate --fake
+git pull
 python manage.py makemigrations
 python manage.py migrate
 sudo supervisorctl restart all
