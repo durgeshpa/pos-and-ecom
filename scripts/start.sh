83097d454ff2725a575ca6851e2fed4fc34dc352
nripesh <nripesh@gramfactory.com>
2019-05-07 00:36:03 +0530

    auto deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/project/retailer-backend/
 #fake all previous migrations
 python manage.py makemigrations
 python manage.py migrate --fake
-su - ubuntu
+su ubuntu
 echo "############################-----User Is ------####### "
 echo "$USER"
 #pull latest code
