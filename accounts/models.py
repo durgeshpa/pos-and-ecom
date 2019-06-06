171a5dd4d79e43f41264f3a9e8cb1c7d4b210c93
Sagar Bhatia <sagar@gramfactory.com>
2019-06-06 12:45:52 +0530

    removing last login date

--- accounts/models.py
+++ accounts/models.py
@@ -72,7 +72,7 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'),blank=True)
     user_photo = models.ImageField(upload_to='user_photos/', null=True, blank=True)
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default = '6', null=True)
-    last_login_date = models.DateField(auto_now_add=True)
+    #last_login_date = models.DateField(auto_now_add=True)
     imei_no = models.CharField(max_length=20,null=True,blank=True)
 
     USERNAME_FIELD = 'phone_number'
