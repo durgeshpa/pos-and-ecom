3e17008a8d26780d972ee77bf17774f861c8c893
Jagjeet Singh <jagjeet@gramfactory.com>
2019-06-12 15:25:00 +0530

    downloading working

--- shops/admin.py
+++ shops/admin.py
@@ -7,7 +7,7 @@ from .models import (
     Shop, ShopType, RetailerType, ParentRetailerMapping,
     ShopPhoto, ShopDocument, ShopInvoicePattern
 )
-from addresses.models import Address
+from addresses.models import Address, State, City
 from .forms import (ParentRetailerMappingForm, ShopParentRetailerMappingForm,
                     ShopForm, AddressForm, RequiredInlineFormSet,
                     AddressInlineFormSet)
@@ -30,27 +30,89 @@ class ShopResource(resources.ModelResource):
 
 class ExportCsvMixin:
     def export_as_csv(self, request, queryset):
-        #import pdb; pdb.set_trace()
+
+        cities_list = City.objects.values_list('city_name', flat=True)
+        states_list = State.objects.values_list('state_name', flat=True)
+
         output = io.BytesIO()
         data = Address.objects.values_list(
             'shop_name_id', 'shop_name__shop_name',
-            'shop_name__shop_type__shop_type', 'shop_name__shop_owner',
+            'shop_name__shop_type__shop_type',
+            'shop_name__shop_owner__phone_number',
             'shop_name__status', 'id', 'nick_name', 'address_line1',
             'address_contact_name', 'address_contact_number', 'pincode',
             'state__state_name', 'city__city_name', 'address_type'
         ).filter(shop_name__in=queryset)
+
+        data_rows = data.count()
         workbook = xlsxwriter.Workbook(output)
         worksheet = workbook.add_worksheet()
+
+        header_format = workbook.add_format({
+            'border': 1,
+            'bg_color': '#C6EFCE',
+            'bold': True,
+            'text_wrap': True,
+            'valign': 'vcenter',
+            'indent': 1,
+        })
+
+        # to set the width of column
+        worksheet.set_column('A:A', 10)
+        worksheet.set_column('B:B', 130)
+        worksheet.set_column('C:C', 10)
+        worksheet.set_column('D:D', 15)
+        worksheet.set_column('E:E', 10)
+        worksheet.set_column('F:F', 10)
+        worksheet.set_column('G:G', 50)
+        worksheet.set_column('H:H', 100)
+        worksheet.set_column('I:I', 20)
+        worksheet.set_column('J:J', 15)
+        worksheet.set_column('K:K', 10)
+        worksheet.set_column('L:L', 20)
+        worksheet.set_column('M:M', 20)
+        worksheet.set_column('N:N', 10)
+
+        # to set the hieght of row 
+        worksheet.set_row(0, 36)
+
+        # column headings
+        worksheet.write('A1', 'Shop ID', header_format)
+        worksheet.write('B1', 'Shop Name', header_format)
+        worksheet.write('C1', 'Shop Type', header_format)
+        worksheet.write('D1', 'Shop Owner', header_format)
+        worksheet.write('E1', 'Shop Activated', header_format)
+        worksheet.write('F1', 'Address ID', header_format)
+        worksheet.write('G1', 'Address Name', header_format)
+        worksheet.write('H1', 'Address', header_format)
+        worksheet.write('I1', "Contact Person", header_format)
+        worksheet.write('J1', 'Contact Number', header_format)
+        worksheet.write('K1', 'Pincode', header_format)
+        worksheet.write('L1', 'State', header_format)
+        worksheet.write('M1', 'City', header_format)
+        worksheet.write('N1', 'Address Type', header_format)
+
         for row_num, columns in enumerate(data):
             for col_num, cell_data in enumerate(columns):
-                worksheet.write(row_num, col_num, cell_data)
+                worksheet.write(row_num+1, col_num, cell_data)
+
+        worksheet.data_validation(
+            'L2:L{}'.format(data_rows), 
+            {'validate': 'list',
+             'source': list(states_list)})
+
+        worksheet.data_validation(
+            'M2:M{}'.format(data_rows), 
+            {'validate': 'list',
+             'source': list(cities_list)})
+
         workbook.close()
 
         # Rewind the buffer.
         output.seek(0)
 
         # Set up the Http response.
-        filename = 'django_simple.xlsx'
+        filename = 'Shops_sheet.xlsx'
         response = HttpResponse(
             output,
             content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
@@ -58,16 +120,6 @@ class ExportCsvMixin:
         response['Content-Disposition'] = 'attachment; filename=%s' % filename
 
         return response
-        # meta = self.model._meta
-        # list_display = ('shop_name', 'get_shop_parent', 'shop_owner','shop_type','created_at','status', 'get_shop_shipping_address', 'get_shop_city', 'get_shop_pin_code' )
-        # field_names = [field.name for field in meta.fields if field.name in list_display]
-        # response = HttpResponse(content_type='text/csv')
-        # response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
-        # writer = csv.writer(response)
-        # writer.writerow(list_display)
-        # for obj in queryset:
-        #     row = writer.writerow([getattr(obj, field) for field in list_display])
-        # return response
 
     export_as_csv.short_description = "Download CSV of Selected Shops"
 
