eb8dd5a25235328060447f60f7a0e1ed5e3bffa8
Sagar Bhatia <sagar@gramfactory.com>
2019-06-14 19:00:17 +0530

    changing date format in list shipment

--- common/common_utils.py
+++ common/common_utils.py
@@ -3,7 +3,7 @@ import datetime
 
 def convert_date_format_ddmmmyyyy(scheduled_date):
     #This function converts %d/%m/%Y datetime format to a DD/MMM/YYYY 
-    #format and returns a string for the same.
+    #format and returns a string for the same. 0-06-2019 18:24:21
 
     #logging.info("converting date format from %d/%m/%Y to %Y-%m-%d")
-    return datetime.datetime.strptime(scheduled_date,'%d/%m/%Y').strftime("%d/%b/%Y").__str__()
+    return datetime.datetime.strptime(scheduled_date,'%d-%m-%Y %H:%M:%S').strftime("%d/%b/%Y").__str__()
