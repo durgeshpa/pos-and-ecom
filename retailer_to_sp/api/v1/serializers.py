7d733c4e1d43f151831f7ca723db0faf3fa08230
Sagar Bhatia <sagar@gramfactory.com>
2019-06-18 12:28:21 +0530

    adding filter for qc_pending

--- retailer_to_sp/api/v1/serializers.py
+++ retailer_to_sp/api/v1/serializers.py
@@ -489,13 +489,12 @@ class OrderListSerializer(serializers.ModelSerializer):
     shipping_address = AddressSerializer()
     order_status = serializers.CharField(source='get_order_status_display')
     #rt_order_order_product = ListOrderedProductSerializer(many=True)
-    rt_order_order_product = serializers.SerializerMethodField()
+    shipment_data = serializers.SerializerMethodField()
 
     def get_rt_order_order_product(self):
         qs = OrderedProduct.objects.all().exclude(shipment_status='SHIPMENT_CREATED')
         serializer = ListOrderedProductSerializer(instance=qs, many=True)
         return serializer.data
-
 
     def to_representation(self, instance):
         representation = super(OrderListSerializer, self).to_representation(instance)
@@ -505,7 +504,7 @@ class OrderListSerializer(serializers.ModelSerializer):
     class Meta:
         model=Order
         fields = ('id','ordered_cart','order_no','total_final_amount','order_status','shipping_address',
-                  'created_at','modified_at','rt_order_order_product')
+                  'created_at','modified_at','shipment_data') #'rt_order_order_product')
 
 # Order List Related Serializer End
 
