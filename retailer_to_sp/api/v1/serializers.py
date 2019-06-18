842dd53c7863c24f8b69d2dc24d855a773b3eb10
Sagar Bhatia <sagar@gramfactory.com>
2019-06-18 12:44:02 +0530

    testing

--- retailer_to_sp/api/v1/serializers.py
+++ retailer_to_sp/api/v1/serializers.py
@@ -496,7 +496,7 @@ class OrderListSerializer(serializers.ModelSerializer):
     rt_order_order_product = serializers.SerializerMethodField()
 
     def get_rt_order_order_product(self, obj):
-        qs = OrderedProduct.objects.all().exclude(shipment_status='SHIPMENT_CREATED')
+        qs = OrderedProduct.objects.filter(order_id=obj.id).exclude(shipment_status='SHIPMENT_CREATED')
         serializer = ListOrderedProductSerializer(instance=qs, many=True)
         return serializer.data
 
