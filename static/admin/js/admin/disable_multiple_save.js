1ec47bc2e8a3fd425b1b02958e054378b836090f
Sagar Bhatia <sagar@gramfactory.com>
2019-06-19 12:27:14 +0530

    adding js to disable multiple click in admin dashboard

---
+++ static/admin/js/admin/disable_multiple_save.js
@@ -0,0 +1,21 @@
+function test(){
+
+$('input[name=_save]').click(function(){
+
+//$('input[name=_save]').attr("disabled", true);
+alert("hello");
+});
+
+$('input[name=_addanother]').click(function(){
+
+//$('input[name=_addanother]').attr("disabled", true);
+alert("hello");
+});
+
+$('input[name=_continue]').click(function(){
+
+//$('input[name=_addanother]').attr("disabled", true);
+alert("hello");
+});
+
+}
