c74fb254561d4cb7e167fe823befe9627ba25416
Sagar Bhatia <sagar@gramfactory.com>
2019-06-26 19:33:21 +0530

    commenting code

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -203,12 +203,12 @@ class CartProductMapping(models.Model):
             return round(self.cart_product.get_current_shop_price(shop).mrp,2)
 
 
-class PickerAssignment(models.Model):
-    shop = models.ForeignKey(Shop, related_name="picker_shop", on_delete=models.CASCADE)
-    picker_boy = models.ForeignKey(User, related_name="picker_user", on_delete=models.CASCADE)
-    invoice_no = models.CharField(max_length=255, null=True, blank=True)
-    shipment = models.ForeignKey(Shipment, related_name="picker_shipment", on_delete=models.CASCADE)
-    #order = models.ForeignKey(Order, related_name="picker_order", on_delete=models.CASCADE)  
+# class PickerAssignment(models.Model):
+#     shop = models.ForeignKey(Shop, related_name="picker_shop", on_delete=models.CASCADE)
+#     picker_boy = models.ForeignKey(User, related_name="picker_user", on_delete=models.CASCADE)
+#     invoice_no = models.CharField(max_length=255, null=True, blank=True)
+#     shipment = models.ForeignKey(Shipment, related_name="picker_shipment", on_delete=models.CASCADE)
+#     #order = models.ForeignKey(Order, related_name="picker_order", on_delete=models.CASCADE)  
 
 
 
@@ -282,12 +282,12 @@ class Order(models.Model):
     total_discount_amount = models.FloatField(default=0)
     total_tax_amount = models.FloatField(default=0)
 
-    picking_status = models.CharField(max_length=50,choices=PICKING_STATUS)
-    assigned_picker = models.ForeignKey(
-        get_user_model(), related_name='order_assigned_picker',
-        null=True, blank=True, on_delete=models.SET_NULL
-    )
-    picklist = models.ForeignKey(PickList, related_name="picklist_order", on_delete=models.SET_NULL)
+    # picking_status = models.CharField(max_length=50,choices=PICKING_STATUS)
+    # assigned_picker = models.ForeignKey(
+    #     get_user_model(), related_name='order_assigned_picker',
+    #     null=True, blank=True, on_delete=models.SET_NULL
+    # )
+    # picklist = models.ForeignKey(PickList, related_name="picklist_order", on_delete=models.SET_NULL)
 
     order_status = models.CharField(max_length=50,choices=ORDER_STATUS)
     order_closed = models.BooleanField(default=False, null=True, blank=True)
