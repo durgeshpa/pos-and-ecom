11e860e7025937f2c642c716966bae588a8caca3
nripesh <nripesh@gramfactory.com>
2019-07-09 13:46:58 +0530

    fixes

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -414,6 +414,10 @@ class GramGRNProductsList(APIView):
             products_list = es_search(index="all_products", body=body)
 
         for p in products_list['hits']['hits']:
+            ptr = p['ptr'] 
+            loyalty_discount = p['loyalty_discount']
+            cash_discount = p['cash_discount']
+
             if cart_check == True:
                 for c_p in cart_products:
                     if c_p.cart_product_id == p["_source"]["id"]:
