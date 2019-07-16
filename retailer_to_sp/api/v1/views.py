e75b417e376ea21de5fbd0dc61410878b990a541
Mukesh <mukesh.gramfactory.com>
2019-07-16 13:00:06 +0530

    chsnges

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -982,11 +982,11 @@ class DownloadInvoiceSP(APIView):
             no_of_pieces = m.product.rt_cart_product_mapping.last().no_of_pieces
             cart_qty = m.product.rt_cart_product_mapping.last().qty
 
-            #old code for tax start
-            tax_sum = m.product.rt_cart_product_mapping.last().tax
-            tax_sum = round(tax_sum, 2)
-            get_tax_val = tax_sum / 100
-
+            #tax_sum = m.product.rt_cart_product_mapping.last().tax
+            #tax_sum = round(tax_sum, 2)
+            #get_tax_val = tax_sum / 100
+            
+            # old code for tax start
             all_tax_list = m.product.product_pro_tax
             if all_tax_list.exists():
                 for tax_dt in all_tax_list.all():
