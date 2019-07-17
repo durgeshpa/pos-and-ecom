ed9a60725fe92b578a10531111700e335ee9af6e
nripesh <nripesh@gramfactory.com>
2019-07-17 13:06:05 +0530

    added sentry on celery

--- retailer_backend/settings.py
+++ retailer_backend/settings.py
@@ -290,9 +290,10 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 # Initiate Sentry SDK
 if ENVIRONMENT.lower() in ["production","staging", "qa", "qa1"]:
+    from sentry_sdk.integrations.celery import CeleryIntegration
     sentry_sdk.init(
         dsn="https://2f8d192414f94cd6a0ba5b26d6461684@sentry.io/1407300",
-        integrations=[DjangoIntegration()],
+        integrations=[DjangoIntegration(),CeleryIntegration()],
         environment=ENVIRONMENT.lower()
     )
 
