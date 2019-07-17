d99fb702d86fa2aea900ba254dbf7ef760cfd254
nripesh <nripesh@gramfactory.com>
2019-07-17 12:04:33 +0530

    added price change signal

---
+++ products/signals.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from products.models import Product, ProductPrice, ProductCategory, ProductTaxMapping
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from sp_to_gram.tasks import update_shop_product_es
+
+@receiver(post_save, sender=Product)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+@receiver(post_save, sender=ProductPrice)
+def update_elasticsearch(sender, instance=None, created=False, **kwargs):
+    update_shop_product_es.delay(instance.shop.id, instance.product.id, available=instance.price_to_retailer)
