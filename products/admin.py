0641131b45f33243abaf68a874874e43a1ff017f
Jagjeet Singh <jagjeet@gramfactory.com>
2019-07-22 14:01:48 +0530

    fixed

--- products/admin.py
+++ products/admin.py
@@ -420,8 +420,7 @@ class ProductPriceAdmin(admin.ModelAdmin, ExportCsvMixin):
     product_gf_code.short_description = 'Gf Code'
 
     def approve_product_price(self, request, queryset):
-        queryset = queryset.filter(
-            status=False, approval_status='approval_pending')
+        queryset = queryset.filter(approval_status='approval_pending')
         for product in queryset:
             product.status = ProductPrice.APPROVED
             product.save()
