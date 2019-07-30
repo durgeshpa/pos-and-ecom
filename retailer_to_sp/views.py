0a440d8a8da9a80a7890fd32ccbfd4a39b6d4898
Sagar Bhatia <sagar@gramfactory.com>
2019-07-30 13:11:34 +0530

    testing

--- retailer_to_sp/views.py
+++ retailer_to_sp/views.py
@@ -670,8 +670,9 @@ class DownloadPickListPicker(TemplateView,):
                     "product_name": cart_pro.cart_product.product_name,
                     "product_sku": cart_pro.cart_product.product_sku,
                     "product_mrp": round(cart_pro.get_cart_product_price(order_obj.seller_shop).mrp,2),
-                    "ordered_qty": int(cart_pro.qty),
-                    "no_of_pieces":cart_pro.no_of_pieces,
+                    #"ordered_qty": int(cart_pro.qty),
+                    "ordered_qty": int(cart_pro.no_of_pieces),
+                    #"no_of_pieces":cart_pro.no_of_pieces,
                 }
                 cart_product_list.append(product_list)
 
