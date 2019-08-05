48cfb7aeefe0485cfa67bdcd11e71f78c6d33565
Sagar Bhatia <sagar@gramfactory.com>
2019-08-05 19:15:19 +0530

    resolving picking status for shipment

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -721,9 +721,10 @@ class OrderedProduct(models.Model): #Shipment
                                                         address_type='billing'
                                                         ).last().pk)
                 try:
-                    picker = PickerDashboard.objects.get(shipment_id=self.id)
-                    picker.picking_status="picking_complete"
-                    picker.save()
+                    PickerDashboard.objects.filter(shipment_id=self.id).update(picking_status="picking_complete")
+                    #picker = PickerDashboard.objects.filter(shipment_id=self.id).update(picking_status="picking_complete")
+                    # picker.picking_status="picking_complete"
+                    # picker.save()
                 except:
                     raise ValidationError(_("Please assign shipment to picker dashboard"),)
 
