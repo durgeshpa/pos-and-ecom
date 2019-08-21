c6f07abb73300571df03f12d08dcfd09d0510a43
Sagar Bhatia <sagar@gramfactory.com>
2019-08-21 15:07:34 +0530

    adding online payment

--- static/admin/js/CommercialLoadShipments.js
+++ static/admin/js/CommercialLoadShipments.js
@@ -71,7 +71,7 @@ function ShowMessage(msg){
 }
 
 
-var shipment_payment_id;
+//var shipment_payment_id;
 function CreateResponseTable(data){
   var trip_id = $('#id_trip_id').val();
   for (var i = 0; i < data['response_data'].length; i++) {
@@ -81,7 +81,8 @@ function CreateResponseTable(data){
       }
      
       var data1 = data['response_data'][i];
-      shipment_payment_id = data1['shipment_payment']['shipment_payment_id'];
+      var shipment_payment_id = data1['shipment_payment']['shipment_payment_id'];
+      var shipment_payment = "shipment_payment" + shipment_payment_id;
       var pk = data['response_data'][i]['pk'];
       var is_payment_approved = data['response_data'][i]['is_payment_approved'];
       var trip = data['response_data'][i]['trip'];
@@ -90,30 +91,43 @@ function CreateResponseTable(data){
       var invoice_no = "<td><a href='/admin/retailer_to_sp/cart/commercial/"+pk+"/shipment-details/' target='_blank'>"+ data['response_data'][i]['invoice_no'] + "</a></td>";
       var invoice_amount = "<td>" + data['response_data'][i]['invoice_amount'] + "</td>";
       var cash_to_be_collected = "<td>" + data['response_data'][i]['cash_to_be_collected'] + "</td>";
-      var cash_payment = "<td><form class='shipment_payment_info' action=''><input type='text' name='cash_payment' value='"+ data1['shipment_payment']['cash_payment_amount'] +"'></form></td>";
-      var online_payment_mode = "<td><select name='payment_mode'><option value='neft' selected>NEFT</option></select></td>";
-      var online_payment = "<td><input type='text' name='online_payment' value='"+ data1['shipment_payment']['online_payment_amount'] +"'></td>";
-      var reference_no = "<td><input type='text' name='reference_no' value='"+ data1['shipment_payment']['reference_no'] +"'></form></td>";
+      var cash_payment = "<td><form class='"+ shipment_payment +"' action=''><input type='text' name='cash_amount' value='"+ data1['shipment_payment']['cash_payment_amount'] +"'></form></td>";
+      var online_payment_mode = "<td><form class='"+ shipment_payment +"' action=''><select name='payment_mode'><option value='' selected>Select</option>"+
+      "<option value='neft'>NEFT</option><option value='upi'>UPI</option><option value='rtgs'>RTGS</option><option value='imps'>IMPS</option>"+
+      "</select></form></td>";
+      var online_payment = "<td><form class='"+ shipment_payment +"' action=''><input type='text' name='online_amount' value='"+ data1['shipment_payment']['online_payment_amount'] +"'></form></td>";
+      var reference_no = "<td><form class='"+ shipment_payment +"' action=''><input type='text' name='reference_no' value='"+ data1['shipment_payment']['reference_no'] +"'></form></td>";
 
       var invoice_city = "<td>" + data['response_data'][i]['invoice_city'] + "</td>";
       var shipment_address = "<td>" + data['response_data'][i]['shipment_address'] + "</td>";
       var created_at = "<td>" + data['response_data'][i]['created_at'] + "</td>";
-      
+      var submit_payment_button = "<td><form class='"+ shipment_payment +"' action=''><button class='shipment-payments-submit' type='button' data-id='"+ shipment_payment_id +"'>Submit!</button></form></td>";    
+  
       var append_data = "<tr class="+ row +"><td class='original'></td>" + invoice_no + invoice_amount + cash_to_be_collected + cash_payment 
-      + online_payment_mode + online_payment + reference_no 
-      + shipment_status + invoice_city + created_at + order + shipment_address + "</tr>"
+      + online_payment_mode + online_payment + reference_no + shipment_status + 
+      invoice_city + created_at + order + shipment_address + submit_payment_button +"</tr>"
 
       $("tbody#data").append(append_data);
   }
-      var submit_payment_button = "<button class='shipment-payments-submit' type='button'>Submit Payment Info!</button>"
-      $("tbody#data").append(submit_payment_button);
+/*      var submit_payment_button = "<button class='shipment-payments-submit' type='button'>Submit!</button>"*/
+      //$("tbody#data").append(submit_payment_button);
 
+/*      $('form.shipment_payment').submit(function(e){
+           e.preventDefault(); //Prevent the normal submission action
+           var form = this;
+           // ... Handle form submission
+          var id = $("input[name='shipment_payment_id']",form).val(); 
+          update_shipment_payment_information(id);//('{{ shipment_payment.id | escapejs }}');
+          if (count == 0)
+          submit_update_data(id);//('{{ shipment_payment.id | escapejs }}');
+      });*/
 
       $('.shipment-payments-submit').on('click',  function(event) { 
           //event.preventDefault();
-          update_shipment_payment_information();//('{{ shipment_payment.id | escapejs }}');
+          var id = $(this).attr('data-id');
+          update_shipment_payment_information(id);//('{{ shipment_payment.id | escapejs }}');
           if (count == 0)
-          submit_update_data(shipment_payment_id);//('{{ shipment_payment.id | escapejs }}');
+          submit_update_data(id);//('{{ shipment_payment.id | escapejs }}');
 
       });
 
@@ -151,8 +165,9 @@ count = 0;
 function update_shipment_payment_information(shipment_payment_id)
 {
     /* Function to update shipment payment */
-
-    var formarray = $(".shipment_payment_info").serializeArray();
+    var _class = "shipment_payment"+shipment_payment_id;
+    var formarray = $("."+_class).serializeArray();
+    //var formarray = $(".shipment_payment_info").serializeArray();
 
     for(var i=0; i<formarray.length;i++)
     {
@@ -171,9 +186,6 @@ function update_shipment_payment_information(shipment_payment_id)
     /*var cash_payment = {};
     cash_payment['paid_amount'] = formData['cash_payment'];
     formData['cash_payment'] = cash_payment; */
-    formData['cash_amount'] = formData['cash_payment'];
-    formData['online_amount'] = formData['online_payment'];
-
     //submit_update_data(shipment_payment_id);
 }
 
