19feac94c197af94050e060b05cda61fe567b1b8
Mukesh <mukesh.gramfactory.com>
2019-09-04 12:00:17 +0530

    changed shop_user_mapping code

--- shops/models.py
+++ shops/models.py
@@ -315,7 +315,7 @@ class ShopUserMapping(models.Model):
         if self.manager == self:
             raise ValidationError(_('Manager and Employee cannot be same'))
         else:
-            ShopUserMapping.objects.filter(shop=self.shop, shop__shop_type__shop_type='r', employee_group=self.employee_group, status=True).update(status=False)
+            ShopUserMapping.objects.filter(shop=self.shop, employee_group=self.employee_group, status=True).update(status=False)
             self.status = True
         super().save(*args, **kwargs)
 
