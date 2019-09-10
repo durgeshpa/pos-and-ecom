4f74e636c0b5c67746df176efa87860437772600
Sagar Bhatia <sagar@gramfactory.com>
2019-09-10 18:39:33 +0530

    adding serializer

--- payments/api/v1/serializers.py
+++ payments/api/v1/serializers.py
@@ -21,58 +21,15 @@ class OnlinePaymentSerializer(serializers.ModelSerializer):
         
 
 class ShipmentPaymentSerializer(serializers.ModelSerializer):
-    cash_amount = serializers.DecimalField(default=0.0000, max_digits=20, decimal_places=4)
-    payment_mode = serializers.CharField(allow_blank=True)
-    reference_no = serializers.CharField(default="")
-    online_amount = serializers.DecimalField(default=0.0000, max_digits=20, decimal_places=4)
 
-    #cash_payment = CashPaymentSerializer(fields=['paid_amount'])
-    #online_payment = OnlinePaymentSerializer()
     class Meta:
         model = ShipmentPayment
-        fields = ['description', 'cash_amount', 'payment_mode', 'reference_no', 'online_amount']  #"__all__"
+        fields = "__all__"#['description', 'cash_amount', 'payment_mode', 'reference_no', 'online_amount']  #"__all__"
+        depth = 1
 
     def validate(self, data):
         initial_data = self.initial_data
         reference_no = initial_data['reference_no']
         if not re.match("^[a-zA-Z0-9_]*$", reference_no):
             raise serializers.ValidationError('Referece number can not have special character!')
-        return initial_data     
-
-    def update(self, instance, validated_data):
-        
-        try:
-            with transaction.atomic():
-                #import pdb; pdb.set_trace()
-                cash_payment = validated_data.pop('cash_amount', None)
-
-                online_payment_mode = validated_data.pop('payment_mode', None)
-                reference_no = validated_data.pop('reference_no', None)
-                online_payment = validated_data.pop('online_amount', None)
-                description = validated_data.pop('description', None)
-
-                if cash_payment:
-                    _cash_payment = CashPayment.objects.get(payment=instance)
-                    _cash_payment.paid_amount = float(cash_payment) #.paid_amount
-                    _cash_payment.save()
-
-                if online_payment_mode:
-                    _payment_mode, created = PaymentMode.objects.get_or_create(
-                        payment=instance, payment_mode_name="online_payment")
-
-                    _online_payment, created = OnlinePayment.objects.get_or_create(payment=instance)
-                    _online_payment.paid_amount = float(online_payment) 
-                    _online_payment.reference_no = reference_no 
-                    _online_payment.online_payment_type = online_payment_mode
-                    _online_payment.save()
-                else:
-                    online_pay = OnlinePayment.objects.filter(payment=instance)
-                    if online_pay.exists():
-                        online_pay.delete()
-                instance.description = description
-                instance.save()
-                return instance
-        except Exception as e:
-            print (traceback.format_exc(sys.exc_info()))
-            raise serializers.ValidationError(e.message)        
-    
+        return initial_data  
