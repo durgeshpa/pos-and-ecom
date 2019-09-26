aa64c8074de0719a88d5eebe6af940f190b5defb
Raj Shekhar Singh <raj@gramfactory.com>
2019-09-27 03:13:02 +0530

    max_qty of a sku that can be purchased by a customer if offer availed on the sku

--- coupon/serializers.py
+++ coupon/serializers.py
@@ -4,10 +4,19 @@ from .models import *
 
 class CouponSerializer(serializers.ModelSerializer):
     is_applied = serializers.SerializerMethodField()
+    max_qty = serializers.SerializerMethodField()
     class Meta:
         model = Coupon
-        fields = ('coupon_name', 'coupon_code', 'is_applied')
+        fields = ('coupon_name', 'coupon_code', 'is_applied', 'max_qty')
 
     def get_is_applied(self, obj):
         status = False
         return status
+
+    def get_max_qty(self, obj):
+        for product_coupon in obj.rule.product_ruleset.all():
+            if product_coupon.max_qty_per_use > 0:
+                max_qty = product_coupon.max_qty_per_use
+            else:
+                max_qty = '-'
+        return max_qty
