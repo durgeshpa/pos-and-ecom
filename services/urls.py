f61b84ad557c3a8a93602cbef3096edfea870810
shivam <shivam@gramfactory.com>
2019-09-26 11:56:13 +0530

    redshift

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,7 @@ from .views import ResizeImage, SalesReport
 
 urlpatterns = [
     # URLs that do not require a session or valid token
+    url(r'^api/', include('services.api.urls')),
     url(r'^resize-img/(?P<image_path>.*)/(?P<image_name>.*)', ResizeImage.as_view(), name='resize-image'),
+
 ]
