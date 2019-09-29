a6cb49f97260377029d2c0d2848d1d6b4e40d87d
Raj Shekhar Singh <raj@gramfactory.com>
2019-09-29 15:42:32 +0530

    max_qty bug solved

--- retailer_to_sp/api/v1/serializers.py
+++ retailer_to_sp/api/v1/serializers.py
@@ -299,7 +299,7 @@ class CartProductMappingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CartProductMapping
-        fields = ('id', 'cart', 'cart_product', 'qty','qty_error_msg', 'sku_coupon_error_msg', 'is_available','no_of_pieces','product_sub_total', 'product_coupons')
+        fields = ('id', 'cart', 'cart_product', 'qty','qty_error_msg', 'is_available','no_of_pieces','product_sub_total', 'product_coupons')
 
 
 class CartSerializer(serializers.ModelSerializer):
