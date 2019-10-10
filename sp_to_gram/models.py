a6427ad314f456bd83190a5e736519c95664939d
Raj Shekhar Singh <raj@gramfactory.com>
2019-10-10 15:58:24 +0530

    changes

--- sp_to_gram/models.py
+++ sp_to_gram/models.py
@@ -436,8 +436,8 @@ def create_brand_note_id(sender, instance=None, created=False, **kwargs):
 def create_credit_note(instance=None, created=False, **kwargs):
     if created:
         return None
-    if(instance.rt_order_product_order_product_mapping.last() and 
-    instance.rt_order_product_order_product_mapping.all().aggregate(Sum('returned_qty')).get('returned_qty__sum') > 0 or 
+    if(instance.rt_order_product_order_product_mapping.last() and
+    instance.rt_order_product_order_product_mapping.all().aggregate(Sum('returned_qty')).get('returned_qty__sum') > 0 or
     instance.rt_order_product_order_product_mapping.all().aggregate(Sum('damaged_qty')).get('damaged_qty__sum')>0):
         invoice_prefix = instance.order.seller_shop.invoice_pattern.filter(status=ShopInvoicePattern.ACTIVE).last().pattern
         last_credit_note = CreditNote.objects.filter(shop=instance.order.seller_shop, status=True).order_by('credit_note_id').last()
@@ -490,7 +490,7 @@ def create_credit_note(instance=None, created=False, **kwargs):
             grn_item.save()
             try:
                 cart_product_map = instance.order.ordered_cart.rt_cart_list.filter(cart_product=item.product).last()
-                credit_amount += (Decimal(item.returned_qty)+Decimal(item.damaged_qty)) * cart_product_map.item_effective_prices
+                credit_amount += ((item.returned_qty + item.damaged_qty) * cart_product_map.item_effective_prices)
             except Exception as e:
                 logger.exception("Product price not found for {} -- {}".format(item.product, e))
                 credit_amount += Decimal(item.returned_qty) * item.product.product_pro_price.filter(
