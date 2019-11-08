ac7335108162d0c8e33bedf399b8b9dd4f6f83a8
Mukesh <mukesh.gramfactory.com>
2019-11-08 12:59:21 +0530

    daily stock cron changes

--- retailer_backend/cron.py
+++ retailer_backend/cron.py
@@ -77,11 +77,20 @@ class DailyStock(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request, *args, **kwargs):
-        for shop_obj in Shop.objects.filter(shop_type__shop_type='sp'):
+        daily_stock_dt = []
+        for shop_obj in Shop.objects.filter(shop_type__shop_type='sp', shop_name__icontains='GFDN'):
             sp_grn_product = OrderedProductMapping.get_shop_stock(shop_obj)
             product_sum = sp_grn_product.values('product', 'product__product_name', 'product__product_gf_code','product__product_sku').annotate(
                 product_qty_sum=Sum('available_qty')).annotate(damaged_qty_sum=Sum('damaged_qty'))
 
-            for product_dt in product_sum:
-                ShopStock.objects.using('gfanalytics').create(product_id=product_dt['product'], available_qty=product_dt['product_qty_sum'],
-                damage_qty=product_dt['damaged_qty_sum'], shop_id=shop_obj.id, created_at=datetime.now())
+            # grn_list_map = {int(i['product']): (i['delivered_qty_sum'], i['returned_qty_sum']) for i in
+            #                 GRNOrderProductMapping.objects.filter(grn_order__order=obj.order).values('product')}
+
+            daily_stock_dt.append(ShopStock(product_id=i['product'], available_qty=i['product_qty_sum'],
+                damage_qty=i['damaged_qty_sum'], shop_id=shop_obj.id, created_at=datetime.now()) for i in product_sum)
+
+            ShopStock.objects.bulk_create(daily_stock_dt)
+
+            # for product_dt in product_sum:
+            #     ShopStock.objects.using('gfanalytics').create(product_id=product_dt['product'], available_qty=product_dt['product_qty_sum'],
+            #     damage_qty=product_dt['damaged_qty_sum'], shop_id=shop_obj.id, created_at=datetime.now())
