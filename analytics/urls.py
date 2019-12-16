37a4587e28752ef6b037c6cd5399dd07bd9d89ac
nripesh <nripesh@gramfactory.com>
2019-12-16 16:09:48 +0530

    added analytics

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^v1/', include('analytics.api.v1.urls')),
+    url(r'^api/', include('analytics.api.urls')),
 ]
