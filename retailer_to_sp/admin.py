80a9ef2f40ca390cca1b36a1888e87a18d9cc0e2
Sagar Bhatia <sagar@gramafactory.com>
2020-01-08 23:01:52 +0530

    testing buyer shop search

--- retailer_to_sp/admin.py
+++ retailer_to_sp/admin.py
@@ -518,37 +518,37 @@ class ExportCsvMixin:
         writer = csv.writer(response)
         writer.writerow(list_display)
 
-        pickers = PickerDashboard.objects.filter(order__in=queryset, shipment__isnull=True)
-        if pickers.exists():
-            for picker in pickers:
-                obj = picker.order
-                row_items = [getattr(obj, field) for field  in list_display if field not in ['shipment_status','shipment_status_reason','order_shipment_amount',
-                                      'picking_status', 'picker_boy', 'picklist_id'] ]
-                shipment = picker.shipment
-                if shipment:
-                    row_items += [shipment.get_shipment_status_display(), shipment.return_reason, shipment.invoice_amount] 
-                else:
-                    row_items += ["-","-","-"]
-                row_items += [picker.get_picking_status_display(), picker.picker_boy, picker.picklist_id]
+        # pickers = PickerDashboard.objects.filter(order__in=queryset, shipment__isnull=True)
+        # if pickers.exists():
+        #     for picker in pickers:
+        #         obj = picker.order
+        #         row_items = [getattr(obj, field) for field  in list_display if field not in ['shipment_status','shipment_status_reason','order_shipment_amount',
+        #                               'picking_status', 'picker_boy', 'picklist_id'] ]
+        #         shipment = picker.shipment
+        #         if shipment:
+        #             row_items += [shipment.get_shipment_status_display(), shipment.return_reason, shipment.invoice_amount] 
+        #         else:
+        #             row_items += ["-","-","-"]
+        #         row_items += [picker.get_picking_status_display(), picker.picker_boy, picker.picklist_id]
 
-                row = writer.writerow(row_items)
+        #         row = writer.writerow(row_items)
 
-        shipments = OrderedProduct.objects.filter(order__in=queryset)
-        if shipments.exists():
-            for shipment in shipments:
-                obj = shipment.order
-                row_items = [getattr(obj, field) for field  in list_display if field not in ['shipment_status','shipment_status_reason','order_shipment_amount',
-                                      'picking_status', 'picker_boy', 'picklist_id'] ]
+        # shipments = OrderedProduct.objects.filter(order__in=queryset)
+        # if shipments.exists():
+        #     for shipment in shipments:
+        #         obj = shipment.order
+        #         row_items = [getattr(obj, field) for field  in list_display if field not in ['shipment_status','shipment_status_reason','order_shipment_amount',
+        #                               'picking_status', 'picker_boy', 'picklist_id'] ]
 
-                row_items += [shipment.get_shipment_status_display(), shipment.return_reason, shipment.invoice_amount, 
-                    shipment.picking_status, shipment.picker_boy, shipment.picklist_id]
+        #         row_items += [shipment.get_shipment_status_display(), shipment.return_reason, shipment.invoice_amount, 
+        #             shipment.picking_status, shipment.picker_boy, shipment.picklist_id]
 
-                #getattr(shipment, field) for field in list_display_s]
-                row = writer.writerow(row_items)
+        #         #getattr(shipment, field) for field in list_display_s]
+        #         row = writer.writerow(row_items)
 
-        # for obj in queryset:
-        #     row = writer.writerow([getattr(obj, field).replace('<br>', '\n') if field in ['shipment_status','shipment_status_reason','order_shipment_amount',
-        #                           'picking_status', 'picker_boy', 'picklist_id'] else getattr(obj, field) for field in list_display])
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field).replace('<br>', '\n') if field in ['shipment_status','shipment_status_reason','order_shipment_amount',
+                                  'picking_status', 'picker_boy', 'picklist_id'] else getattr(obj, field) for field in list_display])
         return response
     export_as_csv.short_description = "Download CSV of Selected Orders"
 
