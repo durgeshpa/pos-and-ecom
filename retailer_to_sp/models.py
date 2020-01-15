cbd42f45a24024fcdb38a01550aa45372234b261
nripesh <nripesh@gramfactory.com>
2020-01-16 01:58:08 +0530

    trip_amount calculation, bugfix--total_amount aggregated fix

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -869,8 +869,7 @@ class Trip(models.Model):
     @property
     def trip_amount(self):
         return OrderedProductMapping.objects.filter(ordered_product__in=self.rt_invoice_trip.all())\
-        .aggregate(
-            invoice_amount=Sum(F('effective_price')*F('shipped_qty'), output_field=FloatField()).get('invoice_amount')
+        .aggregate(invoice_amount=Sum(F('effective_price')*F('shipped_qty'), output_field=FloatField())).get('invoice_amount')
     
     @property
     def total_received_amount(self):
