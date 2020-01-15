05b47bc21bc07bdce8cfa85715b53e0dfb06eb11
nripesh <nripesh@gramfactory.com>
2020-01-15 08:57:30 +0530

    removed duplicate

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -1282,20 +1282,6 @@ class Invoice(models.Model):
     def invoice_amount(self):
         return self.shipment.invoice_amount 
 
-class Invoice(models.Model):
-    invoice_no = models.CharField(max_length=255, unique=True, db_index=True)
-    shipment = models.OneToOneField(OrderedProduct, related_name='invoice', on_delete=models.DO_NOTHING)
-    invoice_pdf = models.FileField(upload_to='shop_photos/shop_name/documents/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    modified_at = models.DateTimeField(auto_now=True, null=True)
-
-    class Meta:
-        verbose_name = "Invoice"
-        verbose_name_plural = "Invoices"
-
-    def __str__(self):
-        return self.invoice_no
-
 
 class PickerDashboard(models.Model):
 
