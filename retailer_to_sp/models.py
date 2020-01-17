3bef5c8ec58814cc7c1ae8b4e4333ff0811a151a
nripesh <nripesh@gramfactory.com>
2020-01-17 16:56:14 +0530

    total amount fix

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -866,8 +866,9 @@ class Trip(models.Model):
 
     @property
     def trip_amount(self):
-        return OrderedProductMapping.objects.filter(ordered_product__in=self.rt_invoice_trip.all())\
-        .aggregate(invoice_amount=Sum(F('effective_price')*F('shipped_qty'), output_field=FloatField())).get('invoice_amount')
+        return sum([s.invoice_amount for s in self.rt_invoice_trip.all()])
+        # return OrderedProductMapping.objects.filter(ordered_product__in=self.rt_invoice_trip.all())\
+        # .aggregate(invoice_amount=Sum(F('effective_price')*F('shipped_qty'), output_field=FloatField())).get('invoice_amount')
     
     @property
     def total_received_amount(self):
@@ -1517,7 +1518,7 @@ class OrderedProductMapping(models.Model):
             raise ValidationError(_('delivered, returned, damaged qty sum mismatched with shipped_qty'))
         else:
             super().save(*args, **kwargs)
-            if self.ordered_product.shipment_status == OrderedProduct.READY_TO_SHIP:
+            if not self.effictive_price and self.ordered_product.shipment_status != OrderedProduct.SHIPMENT_CREATED:
                 self.effictive_price = self.ordered_product.order.ordered_cart.rt_cart_list.filter(cart_product=self.product).last().item_effective_prices
                 super().save(*args, **kwargs)
 
