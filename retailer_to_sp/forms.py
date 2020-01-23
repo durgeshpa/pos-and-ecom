51d5c5bd86290ade4ed197cd98c08baef55bba78
Raj Shekhar Singh <raj@gramfactory.com>
2020-01-23 12:52:42 +0530

    bulk order

--- retailer_to_sp/forms.py
+++ retailer_to_sp/forms.py
@@ -724,7 +724,7 @@ class BulkCartForm(forms.ModelForm):
         queryset=Address.objects.filter(shop_name__shop_type__shop_type='r'),
         widget=autocomplete.ModelSelect2(
             url='bulk-shipping-address-autocomplete',
-            forward=('buyer_shop',)
+            forward=('buyer_shop',),
         required=False
         )
     )
@@ -732,7 +732,7 @@ class BulkCartForm(forms.ModelForm):
         queryset=Address.objects.filter(shop_name__shop_type__shop_type='r'),
         widget=autocomplete.ModelSelect2(
             url='bulk-billing-address-autocomplete',
-            forward=('buyer_shop',)
+            forward=('buyer_shop',),
         required=False
         )
     )
