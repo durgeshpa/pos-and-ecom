f68e33d4cecef1c6db96dd732c31a2d8fa1e5867
shivam <shivam@gramfactory.com>
2020-01-28 13:20:21 +0530

    eancode

--- updateean.py
+++ updateean.py
@@ -7,7 +7,7 @@ def update_ean_code():
     sheet.cell_value(0,0)
     for i in range(sheet.nrows-1):
         product_ean_code = str(sheet.cell_value(i+1, 4))
-        # print(type(product_ean_code))
+        print(type(product_ean_code.split('.')[0]))
         products = Product.objects.filter(id=sheet.cell_value(i+1, 0))
         products.update(product_ean_code=product_ean_code)
 
