05374cd8cd90887fa7f6b025bdc54c938775343c
shivam <shivam@gramfactory.com>
2020-01-28 12:15:29 +0530

    changed excel file

--- updateean.py
+++ updateean.py
@@ -6,8 +6,8 @@ def update_ean_code():
     sheet = wb.sheet_by_index(0)
     sheet.cell_value(0,0)
     for i in range(sheet.nrows-1):
-        product_ean_code = sheet.cell_value(i+1, 4)
-        print(product_ean_code)
+        product_ean_code = str(sheet.cell_value(i+1, 4))
+        # print(type(product_ean_code))
         products = Product.objects.filter(id=sheet.cell_value(i+1, 0))
         products.update(product_ean_code=product_ean_code)
 
