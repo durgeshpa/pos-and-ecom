73240a8e261d8656a018803b4172200a001e042f
Jagjeet Singh <jagjeet@gramfactory.com>
2020-02-04 12:16:31 +0530

    added excel code

--- retailer_to_sp/utils.py
+++ retailer_to_sp/utils.py
@@ -1,3 +1,6 @@
+import io
+import xlsxwriter
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -5,6 +8,7 @@ from rest_framework.permissions import AllowAny
 from django.utils.html import format_html_join, format_html
 from django.utils.safestring import mark_safe
 from django.urls import reverse
+from django.http import HttpResponse
 
 from products.models import Product
 
@@ -238,3 +242,148 @@ def reschedule_shipment_button(obj):
         (reverse('admin:retailer_to_sp_shipmentrescheduling_add'),
          obj.id)
     )
+
+def create_order_data_excel(queryset):
+    worksheet.write('A1', 'Order No.', header_format)
+    worksheet.write('A1', 'Seller Shop ID', header_format)
+    worksheet.write('A1', 'Seller Shop Name', header_format)
+    worksheet.write('A1', 'Buyer Shop ID', header_format)
+    worksheet.write('A1', 'Buyer Shop Name', header_format)
+    worksheet.write('A1', 'Mobile No.(Buyer Shop)', header_format)
+    worksheet.write('A1', 'Pincode', header_format)
+    worksheet.write('A1', 'City', header_format)
+    worksheet.write('A1', 'Total Final Amount', header_format)
+    worksheet.write('A1', 'Order Status', header_format)
+    worksheet.write('A1', 'Order Created At', header_format)
+    worksheet.write('A1', 'Payment Mode', header_format)
+    worksheet.write('A1', 'Paid Amount', header_format)
+    worksheet.write('A1', 'Total Paid Amount', header_format)
+    worksheet.write('A1', 'Invoice No', header_format)
+    worksheet.write('A1', 'Shipment Status', header_format)
+    worksheet.write('A1', 'Shipment Amount', header_format)
+    worksheet.write('A1', 'Trip Completed At', header_format)
+    worksheet.write('A1', 'Picking Status', header_format)
+    worksheet.write('A1', 'Picker Boy Name', header_format)
+    worksheet.write('A1', 'Picker Boy Mobile No', header_format)
+    worksheet.write('A1', 'Picklist ID', header_format)
+
+
+
+
+
+order_no    seller_shop buyer_shop_id   buyer_shop_with_mobile  pincode city
+total_final_amount  order_status    created_at  payment_mode    paid_amount total_paid_amount   invoice_no
+shipment_status shipment_status_reason  order_shipment_amount   trip_completed_at   picking_status  picker_boy  picklist_id
+    cities_list = City.objects.values_list('city_name', flat=True)
+    states_list = State.objects.values_list('state_name', flat=True)
+    output = io.BytesIO()
+    data = Address.objects.values_list(
+        'shop_name__id', 'shop_name__shop_name', 'shop_name__shop_type__shop_type',
+        'shop_name__shop_owner__phone_number', 'shop_name__status', 'id', 'nick_name',
+        'address_line1', 'address_contact_name', 'address_contact_number',
+        'pincode_link__pincode', 'state__state_name', 'city__city_name', 'address_type',
+        'shop_name__imei_no', 'shop_name__retiler_mapping__parent__shop_name',
+        'shop_name__created_at').filter(shop_name__in=queryset)
+    data_rows = data.count()
+    workbook = xlsxwriter.Workbook(output, {'default_date_format':
+                                            'dd/mm/yy hh:mm:ss'})
+    worksheet = workbook.add_worksheet()
+    unlocked = workbook.add_format({'locked': 0})
+
+    header_format = workbook.add_format({
+        'border': 1,
+        'bg_color': '#C6EFCE',
+        'bold': True,
+        'text_wrap': True,
+        'valign': 'vcenter',
+        'indent': 1,
+    })
+
+    format1 = workbook.add_format({'bg_color': '#FFC7CE',
+                               'font_color': '#9C0006'})
+
+    # to set the width of column
+    worksheet.set_column('A:A', 10)
+    worksheet.set_column('B:B', 100)
+    worksheet.set_column('C:C', 10)
+    worksheet.set_column('D:D', 15)
+    worksheet.set_column('E:E', 10)
+    worksheet.set_column('F:F', 10)
+    worksheet.set_column('G:G', 50)
+    worksheet.set_column('H:H', 100)
+    worksheet.set_column('I:I', 20)
+    worksheet.set_column('J:J', 15)
+    worksheet.set_column('K:K', 10)
+    worksheet.set_column('L:L', 20)
+    worksheet.set_column('M:M', 20)
+    worksheet.set_column('N:N', 10)
+    worksheet.set_column('O:O', 20)
+    worksheet.set_column('P:P', 40)
+    worksheet.set_column('Q:Q', 20)
+
+    # to set the hieght of row
+    worksheet.set_row(0, 36)
+
+    # column headings
+    worksheet.write('A1', 'Shop ID', header_format)
+    worksheet.write('B1', 'Shop Name', header_format)
+    worksheet.write('C1', 'Shop Type', header_format)
+    worksheet.write('D1', 'Shop Owner', header_format)
+    worksheet.write('E1', 'Shop Activated', header_format)
+    worksheet.write('F1', 'Address ID', header_format)
+    worksheet.write('G1', 'Address Name', header_format)
+    worksheet.write('H1', 'Address', header_format)
+    worksheet.write('I1', "Contact Person", header_format)
+    worksheet.write('J1', 'Contact Number', header_format)
+    worksheet.write('K1', 'Pincode', header_format)
+    worksheet.write('L1', 'State', header_format)
+    worksheet.write('M1', 'City', header_format)
+    worksheet.write('N1', 'Address Type', header_format)
+    worksheet.write('O1', 'IMEI', header_format)
+    worksheet.write('P1', 'Parent Shop Name', header_format)
+    worksheet.write('Q1', 'Shop created at', header_format)
+
+
+    for row_num, columns in enumerate(data):
+        for col_num, cell_data in enumerate(columns):
+            if cell_data and col_num == 16:
+                worksheet.write_datetime(row_num + 1, col_num, cell_data)
+            elif cell_data and col_num == 4:
+                worksheet.write_boolean(row_num + 1, col_num, cell_data)
+            else:
+                worksheet.write(row_num + 1, col_num, cell_data)
+
+    worksheet.data_validation(
+        'L2:L{}'.format(data_rows + 1),
+        {'validate': 'list',
+         'source': list(states_list)})
+
+    worksheet.data_validation(
+        'M2:M{}'.format(data_rows + 1),
+        {'validate': 'list',
+         'source': list(cities_list)})
+
+    worksheet.data_validation(
+        'E2:E{}'.format(data_rows + 1),
+        {'validate': 'list',
+         'source': [True, False]})
+
+    worksheet.data_validation(
+        'N2:N{}'.format(data_rows + 1),
+        {'validate': 'list',
+         'source': ['billing', 'shipping']})
+
+    workbook.close()
+
+    # Rewind the buffer.
+    output.seek(0)
+
+    # Set up the Http response.
+    filename = 'Shops_sheet.xlsx'
+    response = HttpResponse(
+        output,
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+
+    return response
