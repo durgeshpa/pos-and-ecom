46e9bf74672879d5fbdb70b93f4255b59f237d44
Raj Shekhar Singh <raj@gramfactory.com>
2020-02-18 17:35:43 +0530

    done

--- retailer_to_sp/models.py
+++ retailer_to_sp/models.py
@@ -1552,7 +1552,7 @@ class OrderedProductMapping(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     effective_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=False)
-    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=False)
+    discounted_price = models.DecimalField(default = 0, max_digits=10, decimal_places=2, null=True, blank=False)
 
     def clean(self):
         super(OrderedProductMapping, self).clean()
