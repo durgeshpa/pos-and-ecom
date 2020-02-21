98dee99cc5621dc3f068540cb8ea53bef0c16ccb
Raj Shekhar Singh <raj@gramfactory.com>
2020-02-21 12:57:38 +0530

    ean code mandatory

--- products/models.py
+++ products/models.py
@@ -122,7 +122,7 @@ class Product(models.Model):
     product_long_description = models.TextField(null=True,blank=True)
     product_sku = models.CharField(max_length=255, blank=False, unique=True)
     product_gf_code = models.CharField(max_length=255, blank=False, unique=True)
-    product_ean_code = models.CharField(max_length=255, blank=True)
+    product_ean_code = models.CharField(max_length=255, blank=False)
     product_hsn = models.ForeignKey(ProductHSN,related_name='product_hsn',null=True,blank=True,on_delete=models.CASCADE)
     product_brand = models.ForeignKey(Brand,related_name='prodcut_brand_product',blank=False,on_delete=models.CASCADE)
     product_inner_case_size = models.CharField(max_length=255,blank=False, default=1)
@@ -578,4 +578,3 @@ class ProductCapping(models.Model):
 
 # post_save.connect(get_category_product_report, sender=Product)
 # post_save.connect(get_master_report, sender=ProductPrice)
-
