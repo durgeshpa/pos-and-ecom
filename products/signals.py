d93cc15b69a3fdb23b17747bdb8d8588cf990a18
shivam <shivam@gramfactory.com>
2020-03-19 19:19:05 +0530

    product deactivation

--- products/signals.py
+++ products/signals.py
@@ -39,10 +39,11 @@ def update_product_image_elasticsearch(sender, instance=None, created=False, **k
 
 @receiver(post_save, sender=Product)
 def update_product_elasticsearch(sender, instance=None, created=False, **kwargs):
-    for prod_price in instance.product_pro_price.filter(status=True).values('seller_shop', 'product'):
-	    update_shop_product_es.delay(prod_price['seller_shop'], prod_price['product'], name=instance.product_name)
-	    update_shop_product_es.delay(prod_price['seller_shop'], prod_price['product'], pack_size=instance.product_inner_case_size)
-	    update_shop_product_es.delay(prod_price['seller_shop'], prod_price['product'], product_status=instance.status)
+    if instance.status:
+        for prod_price in instance.product_pro_price.filter(status=True).values('seller_shop', 'product'):
+            update_shop_product_es.delay(prod_price['seller_shop'], prod_price['product'], name=instance.product_name)
+            update_shop_product_es.delay(prod_price['seller_shop'], prod_price['product'], pack_size=instance.product_inner_case_size)
+            update_shop_product_es.delay(prod_price['seller_shop'], prod_price['product'], product_status=instance.status)
 
 
 post_save.connect(get_category_product_report, sender=Product)
