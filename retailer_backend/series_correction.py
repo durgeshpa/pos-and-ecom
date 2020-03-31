58527b9d87179e22590400c67260efb39ff09301
Raj Shekhar Singh <raj@gramfactory.com>
2020-03-31 19:02:31 +0530

    invoice seriesand order series correction for bulk orders in seller

--- retailer_backend/series_correction.py
+++ retailer_backend/series_correction.py
@@ -1,10 +1,10 @@
 from retailer_to_sp.models import *
-for m in Invoice.objects.filter(invoice_no__startswith='GB').all():
-    m.invoice_no = m.invoice_no[1:]
+for m in Invoice.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
+    m.invoice_no = m.invoice_no[2:]
     print(m.invoice_no)
     m.save()
 
-for m in Order.objects.filter(order_no__startswith = 'GB').all():
-    m.order_no = m.order_no[1:]
+for m in Order.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
+    m.order_no = m.order_no[2:]
     print(m.invoice_no)
     m.save()
