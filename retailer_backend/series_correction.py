bb3f1728c210ecc68eff73504043883675b3c402
Raj Shekhar Singh <raj@gramfactory.com>
2020-03-31 21:09:54 +0530

    done

--- retailer_backend/series_correction.py
+++ retailer_backend/series_correction.py
@@ -1,10 +1,8 @@
 from retailer_to_sp.models import *
 for m in Invoice.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
-    m.invoice_no = m.invoice_no[2:]
-    print(m.invoice_no)
-    m.save()
+    invoice_no = m.invoice_no[2:]
+    Invoice.objects.filter(id = m.id).update(invoice_no = invoice_no)
 
 for m in Order.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
-    m.order_no = m.order_no[2:]
-    print(m.invoice_no)
-    m.save()
+    order_no = m.order_no[2:]
+    Order.objects.filter(id = m.id).update(order_no = order_no)
