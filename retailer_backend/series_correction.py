e8d148e6a8dc599129d06789b2ef2ab14dbea12a
nripesh <nripesh@gramfactory.com>
2020-04-01 16:08:46 +0530

    fixed Cart Order number

--- retailer_backend/series_correction.py
+++ retailer_backend/series_correction.py
@@ -2,11 +2,11 @@ from retailer_to_sp.models import *
 from django.db.models import Q
 
 def edit_series():
-	for m in Invoice.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
+	for m in Invoice.objects.filter(Q(invoice_no__startswith='GD') | Q(invoice_no__startswith='PD')).all():
 	    invoice_no = m.invoice_no[2:]
 	    Invoice.objects.filter(id = m.id).update(invoice_no = invoice_no)
 
-	for m in Order.objects.filter(Q(order_no__startswith='GB') | Q(order_no__startswith='PB')).all():
+	for m in Order.objects.filter(Q(order_no__startswith='GD') | Q(order_no__startswith='PD')).all():
 	    order_no = m.order_no[2:]
 	    Order.objects.filter(id = m.id).update(order_no = order_no)
 
