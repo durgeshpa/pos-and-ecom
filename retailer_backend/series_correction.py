9abbb7acc0eba82eea5226b1860fa8f4ad1d00b9
nripesh <nripesh@gramfactory.com>
2020-04-01 13:21:20 +0530

    edit series, callable function

--- retailer_backend/series_correction.py
+++ retailer_backend/series_correction.py
@@ -1,9 +1,14 @@
 from retailer_to_sp.models import *
 from django.db.models import Q
-for m in Invoice.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
-    invoice_no = m.invoice_no[2:]
-    Invoice.objects.filter(id = m.id).update(invoice_no = invoice_no)
 
-for m in Order.objects.filter(Q(order_no__startswith='GB') | Q(order_no__startswith='PB')).all():
-    order_no = m.order_no[2:]
-    Order.objects.filter(id = m.id).update(order_no = order_no)
+def edit_series():
+	for m in Invoice.objects.filter(Q(invoice_no__startswith='GB') | Q(invoice_no__startswith='PB')).all():
+	    invoice_no = m.invoice_no[2:]
+	    Invoice.objects.filter(id = m.id).update(invoice_no = invoice_no)
+
+	for m in Order.objects.filter(Q(order_no__startswith='GB') | Q(order_no__startswith='PB')).all():
+	    order_no = m.order_no[2:]
+	    Order.objects.filter(id = m.id).update(order_no = order_no)
+
+if __name__=='__main__':
+	edit_series()
