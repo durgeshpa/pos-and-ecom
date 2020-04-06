1bfe135dcc41fd221c190ba08d858d26cce4eaf2
shivam <shivam@gramfactory.com>
2020-04-06 11:58:26 +0530

    updated ean for april

--- updateean.py
+++ updateean.py
@@ -37,7 +37,17 @@ def updateEanCodeMarch():
         products.update(product_ean_code=product_ean_code)
 
 
-
+def updateEanCodeApril():
+    wb = xlrd.open_workbook('EAN_Update_2.xlsx')
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+    for i in range(sheet.nrows - 1):
+        product_ean_code = str(sheet.cell_value(i + 1, 3)).split('.')[0]
+        product_gf_code = sheet.cell_value(i + 1, 2)
+        print(product_gf_code, product_ean_code)
+        products = Product.objects.filter(product_gf_code=sheet.cell_value(i + 1, 2))
+        print(products)
+        products.update(product_ean_code=product_ean_code)
 
 
 def update():
