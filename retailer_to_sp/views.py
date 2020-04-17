6fd5b5e5f3f34a529fb23cfa6ee7e7d60adf142f
Jagjeet Singh <jagjeet@gramfactory.com>
2020-04-17 15:43:43 +0530

    fixed

--- retailer_to_sp/views.py
+++ retailer_to_sp/views.py
@@ -1254,9 +1254,10 @@ class OrderCancellation(object):
 
         reserved_qty_queryset = self.get_reserved_qty()
         for item in reserved_qty_queryset:
-            sp_ordered_product_mapping = SPOrderedProductMapping.objects.get(id=item['sp_grn'])
-            sp_ordered_product_mapping.available_qty = sp_ordered_product_mapping.available_qty + item['r_qty']
-            sp_ordered_product_mapping.save()
+            sp_ordered_product_mapping = SPOrderedProductMapping.objects.filter(id=item['sp_grn'])
+            for opm in sp_ordered_product_mapping:
+                opm.available_qty = opm.available_qty + item['r_qty']
+                opm.save()
 
         reserved_qty_queryset.update(reserve_status=OrderedProductReserved.ORDER_CANCELLED)
 
