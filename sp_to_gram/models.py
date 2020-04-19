e91f825a97bdcccbe72d25ba8cf2bfd57ed742af
nripesh <nripesh@gramfactory.com>
2020-04-19 20:18:56 +0530

    removed logs

--- sp_to_gram/models.py
+++ sp_to_gram/models.py
@@ -400,20 +400,16 @@ class StockAdjustmentMapping(models.Model):
 
 def commit_updates_to_es(shop, product):
     status = True
-    logger.exception("entering in update ES, shop {} product {}".format(shop, product))
     db_available_products = OrderedProductMapping.get_product_availability(shop, product)
     products_available = db_available_products.aggregate(Sum('available_qty'))['available_qty__sum']
-    logger.exception("products available : {}".format(products_available))
     available_qty = int(int(products_available)/int(product.product_inner_case_size))
     if not available_qty:
         status = False
 
-    logger.exception("upliading update to ES product : {}, available: {}".format(product, available_qty))
     update_shop_product_es.delay(shop.id, product.id, available=available_qty, status=status)
 
 @receiver(post_save, sender=OrderedProductMapping)
 def update_elasticsearch(sender, instance=None, created=False, **kwargs):
-    logger.exception("updated for instance {}, qty: {}".format(instance.id, instance.available_qty))
     transaction.on_commit(lambda: commit_updates_to_es(instance.shop, instance.product))
 
 @receiver(pre_save, sender=SpNote)
