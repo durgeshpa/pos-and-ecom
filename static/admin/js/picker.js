6f96be0e033e9cce33d7af3a12ea102b701e0afb
Chandan <chandan@Chandans-MacBook-Pro.local>
2020-06-02 14:48:53 +0530

    [ch_gf_1632:-Issue of Error message without selecting file is resolved for order, invoice and picker dashboard]

--- static/admin/js/picker.js
+++ static/admin/js/picker.js
@@ -1,9 +1,20 @@
 /* This js is only for Order, Invoice and Picker Dashboard Admin. */
 (function($) {
-    'use strict';
-    $(document).ready(function(){
-    $(".button").click(function(){
-        document.getElementById("changelist-form").submit();
+    $(document).ready(function() {
+      $("#changelist-form").submit(function( event ) {
+        event.preventDefault()
+            var shipment_id = [];
+            $.each($("input[name='_selected_action']:checked"), function(){
+                shipment_id.push($(this).val());
+            });
+            if(shipment_id.length < 1)
+            {
+            alert("No Order has been selected. Please select atleast one order to perform action.");
+            }
+            else
+            {
+            event.currentTarget.submit();
+            }
     });
-});
+  });
 })(django.jQuery);
