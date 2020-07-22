098509e4f7369b3acdc1f720b83f99da112ac8ec
shivam <shivam@gramfactory.com>
2020-07-22 14:30:55 +0530

    api for checking bin_id present or not

--- wms/api/v1/urls.py
+++ wms/api/v1/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from .views import PutAwayViewSet, BinViewSet, PutAwayProduct, PickupList, BinIDList, PickupDetail, PickupComplete
+from .views import PutAwayViewSet, BinViewSet, PutAwayProduct, PickupList, BinIDList, PickupDetail, PickupComplete, CheckBinID
 
 urlpatterns = [
     # url(r'^upload-csv/$', bins_upload, name="bins_upload"),
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^pickup/$', PickupList.as_view(), name='pick-up'),
     url(r'order-bins/$', BinIDList.as_view(), name='order-bins'),
     url(r'pickup-detail/$', PickupDetail.as_view(), name='details'),
-    url(r'^pick-complete/$', PickupComplete.as_view(), name='pickup-complete')
+    url(r'^pick-complete/$', PickupComplete.as_view(), name='pickup-complete'),
+    url(r'^check-bins/$', CheckBinID.as_view(), name='check-bins')
 
 ]
