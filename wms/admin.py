e72f9518d9cdd0a71a08d6f1906a57b916ca4cec
Chandan <chandan@Chandans-MacBook-Pro.local>
2020-07-22 18:07:39 +0530

    [testing-wms:- changes in  Bin Inventory Admin]

--- wms/admin.py
+++ wms/admin.py
@@ -113,11 +113,11 @@ class InventoryTypeAdmin(admin.ModelAdmin):
 
 class BinInventoryAdmin(admin.ModelAdmin):
     info_logger.info("Bin Inventory Admin has been called.")
-    form = BinInventoryForm
-    list_select_related = ('warehouse', 'sku', 'bin', 'inventory_type')
+    #form = BinInventoryForm
+    #list_select_related = ('warehouse', 'sku', 'bin', 'inventory_type')
     list_display = ('batch_id','warehouse', 'sku', 'bin','inventory_type', 'quantity', 'in_stock')
-    readonly_fields = ('batch_id','warehouse', 'sku', 'bin','inventory_type', 'in_stock')
-    list_filter = ('warehouse', 'sku', 'batch_id')
+    #readonly_fields = ('batch_id','warehouse', 'sku', 'bin','inventory_type', 'in_stock')
+    #list_filter = ('warehouse', 'sku', 'batch_id')
     list_per_page = 50
 
 
