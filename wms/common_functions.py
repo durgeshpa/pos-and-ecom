51072d244d32780d611470198e56890631176a35
shivam <shivam@gramfactory.com>
2020-08-05 20:47:29 +0530

    ordered

--- wms/common_functions.py
+++ wms/common_functions.py
@@ -267,7 +267,6 @@ class OrderManagement(object):
         shop_id = params['shop_id']
         products = params['products']
         transaction_type = params['transaction_type']
-        print("et")
 
         for prod_id, ordered_qty in products.items():
             WarehouseInventory.objects.create(warehouse=Shop.objects.get(id=shop_id),
@@ -497,9 +496,10 @@ def common_for_release(prod_list, shop_id, transaction_type, transaction_id, ord
             available_qty = wim.last().quantity
             if order_status == 'ordered':
                 wim.update(quantity=available_qty)
+                WarehouseInventory.objects.filter(id=ordered_id).update(quantity=reserved_qty,inventory_state=InventoryState.objects.filter(inventory_state='ordered').last())
             else:
                 wim.update(quantity=available_qty+reserved_qty)
-            WarehouseInventory.objects.filter(id=ordered_id).update(quantity=0)
+                WarehouseInventory.objects.filter(id=ordered_id).update(quantity=0)
             warehouse_details = WarehouseInternalInventoryChange.objects.filter(transaction_id=transaction_id,
                                                                            transaction_type='reserved',
                                                                            status=True)
