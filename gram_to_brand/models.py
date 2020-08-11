be97149f1e74231b05e486b8a927f8496ab0d584
Chandan <chandan@Chandans-MacBook-Pro.local>
2020-08-11 16:08:55 +0530

    [#ch_wms_inventory_movement:-Validation for Document Number and document done]

--- gram_to_brand/models.py
+++ gram_to_brand/models.py
@@ -417,11 +417,10 @@ class Document(models.Model):
     document_number = models.CharField(max_length=255,null=True,blank=True)
     document_image = models.FileField(null=True,blank=True,upload_to='brand_invoice')
 
-    def clean(self):
-        import pdb;pdb.set_trace()
-        super(Document).clean()
-        if self.document_image is None:
-            raise ValidationError("Document needs to be uploaded")
+    # def clean(self):
+    #     super(Document).clean()
+    #     if self.document_image is None:
+    #         raise ValidationError("Document needs to be uploaded")
 
 class GRNOrderProductMapping(models.Model):
     grn_order = models.ForeignKey(GRNOrder,related_name='grn_order_grn_order_product',null=True,blank=True,on_delete=models.CASCADE)
