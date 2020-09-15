78eef2407365b603b9d9276503e35bc0296ab8ea
Chandan <chandan@Chandans-MacBook-Pro.local>
2020-09-15 19:16:28 +0530

    [#ch_cron_order_release:-change the cron time for release inventory]

--- retailer_backend/settings.py
+++ retailer_backend/settings.py
@@ -331,7 +331,7 @@ CRONJOBS = [
     ('* */6 * * *', 'retailer_backend.cron.sync_es_products'),
     ('30 21 * * *', 'shops.api.v1.views.set_shop_map_cron', '>>/tmp/shops'),
 
-    ('*/8 * * * *', 'wms.views.release_blocking_with_cron', '>>/tmp/release.log'),
+    ('*/1 * * * *', 'wms.views.release_blocking_with_cron', '>>/tmp/release.log'),
     ('*/5 * * * *', 'wms.views.pickup_entry_creation_with_cron', '>>/tmp/picking'),
     ('* */6 * * *', 'retailer_backend.cron.sync_es_products')
 ]
