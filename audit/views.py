337f8179d08f96048fc5d3cb9e5ef511827149a6
Deepti <deepti@gramfactory.com>
2020-09-18 11:31:54 +0530

    Bin Warehouse inventory match

--- audit/views.py
+++ audit/views.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
 
+from django.http import HttpResponse
 from django.shortcuts import render
 import logging
 from shops.models import Shop
-from .models import (AuditRun,AuditRunItems,AuditDetails,
+from .models import (AuditRun, AuditRunItems, AuditDetails,
                      AUDIT_DETAIL_STATUS_CHOICES, AUDIT_RUN_STATUS_CHOICES, AUDIT_INVENTORY_CHOICES,
                      AUDIT_RUN_TYPE_CHOICES, AUDIT_STATUS_CHOICES)
 from services.models import WarehouseInventoryHistoric, BinInventoryHistoric
-from wms.models import WarehouseInventory, WarehouseInternalInventoryChange, InventoryType, InventoryState, BinInventory, BinInternalInventoryChange
+from wms.models import WarehouseInventory, WarehouseInternalInventoryChange, InventoryType, InventoryState, \
+    BinInventory, BinInternalInventoryChange
 from products.models import Product
 import datetime
 
@@ -15,7 +17,7 @@ info_logger = logging.getLogger('file-info')
 
 
 def index(request):
-    start_automated_inventory_audit()
+    return HttpResponse(start_automated_inventory_audit())
 
 
 def initialize_dict():
@@ -34,8 +36,12 @@ def run_warehouse_level_audit(audit_run):
 
     last_day_inventory = WarehouseInventoryHistoric.objects.filter(warehouse=audit_run.warehouse,
                                                                    archived_at__gte=prev_day,
-                                                                   archived_at__lte=audit_started).values('sku_id', 'inventory_type_id', 'inventory_state_id', 'quantity')
-    info_logger.debug("Audit run {} : Fetched last days snapshot, total rows {}" .format(audit_run, last_day_inventory.count()))
+                                                                   archived_at__lte=audit_started).values('sku_id',
+                                                                                                          'inventory_type_id',
+                                                                                                          'inventory_state_id',
+                                                                                                          'quantity')
+    info_logger.debug(
+        "Audit run {} : Fetched last days snapshot, total rows {}".format(audit_run, last_day_inventory.count()))
     for item in last_day_inventory:
         inventory_calculated[item['sku_id']][item['inventory_type_id']][item['inventory_state_id']] = item['quantity']
 
@@ -43,7 +49,8 @@ def run_warehouse_level_audit(audit_run):
                                                                             created_at__gte=prev_day,
                                                                             created_at__lte=audit_started)
 
-    info_logger.debug("Audit run {} : Fetched last days transactions, total rows {}".format(audit_run, last_day_transactions.count()))
+    info_logger.debug(
+        "Audit run {} : Fetched last days transactions, total rows {}".format(audit_run, last_day_transactions.count()))
 
     for tr in last_day_transactions:
         if isinstance(inventory_calculated[tr.sku.product_sku][tr.initial_type_id][tr.initial_stage_id], dict):
@@ -75,12 +82,19 @@ def run_bin_level_audit(audit_run):
     prev_day = audit_started.date() - datetime.timedelta(1)
     inventory_calculated = initialize_dict()
 
-    current_inventory = BinInventory.objects.filter(warehouse=audit_run.warehouse).values('sku_id', 'batch_id', 'bin_id', 'inventory_type_id', 'quantity')
+    current_inventory = BinInventory.objects.filter(warehouse=audit_run.warehouse).values('sku_id', 'batch_id',
+                                                                                          'bin_id', 'inventory_type_id',
+                                                                                          'quantity')
     last_day_inventory = BinInventoryHistoric.objects.filter(warehouse=audit_run.warehouse,
                                                              archived_at__gte=prev_day,
-                                                             archived_at__lte=audit_started).values('sku_id', 'batch_id', 'bin_id', 'inventory_type_id', 'quantity')
+                                                             archived_at__lte=audit_started).values('sku_id',
+                                                                                                    'batch_id',
+                                                                                                    'bin_id',
+                                                                                                    'inventory_type_id',
+                                                                                                    'quantity')
     for item in last_day_inventory:
-        inventory_calculated[item['sku_id']][item['batch_id']][item['bin_id']][item['inventory_type_id']] = item['quantity']
+        inventory_calculated[item['sku_id']][item['batch_id']][item['bin_id']][item['inventory_type_id']] = item[
+            'quantity']
 
     last_day_transactions = BinInternalInventoryChange.objects.filter(warehouse=audit_run.warehouse,
                                                                       created_at__gte=prev_day,
@@ -97,10 +111,11 @@ def run_bin_level_audit(audit_run):
         inventory_calculated[tr.sku][tr.batch_id][tr.final_bin_id][tr.final_type_id] += tr.quantity
 
     for item in current_inventory:
-        qty_calculated = inventory_calculated[item['sku_id']][item['batch_id']][item['bin_id']][item['inventory_type_id']]
-        status = AUDIT_RUN_STATUS_CHOICES.DIRTY
+        qty_calculated = inventory_calculated[item['sku_id']][item['batch_id']][item['bin_id']][
+            item['inventory_type_id']]
+        status = AUDIT_STATUS_CHOICES.DIRTY
         if item['quantity'] == qty_calculated:
-            status = AUDIT_RUN_STATUS_CHOICES.CLEAN
+            status = AUDIT_STATUS_CHOICES.CLEAN
         audit_run_item = AuditRunItems.objects.create(warehouse=audit_run.warehouse, audit_run=audit_run,
                                                       sku_id=item['sku_id'],
                                                       batch_id=item['batch_id'],
@@ -116,7 +131,9 @@ def run_bin_warehouse_integrated_audit(audit_run):
         warehouse=audit_run.warehouse).values('sku_id', 'inventory_type_id', 'quantity')
     for item in current_warehouse_inventory:
         bin_quantity = BinInventory.available_qty_with_inventory_type(audit_run.warehouse.id,
-                                                                      item['sku_id'], item['inventory_type_id'])
+                                                                      Product.objects.filter(
+                                                                          product_sku=item['sku_id']).last().id,
+                                                                      item['inventory_type_id'])
         status = AUDIT_STATUS_CHOICES.DIRTY
         if bin_quantity == item['quantity']:
             status = AUDIT_STATUS_CHOICES.CLEAN
@@ -130,31 +147,31 @@ def run_bin_warehouse_integrated_audit(audit_run):
 
 def run_audit(audit_run, inventory_choice):
     if inventory_choice == AUDIT_INVENTORY_CHOICES.WAREHOUSE:
-        return run_warehouse_level_audit(audit_run)
+        run_warehouse_level_audit(audit_run)
     if inventory_choice == AUDIT_INVENTORY_CHOICES.BIN:
-        return run_bin_level_audit(audit_run)
+        run_bin_level_audit(audit_run)
     if inventory_choice == AUDIT_INVENTORY_CHOICES.INTEGRATED:
-        return run_bin_warehouse_integrated_audit(audit_run)
-    return audit_run
+        run_bin_warehouse_integrated_audit(audit_run)
 
 
 def start_automated_inventory_audit():
-    audits_to_perform = AuditDetails.objects.filter(audit_inventory_type=AUDIT_RUN_TYPE_CHOICES.AUTOMATED,
+    audits_to_perform = AuditDetails.objects.filter(audit_type=AUDIT_RUN_TYPE_CHOICES.AUTOMATED,
                                                     status=AUDIT_DETAIL_STATUS_CHOICES.ACTIVE)
     for audit in audits_to_perform:
         audit_run = AuditRun.objects.filter(audit=audit, status=AUDIT_RUN_STATUS_CHOICES.IN_PROGRESS)
         if audit_run:
             info_logger.debug("Audit run already in progress for AuditDetails {} ".format(audit))
             continue
-        audit_run = AuditRun.objects.create(warehouse=audit.warehouse, audit=audit, status=AUDIT_RUN_STATUS_CHOICES.IN_PROGRESS)
+        audit_run = AuditRun.objects.create(warehouse=audit.warehouse, audit=audit,
+                                            status=AUDIT_RUN_STATUS_CHOICES.IN_PROGRESS)
         info_logger.debug("Audit run started AuditDetails {} at {} ".format(audit, audit_run.started_at))
         try:
-            audit_run = run_audit(audit_run, audit.audit_inventory_type)
+            run_audit(audit_run, audit.audit_inventory_type)
         except Exception as e:
             info_logger.error("Audit run aborted with Exception ")
-            audit_run.status = "aborted"
+            audit_run.status = AUDIT_RUN_STATUS_CHOICES.ABORTED
         else:
-            audit_run.status = "completed"
+            audit_run.status = AUDIT_RUN_STATUS_CHOICES.COMPLETED
             audit_run.completed_at = datetime.datetime.now()
         info_logger.debug("Audit run completed AuditDetails {} at {} ".format(audit, audit_run.completed_at))
         audit_run.save()
