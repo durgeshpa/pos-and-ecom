6be48ce83f4928bbd126942ad2e6a62e827efd6b
Deepti <deepti@gramfactory.com>
2020-10-12 12:48:40 +0530

    audit start : api end point

--- audit/api/v1/urls.py
+++ audit/api/v1/urls.py
@@ -4,6 +4,6 @@ from audit.api.v1.views import AuditListView, AuditStartView
 
 urlpatterns = [
     url(r'^audits/$', AuditListView.as_view(), name='audits'),
-    url(r'^audit-update/$', AuditStartView.as_view(), name='audits'),
+    url(r'^audit-start/$', AuditStartView.as_view(), name='audits'),
 
 ]
