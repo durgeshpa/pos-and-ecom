3d199d13ed96a927ea6fbc0c0b8b7961a3afeea0
Chandan <chandan@Chandans-MacBook-Pro.local>
2020-10-20 13:55:53 +0530

    [#ch_dev_fixes:-Order and Picklist mismatch issue done]

--- wms/admin.py
+++ wms/admin.py
@@ -668,7 +668,7 @@ class WarehouseInventoryAdmin(admin.ModelAdmin):
     list_select_related = ('warehouse', 'inventory_type', 'inventory_state', 'sku')
 
     readonly_fields = (
-    'warehouse', 'sku', 'inventory_type', 'inventory_state', 'in_stock', 'created_at', 'modified_at',)
+    'warehouse', 'sku', 'inventory_type', 'inventory_state', 'in_stock', 'created_at', 'modified_at', 'quantity',)
     search_fields = ('sku__product_sku',)
     list_filter = [Warehouse, SKUFilter, InventoryTypeFilter, InventoryStateFilter, ('created_at', DateTimeRangeFilter),
                    ('modified_at', DateTimeRangeFilter)]
