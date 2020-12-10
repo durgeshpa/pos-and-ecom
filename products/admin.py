310d3d1ffeae97e17ae1c50ea2ca6c5385a07674
Chandan <chandan@Chandans-MacBook-Pro.local>
2020-12-10 20:08:03 +0530

    [#es_index:-Product barcode issue resolved if batch id is not exist in GRN]

--- products/admin.py
+++ products/admin.py
@@ -494,6 +494,7 @@ class ParentProductAdmin(admin.ModelAdmin):
         ParentProductCategoryAdmin, ParentProductImageAdmin, ParentProductTaxMappingAdmin
     ]
     list_filter = [ParentCategorySearch, ParentBrandFilter, ParentIDFilter, 'status']
+    list_per_page = 50
     autocomplete_fields = ['product_hsn', 'parent_brand']
 
     def product_gst(self, obj):
@@ -821,6 +822,7 @@ class ProductAdmin(admin.ModelAdmin, ExportCsvMixin):
     search_fields = ['product_name', 'id']
     # list_filter = [BrandFilter, CategorySearch, ProductSearch, 'status']
     list_filter = [CategorySearch, ProductBrandSearch, ProductSearch, ChildParentIDFilter, 'status']
+    list_per_page = 50
     # prepopulated_fields = {'product_slug': ('product_name',)}
     # inlines = [
     #     ProductCategoryAdmin, ProductOptionAdmin,
