65a0460a67fdf46d3addf7411e814c55f2daeb3c
ShalineeB <shalinee@gramfactory.com>
2020-12-23 11:47:53 +0530

    modified models

--- products/models.py
+++ products/models.py
@@ -782,14 +782,9 @@ class ProductPriceCSV(models.Model):
 class ProductVendorMapping(models.Model):
     vendor = models.ForeignKey(Vendor,related_name='vendor_brand_mapping',on_delete=models.CASCADE)
     product = models.ForeignKey(Product,related_name='product_vendor_mapping',on_delete=models.CASCADE)
-<<<<<<< HEAD
     product_price = models.FloatField(verbose_name='Brand to Gram Price (Per Piece)',null=True,blank=True) #(Per piece)
     product_price_pack = models.FloatField(verbose_name='Brand to Gram Price (Per Pack)',null=True,blank=True)
     brand_to_gram_price_unit = models.CharField(max_length = 100 ,default="Per Piece")
-=======
-    product_price = models.FloatField(verbose_name='Brand to Gram Price ') #(Per piece)
-    # product_price_pack = models.FloatField(verbose_name='Brand to Gram Price (Per pack)')
->>>>>>> dev
     product_mrp = models.FloatField(null=True,blank=True)
     case_size = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
