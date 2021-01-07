2dfb4339ec5b537240021c8e0838fdc3e5d08f76
Gramfactory <yash@gramfactory.com>
2021-01-08 01:25:30 +0530

    [#Bulk Order code finalize for capping and made some changs in add to cart, reserve and creater order]

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -582,11 +582,11 @@ class AddToCart(APIView):
                             cart_mapping, _ = CartProductMapping.objects.get_or_create(cart=cart, cart_product=product)
                             if (capping.capping_qty - ordered_qty) > 0:
                                 if (capping.capping_qty - ordered_qty) < 0:
-                                    cart_mapping.capping_error_msg = ['The Purchase Limit of the Product is %s' % (
-                                            0)]
+                                    cart_mapping.capping_error_msg = 'The Purchase Limit of the Product is %s' % (
+                                            capping.capping_qty - ordered_qty)
                                 else:
-                                    cart_mapping.capping_error_msg = ['The Purchase Limit of the Product is %s' % (
-                                            capping.capping_qty - ordered_qty)]
+                                    cart_mapping.capping_error_msg = 'The Purchase Limit of the Product is %s' % (
+                                            capping.capping_qty - ordered_qty)
                             else:
                                 cart_mapping.capping_error_msg = ['You have already exceeded the purchase limit of this product']
                                 CartProductMapping.objects.filter(cart=cart, cart_product=product).delete()
