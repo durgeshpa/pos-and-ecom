cb5d3d358b7820a167a9016c55c6f6626a640f71
ShalineeB <shalinee@gramfactory.com>
2021-01-11 17:57:39 +0530

    added otp functionality

--- marketing/views.py
+++ marketing/views.py
@@ -95,14 +95,14 @@ class Registrations(GenericAPIView):
                     updated_values = {'referral_code': user_referral_code}
                     obj, created = MLMUser.objects.update_or_create(phone_number=phone_number, defaults=updated_values)
                     obj.save()
-                msg = requests.post('http://127.0.0.1:8000/marketing/validate/', data=request.POST)
-                return Response(msg, status=status.HTTP_200_OK)
+                msg = ValidateOTP(data=request.POST)
+                return Response(msg.data, status=status.HTTP_200_OK)
             else:
                 user_referral_code = Referral.generate_unique_referral_code()
                 user = MLMUser.objects.create(phone_number=phone_number, referral_code=user_referral_code)
                 user.save()
                 Referral.store_parent_referral_user(referral_code, user_referral_code)
-                msg = requests.post('http://127.0.0.1:8000/marketing/validate/', data=request.POST)
+                msg = ValidateOTP(data=request.POST)
                 return Response(msg, status=status.HTTP_200_OK)
         except Exception:
             return Response(Exception, status=status.HTTP_403_FORBIDDEN)
@@ -117,7 +117,6 @@ class Login(GenericAPIView):
             data = request.data
             phone_number = data.get('phone_number')
             otp = data.get('otp')
-
             if phone_number is None:
                 return Response({'error': 'Please provide phone_number '},
                                 status=HTTP_400_BAD_REQUEST)
@@ -135,129 +134,99 @@ class Login(GenericAPIView):
                     updated_values = {'referral_code': user_referral_code}
                     obj, created = MLMUser.objects.update_or_create(phone_number=phone_number, defaults=updated_values)
                     obj.save()
-                msg = requests.post('http://127.0.0.1:8000/marketing/validate/', data=request.POST)
-                return Response(msg, status=status.HTTP_200_OK)
+                msg = ValidateOTP(data=request.POST)
+                return Response(msg.data, status=status.HTTP_200_OK)
             else:
                 user_referral_code = Referral.generate_unique_referral_code()
                 user = MLMUser.objects.create(phone_number=phone_number, referral_code=user_referral_code)
                 user.save()
-                msg = requests.post('http://127.0.0.1:8000/marketing/validate/', data=request.POST)
-                return Response(msg, status=status.HTTP_200_OK)
+                msg = ValidateOTP(data=request.POST)
+                return Response(msg.data, status=status.HTTP_200_OK)
         except Exception:
             return Response(Exception, status=status.HTTP_403_FORBIDDEN)
 
-class ValidateOTP(CreateAPIView):
-    permission_classes = (AllowAny,)
-    queryset = PhoneOTP.objects.all()
-    serializer_class = PhoneOTPValidateSerializer
+def ValidateOTP(data):
+    number = data.get("phone_number")
+    otp = data.get("otp")
+    user = PhoneOTP.objects.filter(phone_number=number)
+    if user.exists():
+        user = user.last()
+        msg, status_code = verify(otp, user)
+        return Response(msg)
+    else:
+        msg = {'is_success': False,
+               'message': VALIDATION_ERROR_MESSAGES['USER_NOT_EXIST'],
+               'response_data': None}
+        return Response(msg,
+                        status=status.HTTP_406_NOT_ACCEPTABLE
+                        )
+def expired(user):
+    current_time = datetime.datetime.now()
+    expiry_time = datetime.timedelta(seconds=user.expires_in)
+    created_time = user.created_at
+    if current_time - created_time <= expiry_time:
+        return False
+    else:
+        return True
 
-    def post(self, request, format=None):
-        """
-            This method will validate otp with given phone number.
-        """
-        serializer = self.serializer_class(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            number = request.data.get("phone_number")
-            otp = request.data.get("otp")
-            user = PhoneOTP.objects.filter(phone_number=number)
+def max_attempts(user, attempts):
+    if user.attempts < getattr(settings, 'OTP_ATTEMPTS', attempts):
+        return False
+    else:
+        return True
 
-            if user.exists():
-                user = user.last()
-                msg, status_code = self.verify(otp, user)
-                return Response(msg,
-                                status=status_code
-                                )
-            else:
-                msg = {'is_success': False,
-                       'message': VALIDATION_ERROR_MESSAGES['USER_NOT_EXIST'],
-                       'response_data': None}
-                return Response(msg,
-                                status=status.HTTP_406_NOT_ACCEPTABLE
-                                )
-        else:
-            errors = []
-            for field in serializer.errors:
-                for error in serializer.errors[field]:
-                    if 'non_field_errors' in field:
-                        result = error
-                    else:
-                        result = ''.join('{} : {}'.format(field, error))
-                    errors.append(result)
-            msg = {'is_success': False,
-                   'message': [error for error in errors],
-                   'response_data': None}
-            return Response(msg,
-                            status=status.HTTP_406_NOT_ACCEPTABLE)
-
-    def expired(self, user):
-        current_time = datetime.datetime.now()
-        expiry_time = datetime.timedelta(seconds=user.expires_in)
-        created_time = user.created_at
-        if current_time - created_time <= expiry_time:
-            return False
-        else:
-            return True
-
-    def max_attempts(self, user, attempts):
-        if user.attempts < getattr(settings, 'OTP_ATTEMPTS', attempts):
-            return False
-        else:
-            return True
-
-    def verify(self, otp, user):
-        if otp == user.otp:
-            if not self.expired(user) and not self.max_attempts(user, 5):
-                user.is_verified = 1
-                user.save()
-
-                id = MLMUser.objects.get(phone_number=user.phone_number).id
-                user_obj = MLMUser.objects.get(pk=id)
-                user_obj.status = 1
-                user_obj.save()
-                token = user_obj.id = uuid.uuid4()
-                msg = {'phone_number': user_obj.phone_number,
-                       'token': token,
-                       'referral_code': user_obj.referral_code,
-                       'name': user_obj.name,
-                       'email_id': user_obj.email
-                       }
-                status_code = status.HTTP_200_OK
-                return msg, status_code
-            elif self.max_attempts(user, 5):
-                error_msg = VALIDATION_ERROR_MESSAGES['OTP_ATTEMPTS_EXCEEDED']
-                status_code = status.HTTP_406_NOT_ACCEPTABLE
-                revoke = RevokeOTP(user.phone_number, error_msg)
-                msg = revoke.update()
-                return msg, status_code
-            elif self.expired(user):
-                error_msg = VALIDATION_ERROR_MESSAGES['OTP_EXPIRED']
-                status_code = status.HTTP_406_NOT_ACCEPTABLE
-                revoke = RevokeOTP(user.phone_number, error_msg)
-                msg = revoke.update()
-                return msg, status_code
-
-        else:
-            if self.max_attempts(user, 5):
-                error_msg = VALIDATION_ERROR_MESSAGES['OTP_ATTEMPTS_EXCEEDED']
-                status_code = status.HTTP_406_NOT_ACCEPTABLE
-                revoke = RevokeOTP(user.phone_number, error_msg)
-                msg = revoke.update()
-                return msg, status_code
-            elif self.expired(user):
-                error_msg = VALIDATION_ERROR_MESSAGES['OTP_EXPIRED']
-                status_code = status.HTTP_406_NOT_ACCEPTABLE
-                revoke = RevokeOTP(user.phone_number, error_msg)
-                msg = revoke.update()
-                return msg, status_code
-            user.attempts += 1
+def verify(otp, user):
+    if otp == user.otp:
+        if not expired(user) and not max_attempts(user, 5):
+            user.is_verified = 1
             user.save()
-            msg = {'is_success': False,
-                   'message': VALIDATION_ERROR_MESSAGES['OTP_NOT_MATCHED'],
-                   'response_data': None}
-            status_code = status.HTTP_406_NOT_ACCEPTABLE
+            id = MLMUser.objects.get(phone_number=user.phone_number).id
+            user_obj = MLMUser.objects.get(pk=id)
+            user_obj.status = 1
+            user_obj.save()
+            token = user_obj.id = uuid.uuid4()
+            msg = {'phone_number': user_obj.phone_number,
+                   'token': token,
+                   'referral_code': user_obj.referral_code,
+                   'name': user_obj.name,
+                   'email_id': user_obj.email
+                   }
+            status_code = status.HTTP_200_OK
             return msg, status_code
+        elif max_attempts(user, 5):
+            error_msg = VALIDATION_ERROR_MESSAGES['OTP_ATTEMPTS_EXCEEDED']
+            status_code = status.HTTP_406_NOT_ACCEPTABLE
+            revoke = RevokeOTP(user.phone_number, error_msg)
+            msg = revoke.update()
+            return msg, status_code
+        elif expired(user):
+            error_msg = VALIDATION_ERROR_MESSAGES['OTP_EXPIRED']
+            status_code = status.HTTP_406_NOT_ACCEPTABLE
+            revoke = RevokeOTP(user.phone_number, error_msg)
+            msg = revoke.update()
+            return msg, status_code
+
+    else:
+        if max_attempts(user, 5):
+            error_msg = VALIDATION_ERROR_MESSAGES['OTP_ATTEMPTS_EXCEEDED']
+            status_code = status.HTTP_406_NOT_ACCEPTABLE
+            revoke = RevokeOTP(user.phone_number, error_msg)
+            msg = revoke.update()
+            return msg, status_code
+        elif expired(user):
+            error_msg = VALIDATION_ERROR_MESSAGES['OTP_EXPIRED']
+            status_code = status.HTTP_406_NOT_ACCEPTABLE
+            revoke = RevokeOTP(user.phone_number, error_msg)
+            msg = revoke.update()
+            return msg, status_code
+        user.attempts += 1
+        user.save()
+        msg = {'is_success': False,
+               'message': VALIDATION_ERROR_MESSAGES['OTP_NOT_MATCHED'],
+               'response_data': None}
+        status_code = status.HTTP_406_NOT_ACCEPTABLE
+        return msg, status_code
+
 
 class RevokeOTP(object):
     """Revoke OTP if epired or attempts exceeds"""
@@ -279,6 +248,6 @@ class RevokeOTP(object):
         phone_otp.last_otp = timezone.now()
         phone_otp.save()
         msg = {'is_success': True,
-               'message': "message sent",
+               'message': "you entered otp is expired,new otp sent in your registered number",
                'response_data': None}
         return msg
