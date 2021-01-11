006628729fb4f951545ef1f0a58b6f3596c96ccd
ShalineeB <shalinee@gramfactory.com>
2021-01-11 13:43:27 +0530

    added otp functionality

--- marketing/admin.py
+++ marketing/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import MLMUser, Referral
+from .models import MLMUser, Referral, PhoneOTP
+# Register your models here.
+
+class PhoneOTPAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'otp', 'is_verified', 'attempts', 'expires_in', 'created_at', 'last_otp', 'resend_in')
+    search_fields = ('phone_number',)
+    ordering = ['-created_at']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class MLMUserAdmin(admin.ModelAdmin):
+    model = MLMUser
+    list_display = ['phone_number', 'name', 'email' ]
 
 
 class ReferralAdmin(admin.ModelAdmin):
@@ -7,5 +27,7 @@ class ReferralAdmin(admin.ModelAdmin):
     list_display = ('referral_to', 'referral_by', 'created_at')
     fields = ('referral_to', 'referral_by')
 
+
+admin.site.register(MLMUser, MLMUserAdmin)
+admin.site.register(PhoneOTP, PhoneOTPAdmin)
 admin.site.register(Referral, ReferralAdmin)
-admin.site.register(MLMUser)
---
+++ marketing/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import MLMUser, PhoneOTP
+
+class PhoneOTPValidateSerializer(serializers.ModelSerializer):
+    """
+    validate the otp send to number
+    """
+    class Meta:
+        model = PhoneOTP
+        fields = (
+            'phone_number',
+            'otp'
+        )
+
+class SendSmsOTPSerializer(serializers.ModelSerializer):
+    """
+    Send OTP SMS to number
+    """
+    class Meta:
+        model = PhoneOTP
+        fields = (
+            'phone_number',
+        )
