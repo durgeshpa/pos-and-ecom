e5fdff6e23546b50514a4b5ce980f8b16280e849
monali <monali@gramfactory.com>
2021-01-24 22:41:41 +0530

    daily rewards notify

--- retailer_backend/settings.py
+++ retailer_backend/settings.py
@@ -378,8 +378,8 @@ CRONJOBS = [
     ('*/5 * * * *', 'audit.cron.create_picklist_cron'),
     ('0 */1 * * *', 'audit.cron.release_products_from_audit'),
     ('00 2 * * *', 'franchise.crons.cron.franchise_sales_returns_inventory'),
-    ('00 00 * * *', 'marketing.crons.hdpos_users.fetch_hdpos_users_cron'),
-    ('00 00 * * *', 'marketing.crons.rewards_sms.rewards_notify_users'),
+    ('00 3 * * *', 'marketing.crons.hdpos_users.fetch_hdpos_users_cron'),
+    ('00 3 * * *', 'marketing.crons.rewards_sms.rewards_notify_users'),
 ]
 
 INTERNAL_IPS = ['127.0.0.1', 'localhost']
