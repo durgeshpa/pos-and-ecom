547c0db4a70c0e0abb11421f5ce8be567145626a
Kartikey Parashar <kartikey@gramfactory.com>
2021-01-28 12:35:53 +0530

    Resolved the issues

--- products/master_data.py
+++ products/master_data.py
@@ -155,16 +155,26 @@ class UploadMasterData(object):
                                 tax = Tax.objects.filter(tax_name=row['tax_1(gst)'])
                                 ParentProductTaxMapping.objects.filter(parent_product=parent_product[0].id).update(
                                     tax=tax[0])
-                                # product = Product.objects.filter(product_sku=row['sku_id'])
-                                # ProductTaxMapping.objects.filter(product=product[0].id).update(tax=tax[0])
+                                if 'sku_id' in row.keys():
+                                    if row['sku_id'] != '':
+                                        product = Product.objects.filter(product_sku=row['sku_id'])
+                                        ProductTaxMapping.objects.filter(product=product[0].id).update(tax=tax[0])
                             if col == 'tax_2(cess)':
                                 tax = Tax.objects.filter(tax_name=row['tax_2(cess)'])
                                 ParentProductTaxMapping.objects.filter(parent_product=parent_product[0].id).update(
                                     tax=tax[0])
+                                if 'sku_id' in row.keys():
+                                    if row['sku_id'] != '':
+                                        product = Product.objects.filter(product_sku=row['sku_id'])
+                                        ProductTaxMapping.objects.filter(product=product[0].id).update(tax=tax[0])
                             if col == 'tax_3(surcharge)':
                                 tax = Tax.objects.filter(tax_name=row['tax_3(surcharge)'])
                                 ParentProductTaxMapping.objects.filter(parent_product=parent_product[0].id).update(
                                     tax=tax[0])
+                                if 'sku_id' in row.keys():
+                                    if row['sku_id'] != '':
+                                        product = Product.objects.filter(product_sku=row['sku_id'])
+                                        ProductTaxMapping.objects.filter(product=product[0].id).update(tax=tax[0])
                             if col == 'brand_case_size':
                                 ParentProduct.objects.filter(parent_id=row['parent_id']).update\
                                     (brand_case_size=row['brand_case_size'])
@@ -225,47 +235,44 @@ class UploadMasterData(object):
             child_data = []
             for row in excel_file_data_list:
                 row_num += 1
-                if not row['status'] == 'deactivated':
-                    count += 1
-                    try:
-                        Product.objects.filter(product_sku=row['sku_id']).update(product_name=row['sku_name'],
-                                                                                 status='active')
-                        fields = ['ean', 'mrp', 'weight_unit', 'weight_value']
-                        for col in fields:
-                            if col in row.keys():
-                                if row[col] != '':
-                                    pass
-                                else:
-                                    fields.remove(col)
+                count += 1
+                try:
+                    Product.objects.filter(product_sku=row['sku_id']).update(product_name=row['sku_name'],
+                                                                             status=row['status'])
+                    fields = ['ean', 'mrp', 'weight_unit', 'weight_value']
+                    for col in fields:
+                        if col in row.keys():
+                            if row[col] != '':
+                                pass
                             else:
                                 fields.remove(col)
-                        for col in fields:
-                            if col == 'ean':
-                                Product.objects.filter(product_sku=row['sku_id']).update(product_ean_code=row['ean'])
-                            if col == 'mrp':
-                                Product.objects.filter(product_sku=row['sku_id']).update(product_mrp=row['mrp'])
-                            if col == 'weight_value':
-                                Product.objects.filter(product_sku=row['sku_id']).update(weight_value=row['weight_value'])
-                        if 'repackaging_type' in row.keys():
-                            if row['repackaging_type'] == 'destination':
-                                Product.objects.filter(product_sku=row['source_sku_id']).update(product_name=row['source_sku_name'])
-                                destination_product = Product.objects.filter(product_sku=row['source_sku_id'])
-                                DestinationRepackagingCostMapping.objects.filter(destination=destination_product[0].id)\
-                                                                         .update(raw_material=row['raw_material'],
-                                                                                 wastage=row['wastage'],
-                                                                                 fumigation=row['fumigation'],
-                                                                                 label_printing=row['label_printing'],
-                                                                                 packing_labour=row['packing_labour'],
-                                                                                 primary_pm_cost=row['primary_pm_cost'],
-                                                                                 secondary_pm_cost=row['secondary_pm_cost'],
-                                                                                 final_fg_cost=row['final_fg_cost'],
-                                                                                 conversion_cost=row['conversion_cost'])
-                            else:
-                                continue
-                    except Exception as e:
-                        child_data.append(str(row_num) + ' ' + str(e))
-                else:
-                    continue
+                        else:
+                            fields.remove(col)
+                    for col in fields:
+                        if col == 'ean':
+                            Product.objects.filter(product_sku=row['sku_id']).update(product_ean_code=row['ean'])
+                        if col == 'mrp':
+                            Product.objects.filter(product_sku=row['sku_id']).update(product_mrp=row['mrp'])
+                        if col == 'weight_value':
+                            Product.objects.filter(product_sku=row['sku_id']).update(weight_value=row['weight_value'])
+                    if 'repackaging_type' in row.keys():
+                        if row['repackaging_type'] == 'destination':
+                            Product.objects.filter(product_sku=row['source_sku_id']).update(product_name=row['source_sku_name'])
+                            destination_product = Product.objects.filter(product_sku=row['source_sku_id'])
+                            DestinationRepackagingCostMapping.objects.filter(destination=destination_product[0].id)\
+                                                                     .update(raw_material=row['raw_material'],
+                                                                             wastage=row['wastage'],
+                                                                             fumigation=row['fumigation'],
+                                                                             label_printing=row['label_printing'],
+                                                                             packing_labour=row['packing_labour'],
+                                                                             primary_pm_cost=row['primary_pm_cost'],
+                                                                             secondary_pm_cost=row['secondary_pm_cost'],
+                                                                             final_fg_cost=row['final_fg_cost'],
+                                                                             conversion_cost=row['conversion_cost'])
+                        else:
+                            continue
+                except Exception as e:
+                    child_data.append(str(row_num) + ' ' + str(e))
             info_logger.info("Total row executed :" + str(count))
             info_logger.info("Some error found in these row while working with Child data Functionality:" + str(child_data))
             info_logger.info("Script Complete to set the Child data")
