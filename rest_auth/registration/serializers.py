e4cd662a88fc90a2dea42c1c9fb3b2666cbcccd0
monali <monali@gramfactory.com>
2021-02-09 13:50:16 +0530

    register serializer

--- rest_auth/registration/serializers.py
+++ rest_auth/registration/serializers.py
@@ -265,6 +265,11 @@ class MlmRegisterSerializer(serializers.Serializer):
     def get_cleaned_data(self):
         return {
             'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'email': self.validated_data.get('email', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
+            'imei_no': self.validated_data.get('imei_no', '')
         }
 
     def save(self, request):
