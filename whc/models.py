a51151849e9f2f50954ffa398abc9f51aea0f99f
ShalineeB <shalinee@gramfactory.com>
2021-02-11 11:02:11 +0530

    Refactored AutoPutAway

--- whc/models.py
+++ whc/models.py
@@ -1,4 +1,3 @@
-from datetime import timezone
 from datetime import datetime
 from django.db import models
 
@@ -15,7 +14,7 @@ from shops.models import Shop
 class BaseQuerySet(query.QuerySet):
 
     def update(self, **kwargs):
-        kwargs['modified_at'] = datetime.now()
+        kwargs['updated_at'] = datetime.now()
         super().update(**kwargs)
 
 class Manager(manager.BaseManager.from_queryset(BaseQuerySet)):
@@ -55,5 +54,3 @@ class AutoOrderProcessing(BaseTimestampModel):
     retailer_shop = models.ForeignKey(Shop, related_name='auto_processing_shop_entries', on_delete=models.CASCADE, null=True)
     cart = models.OneToOneField(Cart, related_name='auto_processing_carts', on_delete=models.CASCADE, null=True)
     order = models.OneToOneField(Order, related_name='auto_processing_orders', on_delete=models.CASCADE, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
