cca713227f21f0d62c1ac7b9ac9121fd27468998
Deepti <deepti@gramfactory.com>
2021-02-15 11:59:40 +0530

    code merge

--- whc/views.py
+++ whc/views.py
@@ -382,9 +382,8 @@ def process_auto_order():
         info_logger.info("process_auto_order|no shop found with id -{}".format(buyer_shop))
         return
     wh_consolidation_vendor = get_config('wh_consolidation_vendor')
-
     if wh_consolidation_vendor is None:
-        info_logger.info("process_auto_order|wh_consolidation_destination is not defined ")
+        info_logger.info("process_auto_order|wh_consolidation_vendor is not defined ")
         return
 
     supplier = Vendor.objects.filter(pk=wh_consolidation_vendor).last()
@@ -424,9 +423,6 @@ def process_auto_order():
                 next_state = process_next(order_processor, entry)
                 if current_state == next_state:
                     info_logger.info("process_auto_order|GRN ID-{}, could not move ahead".format(entry.grn_id))
-                    break
-                if next_state == AutoOrderProcessing.ORDER_PROCESSING_STATUS.DELIVERED:
-                    info_logger.info("process_auto_order|GRN ID-{}, moved to delivered state".format(entry.grn_id))
                     break
                 info_logger.info("process_auto_order|GRN ID-{}, current state-{}".format(entry.grn_id, next_state))
         except Exception as e:
