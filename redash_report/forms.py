74f4620fd3a186aeb6028d521700379b98cd11dc
ShalineeB <shalinee@gramfactory.com>
2021-02-22 18:12:13 +0530

    Refactored SendMail Logic

--- redash_report/forms.py
+++ redash_report/forms.py
@@ -6,13 +6,17 @@ from django.core.exceptions import ValidationError
 class ScheduledRedashForm(forms.ModelForm):
     class Meta:
         model = RedashScheduledReport
-        fields = "__all__"
+        fields = ['csv_url', 'recipients', 'subject', 'body', 'schedule']
+        help_texts = {'recipients': "use commas as separators for multiple email recipients", }
 
     def clean_recipients(self):
         # email validation
-        data = self.cleaned_data['recipients']
-        if "@gramfactory.com" not in data:
-            raise ValidationError("Only gramfactory.com email addresses allowed")
+        recipients = self.cleaned_data['recipients']
+        emails = recipients.split(',')
+        for email in emails:
+            if "@gramfactory.com" not in email:
+                raise ValidationError("Only gramfactory.com email addresses allowed")
 
-        return data
+        return recipients
+
 
