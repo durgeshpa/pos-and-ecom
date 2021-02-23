c11b42cfe2f48105c9a15e086633375877d7e81e
Kartikey Parashar <kartikey@gramfactory.com>
2021-02-23 14:14:08 +0530

    added common_func file for pos

---
+++ pos/common_functions.py
@@ -0,0 +1,10 @@
+from pos.models import RetailerProduct
+
+
+class RetailerProductCls(object):
+
+    @classmethod
+    def create_retailer_product(cls, shop_id, sku, name, mrp, selling_price, linked_product_id, sku_type, description):
+        RetailerProduct.objects.create(shop_id=shop_id, sku=sku, name=name, linked_product_id=linked_product_id,
+                                       mrp=mrp, sku_type=sku_type, selling_price=selling_price, description=description)
+
