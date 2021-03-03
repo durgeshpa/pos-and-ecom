d45cd438d3edc855e384b8186296d2bcb40947d8
monali <monali@gramfactory.com>
2021-03-03 13:05:41 +0530

    search done

--- pos/admin.py
+++ pos/admin.py
@@ -5,8 +5,10 @@ from pos.models import RetailerProduct, RetailerProductImage
 # Register your models here.
 
 class RetailerProductAdmin(admin.ModelAdmin):
-    list_display = ('shop', 'sku', 'name', 'mrp', 'selling_price', 'linked_product', 'description', 'sku_type', 'status', 'created_at', 'modified_at')
-    fields = ('shop', 'linked_product', 'sku', 'name', 'mrp', 'selling_price', 'description', 'sku_type', 'status', 'created_at', 'modified_at')
+    list_display = ('shop', 'sku', 'name', 'mrp', 'selling_price', 'product_ean_code', 'linked_product', 'description',
+                    'sku_type', 'status', 'created_at', 'modified_at')
+    fields = ('shop', 'linked_product', 'sku', 'name', 'mrp', 'selling_price', 'product_ean_code',
+              'description', 'sku_type', 'status', 'created_at', 'modified_at')
     readonly_fields = ('shop', 'linked_product', 'sku_type', 'created_at', 'modified_at')
     list_per_page = 50
 
