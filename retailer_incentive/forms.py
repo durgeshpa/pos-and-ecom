4d3e1302efae99073d02a799cd7dbb5360c466e0
Deepti <deepti@gramfactory.com>
2021-03-04 18:19:38 +0530

    retailer incentive priority

--- retailer_incentive/forms.py
+++ retailer_incentive/forms.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ValidationError
 from tempus_dominus.widgets import DatePicker
 
 from retailer_incentive.models import Scheme, SchemeSlab, SchemeShopMapping
+from retailer_incentive.utils import get_active_mappings
 from shops.models import Shop
 
 
@@ -48,6 +49,18 @@ class SchemeShopMappingCreationForm(forms.ModelForm):
     scheme = forms.ModelChoiceField(queryset=Scheme.objects.all())
     shop = forms.ModelChoiceField(queryset=shop_choice,
                                   widget=autocomplete.ModelSelect2(url='shop-autocomplete'))
+
+    def clean(self):
+        data = self.cleaned_data
+        shop = data['shop']
+        active_mappings = get_active_mappings(shop.id)
+        if active_mappings.count() >= 2:
+            raise ValidationError("Shop Id - {} already has 2 active mappings".format(shop.id))
+        existing_active_mapping = active_mappings.last()
+        if existing_active_mapping and existing_active_mapping.priority == data['priority']:
+            raise ValidationError("Shop Id - {} already has an active {} mappings"
+                                  .format(shop.id, SchemeShopMapping.PRIORITY_CHOICE[data['priority']]))
+
 
     class Meta:
         model = SchemeShopMapping
