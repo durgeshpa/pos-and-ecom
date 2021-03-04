244a78b074eb5f1c85573a8c461a81b5fb8f9160
Deepti <deepti@gramfactory.com>
2021-03-04 13:42:51 +0530

    retailer incentive admin

--- retailer_incentive/api/v1/urls.py
+++ retailer_incentive/api/v1/urls.py
@@ -1,22 +1,11 @@
 # basic URL Configurations
 from django.conf.urls import url
-from django.urls import include, path
-# import routers
-from rest_framework import routers
 
-# import everything from views
-from .views import *
-
-# define the router
-router = routers.DefaultRouter()
-
-# define the router path and viewset to be used
-# router.register(r'scheme', ShopSchemeMappingViewSet)
+from .views import ShopSchemeMappingView, ShopPurchaseMatrix, ShopUserMappingView
 
 # specify URL Path for rest_framework
 urlpatterns = [
-	# path('', include(router.urls))
-	url(r'^scheme/$', ShopSchemeMappingView.as_view(), name='scheme'),
+    url(r'^scheme/$', ShopSchemeMappingView.as_view(), name='scheme'),
     url(r'^purchase-matrix/$', ShopPurchaseMatrix.as_view(), name='purchase-matrix'),
     url(r'^contact/$', ShopUserMappingView.as_view(), name='contact'),
 ]
