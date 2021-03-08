11cbe543af05bcaa0ff6076197cda615821e409b
ShalineeB <shalinee@gramfactory.com>
2021-03-08 20:23:13 +0530

    added POS_coupon add/edit fuctionality

--- pos/urls.py
+++ pos/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
-from pos.views import CatalogueProductCreation, OfferCreation
+from pos.views import CatalogueProductCreation, CouponOfferCreation
 
 urlpatterns = [
     url(r'^catalogue_product/', CatalogueProductCreation.as_view(), name='catalogue_product'),
-    url(r'^catalogue_offers/', OfferCreation.as_view(), name='catalogue_offers'),
+    url(r'^catalogue_offers/', CouponOfferCreation.as_view(), name='catalogue_offers'),
      url(r'^api/', include('pos.api.urls')),
 ]
