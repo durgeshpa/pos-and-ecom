f400743b2231659f062b1aea22cc412e61b2ec4a
ShalineeB <shalinee@gramfactory.com>
2021-03-11 14:35:18 +0530

    Refactored Code

--- pos/urls.py
+++ pos/urls.py
@@ -4,7 +4,7 @@ from pos.views import CatalogueProductCreation, CouponOfferCreation, RetailerPro
 urlpatterns = [
     url(r'^catalogue_product/', CatalogueProductCreation.as_view(), name='catalogue_product'),
     url(r'^retailer-product-autocomplete/', RetailerProductShopAutocomplete.as_view(), name='retailer-product-autocomplete'),
-    url(r'^cart_product_offers/', CouponOfferCreation.as_view(), name='cart_product_offers'),
+    url(r'^cart_catalogue_offers/', CouponOfferCreation.as_view(), name='cart_catalogue_offers'),
     url(r'^api/', include('pos.api.urls')),
 
 ]
