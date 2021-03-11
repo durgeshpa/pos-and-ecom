72c1afbaeac9ba02152daa08caec782371b61e04
ShalineeB <shalinee@gramfactory.com>
2021-03-11 15:00:09 +0530

    Refactored Code

--- pos/urls.py
+++ pos/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import include, url
 from pos.views import CatalogueProductCreation, CouponOfferCreation, RetailerProductShopAutocomplete
 
 urlpatterns = [
-    url(r'^catalogue_product/', CatalogueProductCreation.as_view(), name='catalogue_product'),
+    url(r'^catalogue-product/', CatalogueProductCreation.as_view(), name='catalogue-product'),
     url(r'^retailer-product-autocomplete/', RetailerProductShopAutocomplete.as_view(), name='retailer-product-autocomplete'),
-    url(r'^cart_catalogue_offers/', CouponOfferCreation.as_view(), name='cart_catalogue_offers'),
+    url(r'^offers/', CouponOfferCreation.as_view(), name='offers'),
     url(r'^api/', include('pos.api.urls')),
 
 ]
