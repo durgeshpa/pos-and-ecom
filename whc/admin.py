5ea849946d7af7011ff7e9cd9a3e22af4e290203
Gramfactory <yash@gramfactory.com>
2021-03-21 22:12:26 +0530

    [#ch_report_auto_stock_transfer:-Stock transfer report]

--- whc/admin.py
+++ whc/admin.py
@@ -5,37 +5,51 @@ from io import StringIO
 # django imports
 from django.contrib import admin
 from django.http import HttpResponse
-
-
+from rangefilter.filter import DateTimeRangeFilter
 # app imports
 from .models import AutoOrderProcessing
 
+# log info
 info_logger = logging.getLogger('file-info')
 error_logger = logging.getLogger('file-error')
 debug_logger = logging.getLogger('file-debug')
 
+
 # Register your models here.
 class AutoOrderProcessingAdmin(admin.ModelAdmin):
     actions = ['download_csv_for_auto_order']
-    list_display = ('source_po', 'auto_po', 'grn_warehouse', 'retailer_shop',)
+    list_display = ('invoice_number', 'invoice_date', 'source_po', 'grn', 'auto_po', 'order', 'cart',  'grn_warehouse',
+                    'retailer_shop', 'created_at', 'updated_at',)
+    list_filter = [('created_at', DateTimeRangeFilter)]
     list_per_page = 50
 
-    def invoice_number(self, obj):
+    @staticmethod
+    def invoice_number(obj):
+        """
+        auto order processing object
+        """
         return obj.cart.rt_order_cart_mapping.rt_order_order_product.all()[0].invoice.invoice_no
 
-    def invoice_date(self, obj):
+    @staticmethod
+    def invoice_date(obj):
+        """
+        auto order processing object
+        """
         return obj.cart.rt_order_cart_mapping.rt_order_order_product.all()[0].invoice.created_at
 
     def get_queryset(self, request):
+        """
+        request
+        """
         qs = super(AutoOrderProcessingAdmin, self).get_queryset(request)
-        return qs.filter(state=14).order_by('-auto_po').distinct('auto_po').select_related()
+        return qs.filter(state=14)
 
     def download_csv_for_auto_order(self, request, queryset):
         """
-        :param self:
-        :param request:
-        :param queryset:
-        :return:
+        :param self:self initialization
+        :param request: request object
+        :param queryset: auto order process
+        :return: csv file
         """
         f = StringIO()
         writer = csv.writer(f)
@@ -44,39 +58,66 @@ class AutoOrderProcessingAdmin(admin.ModelAdmin):
                          "GRN Number(GFDN)", "GRN Date", "PO Number(GFDN)", "SKU ID", "Product Name", "GST Rate",
                          "Delivered Qty(GRN)", "Buying Price", "Amount (Qty*Buying Price)", "CGST", "SGST", "IGST",
                          "CESS", "Discount", "TCS Amount", "Invoice Amount"])
+        counter = 0
+        data_dict = {}
         for query in queryset:
-            # iteration for selected id from Admin Dashboard and get the instance
-            for obj in query.auto_po.order_cart_mapping.order_grn_order.all():
-                # get object from queryset
-                for product in obj.grn_order_grn_order_product.all():
-                    tax_percentage = product.product.product_gst
-                    total_amount = (product.delivered_qty*product.po_product_price)
-                    amount_exclude_tax = (total_amount)/(1+tax_percentage/100)
-                    tax_amount = total_amount - amount_exclude_tax
-                    if product.delivered_qty == 0:
-                        continue
-                    else:
-                        writer.writerow([str(query.cart.rt_order_cart_mapping.rt_order_order_product.all()[0].invoice.invoice_no),
-                                         str(query.cart.rt_order_cart_mapping.rt_order_order_product.all()[0].invoice.created_at),
-                                         str(query.auto_po.supplier_name.company_name), str(query.auto_po.supplier_name_id),
-                                         str(query.auto_po.supplier_name.address_line1),
-                                         str(obj.grn_id), str(obj.grn_date), str(obj.order.ordered_cart.po_no), str(product.product.product_sku),
-                                         str(product.product.product_name), str(product.product.product_gst), str(product.delivered_qty),
-                                         str(product.po_product_price), str(total_amount),
-                                         "{:.1f}".format((tax_amount/2)), "{:.1f}".format((tax_amount/2)),
-                                         '',
-                                         str(product.product.product_cess),
-                                         str(query.cart.rt_order_cart_mapping.rt_order_order_product.all().last().order.total_discount_amount),
-                                         str(obj.tcs_amount),
-                                         str(obj.invoice_amount),
-                                         ])
-
+            # get grn object from auto po source
+            for grn_object in query.auto_po.order_cart_mapping.order_grn_order.all():
+                # check condition if grn invoice is same or not from grn object
+                if query.grn.invoice_no == grn_object.invoice_no:
+                    order_products = query.cart.rt_cart_list.all()
+                    for product in grn_object.grn_order_grn_order_product.all():
+                        for order_product in order_products:
+                            # po cart list product is not same as grn product
+                            if order_product.cart_product != product.product:
+                                continue
+                            tax_percentage = product.product.product_gst
+                            total_amount = (product.delivered_qty * product.po_product_price)
+                            counter += 1
+                            if product.product.product_sku in data_dict:
+                                data_dict_item = data_dict[grn_object.grn_id + product.product.product_sku]
+                                data_dict_item[11] += product.delivered_qty
+                                data_dict_item[13] = data_dict_item[11] * data_dict_item[12]
+                                data_dict_item[14] = round(
+                                    (data_dict_item[13] * tax_percentage / (100 + tax_percentage)) / 2, 2)
+                                data_dict_item[15] = round(
+                                    (data_dict_item[13] * tax_percentage / (100 + tax_percentage)) / 2, 22
+                                    )
+                                data_dict[grn_object.grn_id + product.product.product_sku] = data_dict_item
+                                continue
+                            # create dictionary which contains list object
+                            data_dict[grn_object.grn_id + product.product.product_sku] = [
+                                query.cart.rt_order_cart_mapping.rt_order_order_product.all()[0].invoice.invoice_no,
+                                (query.cart.rt_order_cart_mapping.rt_order_order_product.all(
+                                )[0].invoice.created_at.date()),
+                                query.auto_po.supplier_name.company_name, query.auto_po.supplier_name.id,
+                                query.auto_po.supplier_name.address_line1 + " " + query.auto_po.supplier_name.pincode
+                                + " " +
+                                query.auto_po.supplier_name.city.city_name + " " +
+                                query.auto_po.supplier_name.state.state_name,
+                                grn_object.grn_id, grn_object.grn_date, grn_object.order.ordered_cart.po_no,
+                                product.product.product_sku,
+                                product.product.product_name, product.product.product_gst, product.delivered_qty,
+                                product.po_product_price, total_amount,
+                                round((total_amount * tax_percentage / (100 + tax_percentage)) / 2, 2),
+                                round((total_amount * tax_percentage / (100 + tax_percentage)) / 2, 2),
+                                '',
+                                product.product.product_cess,
+                                (query.cart.rt_order_cart_mapping.rt_order_order_product.all().last(
+                                ).order.total_discount_amount),
+                                grn_object.tcs_amount,
+                                grn_object.invoice_amount
+                                ]
+        # iterate to write data in csv file
+        for key, value in data_dict.items():
+            writer.writerow(value)
         f.seek(0)
         response = HttpResponse(f, content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=auto_order.csv'
         return response
 
-
     download_csv_for_auto_order.short_description = "Download CSV of Auto Order"
 
+
+# register admin
 admin.site.register(AutoOrderProcessing, AutoOrderProcessingAdmin)
