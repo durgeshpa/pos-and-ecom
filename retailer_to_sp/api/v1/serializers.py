fe28f05776b4a22f600846ee23c3a208e8b890a2
Deepti <deepti@gramfactory.com>
2021-04-01 10:32:12 +0530

    product serializer

--- retailer_to_sp/api/v1/serializers.py
+++ retailer_to_sp/api/v1/serializers.py
@@ -232,9 +232,9 @@ class ProductsSearchSerializer(serializers.ModelSerializer):
     #product_pro_tax = ProductTaxMappingSerializer(many=True)
     product_opt_product = ProductOptionSerializer(many=True)
     #product_brand = BrandSerializer(read_only=True)
-    # product_price = serializers.SerializerMethodField('product_price_dt')
+    product_price = serializers.SerializerMethodField('product_price_dt')
     price_details = serializers.SerializerMethodField('m_slab_price')
-    # product_mrp = serializers.SerializerMethodField('product_mrp_dt')
+    product_mrp = serializers.SerializerMethodField('product_mrp_dt')
     product_case_size_picies = serializers.SerializerMethodField('product_case_size_picies_dt')
     # margin = serializers.SerializerMethodField('margin_dt')
     loyalty_discount = serializers.SerializerMethodField('loyalty_discount_dt')
@@ -246,17 +246,17 @@ class ProductsSearchSerializer(serializers.ModelSerializer):
         serializer = SlabProductPriceSerializer(product_price)
         return serializer.data
 
-    # def product_price_dt(self, obj):
-    #     self.product_price = obj.get_current_shop_price(
-    #         self.context.get('parent_mapping_id'),
-    #         self.context.get('buyer_shop_id')).get_PTR(1)
-    #     return self.product_price
+    def product_price_dt(self, obj):
+        self.product_price = obj.get_current_shop_price(
+            self.context.get('parent_mapping_id'),
+            self.context.get('buyer_shop_id')).get_PTR(1)
+        return self.product_price
 
-    # def product_mrp_dt(self, obj):
-    #     self.product_mrp = obj.getMRP(
-    #         self.context.get('parent_mapping_id'),
-    #         self.context.get('buyer_shop_id'))
-    #     return self.product_mrp
+    def product_mrp_dt(self, obj):
+        self.product_mrp = obj.getMRP(
+            self.context.get('parent_mapping_id'),
+            self.context.get('buyer_shop_id'))
+        return self.product_mrp
 
     def product_case_size_picies_dt(self, obj):
         return str(int(obj.product_inner_case_size)*int(obj.product_case_size))
