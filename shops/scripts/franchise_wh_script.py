fd3eb0e6155a69d84c07c1fa8bf1280c47a10de1
ShalineeB <shalinee@gramfactory.com>
2021-04-05 19:36:47 +0530

    Refactored code

---
+++ shops/scripts/franchise_wh_script.py
@@ -0,0 +1,30 @@
+# python imports
+import datetime
+import logging
+
+# app imports
+from shops.models import Shop
+
+# logger configuration
+info_logger = logging.getLogger('file-info')
+cron_logger = logging.getLogger('cron_log')
+
+
+def warehouse_code_refactor():
+    """
+        Cron job for refactor warehouse_code when shop_type is Franchise
+        :return:
+    """
+    try:
+        cron_logger.info('Cron job for refactor warehouse_code when shop_type is Franchise|started')
+        shop_franchise = Shop.objects.filter(shop_type=5)   # shop_type = Franchise
+        if shop_franchise:
+            for franchise in shop_franchise:
+                franchise.warehouse_code = '0'+franchise.warehouse_code
+                franchise.save()
+            cron_logger.info('warehouse_code is successfully updated')
+        else:
+            cron_logger.info('no Shop found with shop_type Franchise')
+    except Exception as e:
+        cron_logger.error(e)
+        cron_logger.error('Exception in warehouse_code_refactor cron')
