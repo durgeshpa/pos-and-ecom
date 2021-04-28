6542af22950a9810d2e2a33b24311aa9dab198d6
ShalineeB <shalinee@gramfactory.com>
2021-04-28 19:19:31 +0530

    Refactored Code

--- retailer_incentive/utils.py
+++ retailer_incentive/utils.py
@@ -34,35 +34,47 @@ def get_shop_scheme_mapping_based_on_month(shop_id, month):
                                                               start_date__year=current_year,
                                                               end_date__year=current_year,
                                                               start_date__month=month,
-                                                              end_date__month=month)
+                                                              end_date__month=month).order_by('-start_date',
+                                                                                              'priority')
+
+
     if shop_scheme_mapping_qs:
+        start_end_list = []
         scheme_shop_mapping_list = []
-        if shop_scheme_mapping_qs.filter(priority=SchemeShopMapping.PRIORITY_CHOICE.P1).exists():
-            shop_scheme_mapping = shop_scheme_mapping_qs.filter(priority=SchemeShopMapping.PRIORITY_CHOICE.P1)
-            for shop_scheme_map in shop_scheme_mapping:
-                scheme_shop_mapping_list.append(shop_scheme_map)
-            return scheme_shop_mapping_list
-        else:
-            for shop_scheme in shop_scheme_mapping_qs:
-                scheme_shop_mapping_list.append(shop_scheme)
-            return scheme_shop_mapping_list
+        for scheme in shop_scheme_mapping_qs:
+            start_end = str(scheme.start_date.date()) + str(scheme.end_date.date())
+            if start_end in start_end_list:
+                continue
+            start_end_list += [start_end]
+            scheme_shop_mapping_list.append(scheme)
+        return scheme_shop_mapping_list
     return shop_scheme_mapping_qs
+
+
+
 
 
 def get_shop_scheme_mapping_based_on_month_from_db(shop_id, month):
     """Returns the valid Scheme mapped for given shop_id based on selected month from DB"""
     current_year = today_date.year
-    scheme_shop_mapping_list = []
     shop_scheme_mapping_qs = IncentiveDashboardDetails.objects.filter(shop_id=shop_id,
                                                                       start_date__year=current_year,
                                                                       end_date__year=current_year,
                                                                       start_date__month=month,
                                                                       end_date__month=month).order_by('-start_date',
                                                                                                       'scheme_priority')
+
     if shop_scheme_mapping_qs:
-        for shop_scheme in shop_scheme_mapping_qs:
-            scheme_shop_mapping_list.append(shop_scheme)
+        start_end_list = []
+        scheme_shop_mapping_list = []
+        for scheme in shop_scheme_mapping_qs:
+            start_end = str(scheme.start_date.date()) + str(scheme.end_date.date())
+            if start_end in start_end_list:
+                continue
+            start_end_list += [start_end]
+            scheme_shop_mapping_list.append(scheme)
         return scheme_shop_mapping_list
     return shop_scheme_mapping_qs
+
 
 
