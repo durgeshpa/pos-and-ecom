e5e1f4554a6691f3b4b280cd45d376cf92fb827d
Gramfactory <yash@gramfactory.com>
2021-04-30 11:39:42 +0530

    [#list issue resolved]

--- retailer_incentive/utils.py
+++ retailer_incentive/utils.py
@@ -30,6 +30,7 @@ def get_shop_scheme_mapping(shop_id):
 def get_shop_scheme_mapping_based(shop_id, month):
     """Returns the valid Scheme mapped for given shop_id based on selected month (current_month)"""
     current_year = today_date.year
+    scheme_shop_mapping_list = []
     if month == today_date.month:
         var_priority = 'priority'
         shop_scheme_mapping_qs = SchemeShopMapping.objects.filter(shop_id=shop_id, is_active=True,
@@ -38,15 +39,24 @@ def get_shop_scheme_mapping_based(shop_id, month):
                                                                   start_date__month=month,
                                                                   end_date__month=month).order_by('-start_date',
                                                                                                   var_priority)
-
-    elif month == today_date.month:
-        var_priority = 'scheme_priority'
-        shop_scheme_mapping_qs = IncentiveDashboardDetails.objects.filter(shop_id=shop_id,
-                                                                          start_date__year=current_year,
-                                                                          end_date__year=current_year,
-                                                                          start_date__month=month,
-                                                                          end_date__month=month).order_by('-start_date',
-                                                                                                          var_priority)
+        if shop_scheme_mapping_qs:
+            scheme_shop_mapping_list.append(shop_scheme_mapping_qs.last())
+        else:
+            var_priority = 'scheme_priority'
+            shop_scheme_mapping_qs = IncentiveDashboardDetails.objects.filter(shop_id=shop_id,
+                                                                              start_date__year=current_year,
+                                                                              end_date__year=current_year,
+                                                                              start_date__month=month,
+                                                                              end_date__month=month).order_by('-start_date',
+                                                                                                              var_priority)
+            if shop_scheme_mapping_qs:
+                start_end_date_list = []
+                for scheme in shop_scheme_mapping_qs:
+                    start_end_date = str(scheme.start_date) + str(scheme.end_date)
+                    if start_end_date in start_end_date_list:
+                        continue
+                    start_end_date_list += [start_end_date]
+                    scheme_shop_mapping_list.append(scheme)
 
     else:
         var_priority = 'scheme_priority'
@@ -57,17 +67,17 @@ def get_shop_scheme_mapping_based(shop_id, month):
                                                                           end_date__month=month).order_by('-start_date',
                                                                                                           var_priority)
 
-    if shop_scheme_mapping_qs:
-        start_end_date_list = []
-        scheme_shop_mapping_list = []
-        for scheme in shop_scheme_mapping_qs:
-            start_end_date = str(scheme.start_date) + str(scheme.end_date)
-            if start_end_date in start_end_date_list:
-                continue
-            start_end_date_list += [start_end_date]
-            scheme_shop_mapping_list.append(scheme)
-        return scheme_shop_mapping_list
-    return shop_scheme_mapping_qs
+        if shop_scheme_mapping_qs:
+            start_end_date_list = []
+            scheme_shop_mapping_list = []
+            for scheme in shop_scheme_mapping_qs:
+                start_end_date = str(scheme.start_date) + str(scheme.end_date)
+                if start_end_date in start_end_date_list:
+                    continue
+                start_end_date_list += [start_end_date]
+                scheme_shop_mapping_list.append(scheme)
+    return scheme_shop_mapping_list
+
 
 
 
