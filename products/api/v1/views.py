f25c7cf60086a57f2fa85189b6ae00487dc5631c
ShalineeB <shalinee@gramfactory.com>
2021-05-07 19:01:34 +0530

    added ParentProduct APIs

--- products/api/v1/views.py
+++ products/api/v1/views.py
@@ -6,6 +6,9 @@ from rest_framework.parsers import JSONParser
 
 from .serializers import ParentProductSerializers
 from products.utils import MultipartJsonParser
+from products.models import  ParentProduct
+
+from retailer_backend.utils import SmallOffsetPagination
 
 
 class ParentProductView(GenericAPIView):
@@ -13,9 +16,26 @@ class ParentProductView(GenericAPIView):
     authentication_classes = (authentication.TokenAuthentication,)
     permission_classes = (AllowAny,)
     parser_classes = [MultipartJsonParser, JSONParser]
+    parent_product_list = ParentProduct.objects.all()
 
-    def get(self, request, *args, **kwargs):
-        pass
+    def get(self, request):
+        """ GET API to get Parent Product List """
+        if request.GET.get('parent_product_id'):
+            """
+               Get Parent Product when product_id is given in params
+            """
+            id = request.GET.get('parent_product_id')
+            parent_pro_id = self.parent_product_list.filter(id=id).last()
+            if parent_pro_id is None:
+                msg = {'is_success': False,
+                       'message': ['Please Provide a Valid parent_product_id'],
+                       'data': None}
+                return Response(msg, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+            self.parent_product_list = self.parent_product_list.filter(id=id)
+        parent_product = SmallOffsetPagination().paginate_queryset(self.parent_product_list, request)
+        serializer = ParentProductSerializers(parent_product, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
 
