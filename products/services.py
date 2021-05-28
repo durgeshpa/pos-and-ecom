40903536143488252dfebc12972a5333e5e9f075
ShalineeB <shalinee@gramfactory.com>
2021-05-28 17:19:52 +0530

    Refactored Code

--- products/services.py
+++ products/services.py
@@ -4,7 +4,8 @@ from django.db.models import Q
 # search using parent_id, name & category_name based on criteria that matches
 def parent_product_search(queryset, search_text):
     queryset = queryset.filter(Q(name__icontains=search_text) | Q(parent_id__icontains=search_text)
-                               | Q(parent_product_pro_category__category__category_name__icontains=search_text))
+                             | Q(product_parent_product__product_name__icontains=search_text)
+                             | Q(product_parent_product__product_sku__icontains=search_text)                              )
     return queryset
 
 
