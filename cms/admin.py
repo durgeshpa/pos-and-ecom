529ce43c7bd94305783ba22c931a4b8c570fbc04
todi-2000 <manshitodi@gmail.com>
2021-06-10 19:55:37 +0530

    setup

--- cms/admin.py
+++ cms/admin.py
@@ -1,8 +1,8 @@
-import admin
-from cms.models import Card, CardVersion, CardData, CardItem, Application
+from django.contrib import admin
+from cms.models import Card, CardVersion, CardData, CardItem, Application, ApplicationPage
 
 admin.site.register(Application)
-
+admin.site.register(ApplicationPage)
 admin.site.register(Card)
 admin.site.register(CardVersion)
 admin.site.register(CardData)
--- cms/api/v1/urls.py
+++ cms/api/v1/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
 
+from .views import ApplicationDetailView, ApplicationView
 urlpatterns = [
+    path('apps/', ApplicationView.as_view(), name = 'apps'),
+    path('apps/<id>', ApplicationDetailView.as_view(), name = 'app_detail')
 ]
