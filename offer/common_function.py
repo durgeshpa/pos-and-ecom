8108b8e40619b6a5955260d3b039665858b0617f
shalinee <shalinee@gramfactory.com>
2021-07-29 11:18:23 +0530

    Offer Slot API Creation

--- offer/common_function.py
+++ offer/common_function.py
@@ -29,7 +29,7 @@ class OfferCls(object):
             Create Parent Product Log
         """
         action, create_updated_by = created_updated_by(log_obj, action)
-        offer_page_product_log = OfferLog.objects.create(offer_page=log_obj, updated_by=create_updated_by,
+        offer_page_product_log = OfferLog.objects.create(offer_banner_slot=log_obj, updated_by=create_updated_by,
                                                          action=action)
         dict_data = {'updated_by': offer_page_product_log.updated_by, 'updated_at': offer_page_product_log.update_at,
                      'offer_page': log_obj, }
