a743840ae8b819212e3b42539f4de31ad9a8523e
shalinee <shalinee@gramfactory.com>
2021-07-29 11:29:52 +0530

    Offer Slot API Creation

--- offer/common_function.py
+++ offer/common_function.py
@@ -12,27 +12,56 @@ class OfferCls(object):
     @classmethod
     def create_offer_page_log(cls, log_obj, action):
         """
-            Create Parent Product Log
+            Create Offer Page Log
         """
         action, create_updated_by = created_updated_by(log_obj, action)
-        offer_page_product_log = OfferLog.objects.create(offer_page=log_obj, updated_by=create_updated_by,
+        offer_page_log = OfferLog.objects.create(offer_page=log_obj, updated_by=create_updated_by,
                                                          action=action)
-        dict_data = {'updated_by': offer_page_product_log.updated_by, 'updated_at': offer_page_product_log.update_at,
+        dict_data = {'updated_by': offer_page_log.updated_by, 'updated_at': offer_page_log.update_at,
                      'offer_page': log_obj, }
         info_logger.info("offer page update info ", dict_data)
 
-        return offer_page_product_log
+        return offer_page_log
 
     @classmethod
     def create_offer_banner_slot_log(cls, log_obj, action):
         """
-            Create Parent Product Log
+            Create Offer Banner Slot Log
         """
         action, create_updated_by = created_updated_by(log_obj, action)
-        offer_page_product_log = OfferLog.objects.create(offer_banner_slot=log_obj, updated_by=create_updated_by,
+        offer_banner_slot_log = OfferLog.objects.create(offer_banner_slot=log_obj, updated_by=create_updated_by,
                                                          action=action)
-        dict_data = {'updated_by': offer_page_product_log.updated_by, 'updated_at': offer_page_product_log.update_at,
-                     'offer_page': log_obj, }
-        info_logger.info("offer page update info ", dict_data)
+        dict_data = {'updated_by': offer_banner_slot_log.updated_by, 'updated_at': offer_banner_slot_log.update_at,
+                     'offer_banner_slot': log_obj, }
+        info_logger.info("offer banner slot update info ", dict_data)
 
-        return offer_page_product_log
+        return offer_banner_slot_log
+
+    @classmethod
+    def create_offer_banner_log(cls, log_obj, action):
+        """
+            Create Offer Banner Log
+        """
+        action, create_updated_by = created_updated_by(log_obj, action)
+        offer_banner_log = OfferLog.objects.create(offer_banner=log_obj, updated_by=create_updated_by,
+                                                         action=action)
+        dict_data = {'updated_by': offer_banner_log.updated_by, 'updated_at': offer_banner_log.update_at,
+                     'offer_banner': log_obj, }
+        info_logger.info("offer banner update info ", dict_data)
+
+        return offer_banner_log
+
+    @classmethod
+    def create_top_sku_log(cls, log_obj, action):
+        """
+            Create Top Sku Log
+        """
+        action, create_updated_by = created_updated_by(log_obj, action)
+        top_sku_log = OfferLog.objects.create(top_sku=log_obj, updated_by=create_updated_by,
+                                                    action=action)
+        dict_data = {'updated_by': top_sku_log.updated_by, 'updated_at': top_sku_log.update_at,
+                     'top_sku': log_obj, }
+        info_logger.info("top sku update info ", dict_data)
+
+        return top_sku_log
+    
