f8763349bda8cbe78c338ec395c236a18a22fe77
monali <monali@gramfactory.com>
2021-08-03 15:12:19 +0530

    pay mode change

--- pos/admin.py
+++ pos/admin.py
@@ -745,6 +745,9 @@ class PaymentTypeAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
 
 class ProductChangeFieldsAdmin(admin.TabularInline):
     model = ProductChangeFields
