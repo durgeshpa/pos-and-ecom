7abad516e7df14edef78ed667f90a3ce0a97218c
shalinee <shalinee@gramfactory.com>
2021-08-09 18:07:54 +0530

    bulk product vendor mapping

--- products/forms.py
+++ products/forms.py
@@ -2256,7 +2256,6 @@ class SlabInlineFormSet(BaseInlineFormSet):
                 raise ValidationError("Quantity should be greater than earlier slabs quantity")
             elif form.prefix != 'price_slabs-0' and slab_data.get('selling_price') and (slab_data.get('selling_price') >= last_slab_selling_price \
                     or (last_slab_offer_price and slab_data.get('selling_price') >= last_slab_offer_price)) :
-                print(last_slab_offer_price)
                 raise ValidationError("Selling price should be less than earlier slabs selling price/offer price.")
             elif slab_data.get('offer_price') is not None:
                 if slab_data.get('selling_price') <= slab_data.get('offer_price'):
