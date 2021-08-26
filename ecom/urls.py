863adf09859a0f3bb348d2a166b2d56833b7b704
todi-2000 <manshitodi@gmail.com>
2021-08-26 06:03:21 +0530

    fixed import issue

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+
 urlpatterns = [
     url(r'^api/', include('ecom.api.urls')),
 ]
--- ecom/views.py
+++ ecom/views.py
@@ -1,4 +1,3 @@
-from re import I
 from django.shortcuts import render
 
 # Create your views here.
