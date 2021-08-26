f38b594dcb8d6a730ef88c756abe28c69f237862
todi-2000 <manshitodi@gmail.com>
2021-08-26 06:00:49 +0530

    updated

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,7 +1,4 @@
 from django.conf.urls import include, url
-from .views import test
-
 urlpatterns = [
     url(r'^api/', include('ecom.api.urls')),
-    url(r'^test/', test)
 ]
--- ecom/views.py
+++ ecom/views.py
@@ -1,9 +1,4 @@
 from re import I
 from django.shortcuts import render
-from django.http import HttpResponse
-from .cron import bestseller_product
 
 # Create your views here.
-def test(req):
-    bestseller_product()
-    return HttpResponse('True')
