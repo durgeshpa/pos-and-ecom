84119d5e342ee3ecfdf73af74be5482e63215de6
monali <monali@gramfactory.com>
2021-08-26 20:35:16 +0530

    merged

--- ecom/api/v1/serializers.py
+++ ecom/api/v1/serializers.py
@@ -10,7 +10,7 @@ from shops.models import Shop
 from retailer_to_sp.models import Order, OrderedProductMapping, CartProductMapping
 from pos.models import RetailerProduct
 
-from ecom.models import Address, EcomOrderAddress
+from ecom.models import Address, EcomOrderAddress, Tag
 
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -239,6 +239,60 @@ class EcomOrderDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('id', 'order_no', 'products', 'order_amount', 'address')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Serializer for tags
+    """
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+        if obj.status:
+            return "Active"
+        else:
+            return "Inactive"
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'key', 'name', 'position')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer to get product details
+    """
+    image = serializers.SerializerMethodField()
+    selling_price = serializers.SerializerMethodField()
+
+    def get_selling_price(self, obj):
+        if obj.online_price:
+            return obj.online_price
+        else:
+            return obj.selling_price
+
+    def get_image(self, obj):
+        return obj.retailer_product_image.last().image.url if obj.retailer_product_image.last() else None
+
+    class Meta:
+        model = RetailerProduct
+        fields = ('id', 'name', 'mrp', 'selling_price', 'image')
+
+
+class TagProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer to get product by Tag
+    """
+
+    class Meta:
+        model = Tag
+        fields = ('key', 'name')
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        product = self.context.get('product')
+        data['products'] = ProductSerializer(product, many = True).data
+        return data
 
 
 class EcomShipmentProductSerializer(serializers.Serializer):
