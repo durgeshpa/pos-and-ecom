91bf2aa9d4037436e8255335f2f4b7161b3441dc
shalinee <shalinee@gramfactory.com>
2021-09-01 13:40:52 +0530

    added EAN in PO

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -4403,8 +4403,8 @@ def pdf_generation(request, ordered_product):
             ordered_prodcut = {
                 "product_sku": m.product.product_gf_code,
                 "product_short_description": m.product.product_short_description,
-                "product_hsn": m.product.product_hsn,
                 "product_ean_code": m.product.product_ean_code,
+                "product_hsn": m.product.product_hsn,
                 "product_tax_percentage": "" if tax_sum == 0 else str(tax_sum) + "%",
                 "product_mrp": product_pro_price_mrp,
                 "shipped_qty": m.shipped_qty,
