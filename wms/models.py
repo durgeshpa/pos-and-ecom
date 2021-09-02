52228ee5b367cd5906d9633aba2c399eb15ba0e1
Kamal Agarwal <kamal@gramfactory.com>
2021-09-02 10:13:52 +0530

    Add new cron to assign putaway users to all those putaways whose zone exists but putaway user not assigned.

--- wms/models.py
+++ wms/models.py
@@ -245,7 +245,7 @@ class In(models.Model):
 
 
 class Putaway(models.Model):
-    PUTAWAY_STATUS_CHOICE = Choices((0,'NEW', 'New'), (1, 'ASSIGNED', 'Assigned'), (2, 'INITIATED', 'Initiated'),
+    PUTAWAY_STATUS_CHOICE = Choices((0, 'NEW', 'New'), (1, 'ASSIGNED', 'Assigned'), (2, 'INITIATED', 'Initiated'),
                                     (3, 'COMPLETED', 'Completed'), (4, 'CANCELLED', 'Cancelled'))
     warehouse = models.ForeignKey(Shop, null=True, blank=True, on_delete=models.DO_NOTHING)
     putaway_user = models.ForeignKey(get_user_model(), null=True, blank=True, related_name='putaway_user',
@@ -257,7 +257,7 @@ class Putaway(models.Model):
     inventory_type = models.ForeignKey(InventoryType, null=True, blank=True, on_delete=models.DO_NOTHING)
     quantity = models.PositiveIntegerField()
     putaway_quantity = models.PositiveIntegerField(null=True, blank=True, default=0)
-    status = models.CharField(choices=PUTAWAY_STATUS_CHOICE, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=PUTAWAY_STATUS_CHOICE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
