c84ff269f80bb843e135d27a328684cbe566013a
Deepti <deepti@gramfactory.com>
2021-09-17 19:05:13 +0530

    Zone Pickers mapping and QC Area Creation

--- wms/signals.py
+++ wms/signals.py
@@ -1,9 +1,12 @@
 import logging
+import sys
 
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from wms.models import ZonePutawayUserAssignmentMapping, Zone
+from common.common_utils import barcode_gen
+from wms.models import ZonePutawayUserAssignmentMapping, Zone, QCArea
 
 logger = logging.getLogger(__name__)
 info_logger = logging.getLogger('file-info')
@@ -30,3 +33,14 @@ def create_zone_putaway_user_assignment_mapping(sender, instance=None, created=F
         for user in instance.putaway_users.all():
             ZonePutawayUserAssignmentMapping.objects.update_or_create(zone=instance, user=user, defaults={})
             info_logger.info("ZonePutawayUser mapping created for zone " + str(instance) + ", user:" + str(user))
+
+
+@receiver(post_save, sender=QCArea)
+def create_qc_area_barcode(sender, instance=None, created=False, update_fields=None, **kwargs):
+    """ Generates barcode_txt and bar_code image for QCArea"""
+    if created:
+        instance.area_barcode_txt = '30' + str(instance.id).zfill(10)
+        image = barcode_gen(str(instance.area_barcode_txt))
+        instance.area_barcode = InMemoryUploadedFile(image, 'ImageField', "%s.jpg" % instance.area_id, 'image/jpeg',
+                                                 sys.getsizeof(image), None)
+        instance.save()
