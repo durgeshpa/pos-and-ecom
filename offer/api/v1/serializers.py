ef4058acf1e2bf3e8fafa4e0667aaa3851f46957
Gramfactory <yash@gramfactory.com>
2021-09-22 14:26:52 +0530

    [Inactive top sku for shop]

--- offer/api/v1/serializers.py
+++ offer/api/v1/serializers.py
@@ -254,6 +254,8 @@ class TopSKUSerializers(serializers.ModelSerializer):
         top_sku = obj.offer_top_sku.all().values('product__id')
         products = Product.objects.filter(id__in = top_sku)
         data = ProductSerializers(products, many=True).data
+        top_sku= TopSKU.objects.filter(shop=obj.shop).exclude(id=obj.id)
+        top_sku.update(status=False)
         return data
 
     
