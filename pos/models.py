92a41a90e228608c38393bfaef560b57a7df7ebf
shalinee <shalinee@gramfactory.com>
2021-10-22 16:47:52 +0530

    added deleted field in RetailerProduct

--- pos/models.py
+++ pos/models.py
@@ -75,6 +75,7 @@ class RetailerProduct(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     online_enabled = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
     online_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
