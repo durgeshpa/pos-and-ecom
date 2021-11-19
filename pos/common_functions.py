340b6222ae1ca17b9e6325b9faca7a247f5b2498
Gramfactory <yash@gramfactory.com>
2021-11-19 19:28:24 +0530

    [#sample download file while upload file]

--- pos/common_functions.py
+++ pos/common_functions.py
@@ -227,6 +227,29 @@ class PosInventoryCls(object):
                                                 f_state_obj, user, i_qty, f_qty, remarks)
 
     @classmethod
+    def app_stock_inventory(cls, pid, i_state, f_state, qty, user, transaction_id, transaction_type, remarks=None):
+        """
+            Create/Update available inventory for product
+        """
+        i_state_obj = PosInventoryState.objects.get(inventory_state=i_state)
+        f_state_obj = i_state_obj if i_state == f_state else PosInventoryState.objects.get(inventory_state=f_state)
+        pos_inv, created = PosInventory.objects.get_or_create(product_id=pid, inventory_state=f_state_obj)
+        if not created and Decimal(qty) == pos_inv.quantity:
+            return
+        i_qty, f_qty = None, None
+        if transaction_type == PosInventoryChange.STOCK_UPDATE:
+            i_qty = pos_inv.quantity
+            f_qty = Decimal(qty)
+            if i_qty == f_qty:
+                info_logger.info(f"POS|stock_inventory|initial quantity {i_qty} and final quantity {f_qty} is same")
+                return
+        qty_change = Decimal(qty) - pos_inv.quantity
+        pos_inv.quantity = Decimal(qty)
+        pos_inv.save()
+        PosInventoryCls.create_inventory_change(pid, qty_change, transaction_type, transaction_id, i_state_obj,
+                                                f_state_obj, user, i_qty, f_qty, remarks)
+
+    @classmethod
     def order_inventory(cls, pid, i_state, f_state, qty, user, transaction_id, transaction_type):
         """
             Manage Order related product inventory (Order creation, cancellations, returns)
