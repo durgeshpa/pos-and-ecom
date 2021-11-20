21ed8958b095a675ac97f2bd341e6016ce105d04
Brijmohan SIngh <brij@gramfactory.com>
2021-11-20 13:32:35 +0530

    changes

--- ecom/api/v1/serializers.py
+++ ecom/api/v1/serializers.py
@@ -345,7 +345,8 @@ class ProductSerializer(serializers.ModelSerializer):
             image = retailer_object.image.url
         return image
 
-    class Meta:
+
+class Meta:
         model = RetailerProduct
         fields = ('id', 'name', 'mrp', 'online_price', 'image')
 
