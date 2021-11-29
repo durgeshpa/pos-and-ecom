c804b06e5faaf8d168e1543048f6474fc2c2b9ef
Deepti <deepti@gramfactory.com>
2021-11-29 17:13:54 +0530

    QC Rejected Shipments

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -6621,7 +6621,6 @@ class ShipmentQCView(generics.GenericAPIView):
         order_by('-id')
 
 
-
     def get(self, request):
         if not request.GET.get('status'):
             return get_response("'status' | This is mandatory")
