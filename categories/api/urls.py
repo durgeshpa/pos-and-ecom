f7fda3243f64906276fdc86be638e0fbb8d6e86b
Gramfactory <yash@gramfactory.com>
2021-11-30 13:30:33 +0530

    [ch_prod_category_issue:-Category issue reoslved]

--- categories/api/urls.py
+++ categories/api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url,include
 #from .views import (PasswordResetView, PasswordResetConfirmView)
 
 urlpatterns = [
-    url(r'^v2/', include('categories.api.v2.urls'))
+    url(r'^v1/', include('categories.api.v2.urls')),
+    url(r'^v2/', include('categories.api.v2.urls')),
 ]
