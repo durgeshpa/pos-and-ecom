8927885d78c7646c3b2d713f691ec06689061af3
todi-2000 <manshitodi@gmail.com>
2021-12-06 16:20:22 +0530

    fixed issue

--- pos/admin.py
+++ pos/admin.py
@@ -114,7 +114,7 @@ class RetailerProductAdmin(admin.ModelAdmin):
                     'modified_at')
     fields = ('shop', 'linked_product', 'sku', 'name', 'mrp', 'selling_price', 'product_ean_code',
               'description', 'sku_type', 'status', 'is_deleted', 'purchase_pack_size', 'initial_purchase_value',
-              'online_enabled', 'online_price', 'created_at', 'modified_at','measurement_category','product_pack_type')
+              'online_enabled', 'online_price', 'created_at', 'modified_at','product_pack_type','measurement_category')
     readonly_fields = ('shop', 'sku', 'product_ean_code',
                        'purchase_pack_size', 'online_enabled', 'online_price', 'name', 'created_at',
                        'sku_type', 'mrp', 'modified_at', 'description', 'initial_purchase_value')
