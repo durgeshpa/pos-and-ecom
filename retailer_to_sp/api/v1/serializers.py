f4f09e719592c49351226226c1fffb8f09a31ce6
Deepti <deepti@gramfactory.com>
2021-12-09 19:35:30 +0530

    fix:dispatch trip creation

--- retailer_to_sp/api/v1/serializers.py
+++ retailer_to_sp/api/v1/serializers.py
@@ -2078,7 +2078,8 @@ class DispatchTripCrudSerializers(serializers.ModelSerializer):
 
         if 'delivery_boy' in self.initial_data and self.initial_data['delivery_boy']:
             try:
-                delivery_boy = User.objects.get(id=self.initial_data['delivery_boy'], shop_employee__shop=seller_shop)
+                delivery_boy = User.objects.filter(id=self.initial_data['delivery_boy'],
+                                                shop_employee__shop=seller_shop).last()
             except:
                 raise serializers.ValidationError("Invalid delivery_boy | User not found for " + str(seller_shop))
             if delivery_boy.groups.filter(name='Delivery Boy').exists():
