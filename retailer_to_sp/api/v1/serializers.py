246750e10edf092b5d4df53b7c84beaafc12ce5c
Kamal Agarwal <kamal@gramfactory.com>
2021-12-24 15:52:54 +0530

    API | GET | Order detail by Crate Id

--- retailer_to_sp/api/v1/serializers.py
+++ retailer_to_sp/api/v1/serializers.py
@@ -2380,7 +2380,7 @@ class ShipmentDetailsByCrateSerializer(serializers.ModelSerializer):
         shop_owner_name = obj.order.buyer_shop.shop_owner.first_name + obj.order.buyer_shop.shop_owner.last_name
         return shop_owner_name
 
-    def get_shipment_crates_packaging(self, obj):
+    def get_total_shipment_crates(self, obj):
         if obj:
             return DispatchItemsSerializer(obj.shipment_packaging.filter(
                 packaging_type=ShipmentPackaging.CRATE, crate__isnull=False), read_only=True, many=True).data
