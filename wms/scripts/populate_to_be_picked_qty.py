d76a0310d017088180309ef3cedc8c7dabf1c078
Kamal Agarwal <kamal@gramfactory.com>
2021-12-27 16:53:23 +0530

    Change

--- wms/scripts/populate_to_be_picked_qty.py
+++ wms/scripts/populate_to_be_picked_qty.py
@@ -40,24 +40,24 @@ def populate_to_be_picked_quantity(warehouse):
     """
         Case 2 | Set to_be_picked_qty from PickupBinInventory whose pickup status not in [completed and cancelled]
     """
-    # bin_qty_dict = {}
-    # pbi_qs = PickupBinInventory.objects.filter(~Q(pickup__status__in=['picking_complete', 'picking_cancelled']),
-    #                                            warehouse=warehouse)\
-    #                            .values('bin_id').annotate(pickup_qty=Sum('quantity'),
-    #                                                       picked_qty=Sum('pickup_quantity'))
-    # for pbi in pbi_qs:
-    #     if not pbi['picked_qty']:
-    #         pbi['picked_qty'] = 0
-    #     to_be_picked_qty = pbi['pickup_qty'] - pbi['picked_qty']
-    #     bin_qty_dict[pbi['bin_id']] = to_be_picked_qty
-    #
-    # print(bin_qty_dict)
-    #
-    # for bin_inv_id, qty in bin_qty_dict.items():
-    #     bin_inv_obj = BinInventory.objects.get(id=bin_inv_id)
-    #     bin_inv_obj.to_be_picked_qty = qty
-    #     bin_inv_obj.save()
-    #     print('Bin-{}, Batch-{}, Type-{}, to be picked quantity-{}, UPDATED'
-    #           .format(bin_inv_obj.bin, bin_inv_obj.batch_id, bin_inv_obj.inventory_type, bin_inv_obj.to_be_picked_qty))
-    #
-    # print('Warehouse-{}|populate_to_be_picked_quantity|ENDED'.format(warehouse))
+    bin_qty_dict = {}
+    pbi_qs = PickupBinInventory.objects.filter(~Q(pickup__status__in=['picking_complete', 'picking_cancelled']),
+                                               warehouse=warehouse)\
+                               .values('bin_id').annotate(pickup_qty=Sum('quantity'),
+                                                          picked_qty=Sum('pickup_quantity'))
+    for pbi in pbi_qs:
+        if not pbi['picked_qty']:
+            pbi['picked_qty'] = 0
+        to_be_picked_qty = pbi['pickup_qty'] - pbi['picked_qty']
+        bin_qty_dict[pbi['bin_id']] = to_be_picked_qty
+
+    print(bin_qty_dict)
+
+    for bin_inv_id, qty in bin_qty_dict.items():
+        bin_inv_obj = BinInventory.objects.get(id=bin_inv_id)
+        bin_inv_obj.to_be_picked_qty = qty
+        bin_inv_obj.save()
+        print('Bin-{}, Batch-{}, Type-{}, to be picked quantity-{}, UPDATED'
+              .format(bin_inv_obj.bin, bin_inv_obj.batch_id, bin_inv_obj.inventory_type, bin_inv_obj.to_be_picked_qty))
+
+    print('Warehouse-{}|populate_to_be_picked_quantity|ENDED'.format(warehouse))
