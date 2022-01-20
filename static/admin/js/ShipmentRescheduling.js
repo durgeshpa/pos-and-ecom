65c21c24caed777bc1849e15e07547e38ee1a95d
Gramfactory <yash@gramfactory.com>
2022-01-20 13:09:57 +0530

    [#ch_1yzjuw3:-Reschedule date chnage logic done]

--- static/admin/js/ShipmentRescheduling.js
+++ static/admin/js/ShipmentRescheduling.js
@@ -15,7 +15,7 @@
     'FULLY_RETURNED_AND_COMPLETED','FULLY_DELIVERED_AND_COMPLETED']
     if ($.inArray($('select#id_shipment_status').val(), shipment_status) == -1){
            $("select[id$='rescheduling_reason']").css('pointer-events','none');
-           $("input[id$='rescheduling_date']").css('pointer-events','none');
+           $("input[id$='rescheduling_date']");
 
  }
 });
