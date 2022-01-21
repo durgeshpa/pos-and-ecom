f2ebcb2e75f19c59018103c47529d8c1e399bc1e
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-01-21 15:46:08 +0530

    added PAYU key in order list api

--- pos/common_functions.py
+++ pos/common_functions.py
@@ -316,7 +316,7 @@ class PosInventoryCls(object):
 def api_response(msg, data=None, status_code=status.HTTP_406_NOT_ACCEPTABLE, success=False, extra_params=None):
     ret = {"is_success": success, "message": msg, "response_data": data}
     if extra_params:
-            ret.update(extra_params)
+        ret.update(extra_params)
     return Response(ret, status=status_code)
 
 
