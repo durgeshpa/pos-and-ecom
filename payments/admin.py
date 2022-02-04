15eb9d3cd02a298d3f04afcee243cd7cd60fc9da
Shalinee Bhawnanai <shalinee@gramfactory.com>
2022-02-04 23:27:21 +0530

    Changes

--- payments/admin.py
+++ payments/admin.py
@@ -287,9 +287,6 @@ class ShipmentPaymentInlineAdmin(admin.TabularInline, PermissionMixin):
     readonly_fields = ("paid_amount", "payment_mode_name", "reference_no","payment_approval_status")
     extra = 0
 
-    class Media:
-        js = ("js/shipment_payment_add_another.js",)
-
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "parent_order_payment":
             try:
