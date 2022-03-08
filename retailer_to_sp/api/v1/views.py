562b78196a7283cc4a5f427ad758aa213cb42e61
Deepti <deepti@gramfactory.com>
2022-03-08 11:07:07 +0530

    invoice filter for missing packages

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -8350,7 +8350,6 @@ class DispatchCenterShipmentView(generics.GenericAPIView):
                         DispatchTripShipmentPackages.DAMAGED_AT_UNLOADING])
             elif invoice_package_status == PACKAGE_VERIFY_CHOICES.MISSING:
                 self.queryset = self.queryset.filter(
-                    ~Q(trip_shipment__shipment_status=DispatchTripShipmentMapping.CANCELLED),
                     trip_shipment__trip_shipment_mapped_packages__package_status__in=[
                         DispatchTripShipmentPackages.MISSING_AT_LOADING,
                         DispatchTripShipmentPackages.MISSING_AT_UNLOADING])
