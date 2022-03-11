b9c6074aa98d9a2e82c3dd5b18e45be6f115d2a7
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-03-11 17:40:12 +0530

    changes in cron

--- retailer_to_sp/cron.py
+++ retailer_to_sp/cron.py
@@ -2,7 +2,7 @@
 import logging
 
 # logger configuration
-from sp_to_gram.tasks import upload_shop_stock
+from sp_to_gram.tasks import upload_all_products_in_es
 
 info_logger = logging.getLogger('file-info')
 cron_logger = logging.getLogger('cron_log')
@@ -11,8 +11,7 @@ cron_logger = logging.getLogger('cron_log')
 def all_products_es_refresh():
     cron_logger.info('RefreshEs| Started for index named all_products')
     try:
-        shop_id = None
-        upload_shop_stock(shop_id)
+        upload_all_products_in_es()
         cron_logger.info_logger('RefreshEs has been done for index named all_products')
     except Exception as e:
         cron_logger.error('Exception during ES refresh .........', e)
