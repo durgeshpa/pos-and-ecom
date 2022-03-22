61c028a842d8550082a21bd7bb5f8728e6762f67
nikiforniket <nikeetkumar7857@gmail.com>
2022-03-22 17:43:03 +0530

    Added script to reload missing b2b categories

--- categories/tasks.py
+++ categories/tasks.py
@@ -36,3 +36,38 @@ def copy_category_tree_data():
                                        category_data=b2c_category,
                                        category_data_order=positiondata.category_data_order)
     load_b2c_parent_category_data(cat_map)
+
+@task
+def copy_b2c_category_data():
+    cat_map = {}
+    missing_cats = []
+    b2c_categories = B2cCategory.objects.all()
+    print(b2c_categories.count())
+    for b2c_category in b2c_categories:
+        if not Category.objects.filter(category_name=b2c_category.category_name).exists():
+            print("Missing or new Category found :: " , b2c_category.category_name)
+            print("Found category parent :: ", b2c_category.category_parent.category_name if b2c_category.category_parent else None)
+            category = Category.objects.create(
+                category_name=b2c_category.category_name, 
+                category_slug=b2c_category.category_slug,
+                category_desc=b2c_category.category_desc,
+                category_parent=None,
+                category_sku_part=b2c_category.category_sku_part,
+                category_image=b2c_category.category_image,
+                updated_by=b2c_category.updated_by,
+                status=b2c_category.status
+            )
+            missing_cats.append(b2c_category.id)
+        else:
+            category = Category.objects.get(category_name=b2c_category.category_name)
+            print("Found category :: ", category)
+        cat_map[(b2c_category.id, b2c_category.category_parent.id if b2c_category.category_parent else None)] = category
+    for key, value in cat_map.items():
+        cat_parent_id = key[-1]
+        if cat_parent_id and key[0] in missing_cats:
+            print("Mapping Parent for :: category :: ", value)
+            b2c_category = b2c_categories.get(id=cat_parent_id)
+            category_parent = cat_map.get((cat_parent_id, b2c_category.category_parent.id if b2c_category.category_parent else None))
+            print("Parent Found :: ", category_parent)
+            value.category_parent = category_parent
+            value.save()
