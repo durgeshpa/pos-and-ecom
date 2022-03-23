07bbb0f9eaf18a495b1c4b242e875c347818de7b
nikiforniket <nikeetkumar7857@gmail.com>
2022-03-23 17:00:54 +0530

    Fixed script to handle non found b2b type cat in b2c

--- products/scripts/load_b2b_b2c_cat.py
+++ products/scripts/load_b2b_b2c_cat.py
@@ -10,12 +10,15 @@ def run():
                 b2c_categories = product.parent_product_pro_b2c_category.all()
                 if product.parent_product_pro_category.exists():
                     for cat in categories:
-                        b2c_ct = B2cCategory.objects.get(category_name=cat.category.category_name)
-                        ParentProductB2cCategory.objects.create(
-                            parent_product = product,
-                            category = b2c_ct,
-                            status = cat.status
-                        )
+                        if B2cCategory.objects.filter(category_name=cat.category.category_name).exists():
+                            b2c_ct = B2cCategory.objects.get(category_name=cat.category.category_name)
+                            ParentProductB2cCategory.objects.create(
+                                parent_product = product,
+                                category = b2c_ct,
+                                status = cat.status
+                            )
+                        else:
+                            print(cat.category.category_name, cat.category.id)
                 elif product.parent_product_pro_b2c_category.exists():
                     for b2c_cat in b2c_categories:
                         ct = Category.objects.get(category_name=b2c_cat.category.category_name)
