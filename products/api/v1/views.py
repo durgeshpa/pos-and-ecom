d6a76c32bcf717c676ed86860330bfd6a5bea179
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-03-24 20:04:23 +0530

    added b2c parent product edit functionality

--- products/api/v1/views.py
+++ products/api/v1/views.py
@@ -91,7 +91,7 @@ class B2cCategoryListView(GenericAPIView):
             self.queryset = category_search(self.queryset, search_text)
         category = SmallOffsetPagination().paginate_queryset(self.queryset, request)
         serializer = self.serializer_class(category, many=True)
-        msg = "" if category else "no category found"
+        msg = "" if category else "no b2c category found"
         return get_response(msg, serializer.data, True)
 
 
@@ -264,10 +264,11 @@ class ParentProductView(GenericAPIView):
     authentication_classes = (authentication.TokenAuthentication,)
     permission_classes = (AllowAny,)
     queryset = ParentProducts.objects.select_related('parent_brand', 'product_hsn', 'updated_by').prefetch_related(
-        'parent_product_pro_category', 'parent_product_pro_b2c_category', 'parent_product_pro_tax', 'product_parent_product', 'parent_product_log',
-        'product_parent_product__product_pro_image', 'parent_product_pro_category__category',
-        'product_parent_product__product_vendor_mapping', 'parent_product_pro_tax__tax', 'parent_product_pro_image',
-        'parent_product_log__updated_by', 'product_parent_product__product_vendor_mapping__vendor', ) \
+        'parent_product_pro_category', 'parent_product_pro_b2c_category', 'parent_product_pro_tax',
+        'product_parent_product', 'parent_product_log', 'product_parent_product__product_pro_image',
+        'parent_product_pro_category__category', 'product_parent_product__product_vendor_mapping',
+        'parent_product_pro_tax__tax', 'parent_product_pro_image', 'parent_product_log__updated_by',
+        'product_parent_product__product_vendor_mapping__vendor', ) \
         .only('id', 'parent_id', 'name', 'inner_case_size', 'product_type', 'is_ptr_applicable', 'updated_by',
               'ptr_percent', 'ptr_type', 'status', 'parent_brand__brand_name', 'parent_brand__brand_code',
               'updated_at', 'product_hsn__product_hsn_code', 'is_lead_time_applicable', 'is_ars_applicable',
