935c82a3b830bc866d070688036eacf73523cd6a
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-03-31 14:00:29 +0530

    Invoice Upload

--- zoho/admin.py
+++ zoho/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from .models import ZohoFileUpload
-
+from .models import ZohoFileUpload, ZohoInvoice, ZohoInvoiceItem
 
 # Register your models here.
 from .views import bulk_zoho_invoice_file_upload, bulk_zoho_credit_note_file_upload
 
 
 class ZohoFileUploadAdmin(admin.ModelAdmin):
-    list_display = ['file', 'upload_type', 'created_by', 'created_at']
-    list_filter = ['upload_type', 'created_at']
+    list_display = ['file', 'upload_type', 'created_by', 'updated_by', 'created_at', 'updated_at']
+    list_filter = ['upload_type', 'created_at', 'updated_at']
     ordering = ('-created_at',)
 
     def has_add_permission(self, request):
         return False
 
     def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request):
         return False
 
     def get_urls(self):
@@ -35,4 +37,33 @@ class ZohoFileUploadAdmin(admin.ModelAdmin):
         return urls
 
 
+class ZohoInvoiceItemAdmin(admin.TabularInline):
+    model = ZohoInvoiceItem
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    class Media:
+        pass
+
+
+class ZohoInvoiceAdmin(admin.ModelAdmin):
+    list_display = ['invoice_date', 'invoice_id', 'invoice_number', 'invoice_status']
+    inlines = [ZohoInvoiceItemAdmin, ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(ZohoFileUpload, ZohoFileUploadAdmin)
+admin.site.register(ZohoInvoice, ZohoInvoiceAdmin)
+
--- zoho/views.py
+++ zoho/views.py
@@ -1,13 +1,15 @@
 import logging
 
 from django.db import transaction
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from zoho.common_function import ZohoInvoiceCls, error_invoice_csv_file, error_invoice_credit_note_csv_file
+from zoho.common_validators import bulk_invoice_data_validation
 from zoho.forms import ZohoInvoiceFileUploadForm, ZohoCreditNoteFileUploadForm
 # Logger
 from zoho.models import ZohoFileUpload
-from zoho.services import zoho_credit_note_data_upload, zoho_invoice_data_upload
 
 info_logger = logging.getLogger('file-info')
 error_logger = logging.getLogger('file-error')
@@ -18,16 +20,17 @@ cron_logger = logging.getLogger('cron_log')
 def bulk_zoho_invoice_file_upload(request):
     if request.method == 'POST':
         info_logger.info("POST request while bulk zoho invoice file upload.")
-        form = ZohoInvoiceFileUploadForm(request.POST)
+        form = ZohoInvoiceFileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             info_logger.info("Data validation has been successfully done.")
             try:
                 invoice_file = form.cleaned_data['file']
-                ZohoFileUpload.objects.create(file=invoice_file, upload_type='Invoice',
-                                              created_by=request.user, updated_by=request.user)
+                invoice_file_obj = ZohoFileUpload.objects.create(file=invoice_file, upload_type=ZohoFileUpload.INVOICE,
+                                                                 created_by=request.user, updated_by=request.user)
+                response = pos_save_invoice_file(invoice_file_obj)
                 info_logger.info("Invoice File uploaded")
-                with transaction.atomic():
-                    zoho_invoice_data_upload(invoice_file)
+                if isinstance(response, HttpResponse):
+                    return response
 
                 return redirect('/admin/zoho/zohofileupload/')
             except Exception as e:
@@ -47,11 +50,11 @@ def bulk_zoho_credit_note_file_upload(request):
             info_logger.info("Data validation has been successfully done.")
             try:
                 credit_note_file = form.cleaned_data['file']
-                ZohoFileUpload.objects.create(file=credit_note_file, upload_type='Credit Note',
+                ZohoFileUpload.objects.create(file=credit_note_file, upload_type=ZohoFileUpload.CREDIT_NOTE,
                                               created_by=request.user, updated_by=request.user)
                 info_logger.info("Credit Note File uploaded")
                 with transaction.atomic():
-                    zoho_credit_note_data_upload(credit_note_file)
+                    pos_save_credit_note_file(credit_note_file)
                 return redirect('/admin/zoho/zohofileupload/')
 
             except Exception as e:
@@ -61,3 +64,27 @@ def bulk_zoho_credit_note_file_upload(request):
     else:
         form = ZohoCreditNoteFileUploadForm()
     return render(request, 'admin/zoho/bulk-upload-credit-note.html', {'form': form})
+
+
+def pos_save_invoice_file(bulk_invoice_obj):
+    response_file = None
+    if bulk_invoice_obj:
+        if bulk_invoice_obj.file:
+            error_list, validated_rows = bulk_invoice_data_validation(bulk_invoice_obj.file)
+            if validated_rows:
+                ZohoInvoiceCls.create_zoho_invoice(validated_rows, bulk_invoice_obj.created_by)
+            if len(error_list) > 1:
+                response_file = error_invoice_credit_note_csv_file(error_list, 'zoho_invoice_error')
+    return response_file
+
+
+def pos_save_credit_note_file(bulk_invoice_obj):
+    response_file = None
+    if bulk_invoice_obj:
+        if bulk_invoice_obj.file:
+            error_list, validated_rows = bulk_invoice_data_validation(bulk_invoice_obj.file)
+            if validated_rows:
+                ZohoInvoiceCls.create_zoho_invoice(validated_rows, bulk_invoice_obj.created_by)
+            if len(error_list) > 1:
+                response_file = error_invoice_credit_note_csv_file(error_list, 'zoho_credit_note_error')
+    return response_file
