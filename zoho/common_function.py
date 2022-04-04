eb2ca653ea663c27ebd8360f6c9ce6f7be824e12
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-04-04 12:54:21 +0530

    added loggers

--- zoho/common_function.py
+++ zoho/common_function.py
@@ -14,6 +14,7 @@ cron_logger = logging.getLogger('cron_log')
 class ZohoInvoiceCls:
     @classmethod
     def create_zoho_invoice(cls, validated_rows, created_by):
+        info_logger.info(f"ZohoInvoiceCls|create_zoho_invoice|User {created_by}")
         try:
             created_invoice = []
             for row in validated_rows:
@@ -23,25 +24,29 @@ class ZohoInvoiceCls:
                 zoho_invoice_obj, created = ZohoInvoice.objects.update_or_create(
                     invoice_number=invoice_number, defaults=invoice_kwargs)
                 if created:
+                    info_logger.info(f"ZohoInvoiceCls|create_zoho_invoice|created|{invoice_number}|{zoho_invoice_obj}")
                     created_invoice.append(zoho_invoice_obj)
                     zoho_invoice_obj.created_by = created_by
                 else:
+                    info_logger.info(f"ZohoInvoiceCls|create_zoho_invoice|updated|{invoice_number}|{zoho_invoice_obj}")
                     zoho_invoice_obj.updated_by = created_by
                 zoho_invoice_obj.save()
                 # Store Items in db
                 if zoho_invoice_obj in created_invoice:
                     items_kwargs["zoho_invoice"] = zoho_invoice_obj
-                    ZohoInvoiceItem.objects.create(**items_kwargs)
+                    zoho_item_instance = ZohoInvoiceItem.objects.create(**items_kwargs)
+                    info_logger.info(f"ZohoInvoiceCls|create_zoho_invoice|item created|"
+                                     f"{invoice_number}|{zoho_item_instance}")
                     # product_id = items_kwargs.pop('product_id', None)
                     # ZohoInvoiceItem.objects.update_or_create(
                     #     product_id=product_id, zoho_invoice=zoho_invoice_obj, defaults=items_kwargs)
 
         except Exception as e:
-            error_logger.error(e)
-            print(e)
+            error_logger.error(f"ZohoInvoiceCls|create_zoho_invoice|User {created_by}|Error {e}")
 
     @classmethod
     def create_zoho_credit_note(cls, validated_rows, created_by):
+        info_logger.info(f"ZohoInvoiceCls|create_zoho_credit_note|User {created_by}")
         try:
             created_credit_note = []
             for row in validated_rows:
@@ -51,22 +56,27 @@ class ZohoInvoiceCls:
                 credit_note_obj, created = ZohoCreditNote.objects.update_or_create(
                     credit_note_number=credit_note_number, defaults=credit_note_kwargs)
                 if created:
+                    info_logger.info(f"ZohoInvoiceCls|create_zoho_credit_note|created|"
+                                     f"{credit_note_number}|{credit_note_obj}")
                     created_credit_note.append(credit_note_obj)
                     credit_note_obj.created_by = created_by
                 else:
+                    info_logger.info(f"ZohoInvoiceCls|create_zoho_credit_note|updated|"
+                                     f"{credit_note_number}|{credit_note_obj}")
                     credit_note_obj.updated_by = created_by
                 credit_note_obj.save()
                 # Store Items in db
                 if credit_note_obj in created_credit_note:
                     credit_note_items_kwargs["zoho_credit_note"] = credit_note_obj
-                    ZohoCreditNoteItem.objects.create(**credit_note_items_kwargs)
+                    zoho_credit_item_instance = ZohoCreditNoteItem.objects.create(**credit_note_items_kwargs)
+                    info_logger.info(f"ZohoInvoiceCls|create_zoho_credit_note|item created|"
+                                     f"{credit_note_number}|{zoho_credit_item_instance}")
                     # product_id = credit_note_items_kwargs.pop('product_id', None)
                     # ZohoCreditNoteItem.objects.update_or_create(
                     #     product_id=product_id, zoho_credit_note=credit_note_obj, defaults=credit_note_items_kwargs)
 
         except Exception as e:
-            error_logger.error(e)
-            print(e)
+            error_logger.error(f"ZohoInvoiceCls|create_zoho_credit_note|User {created_by}|Error {e}")
 
 
 def error_invoice_credit_note_csv_file(data_list, file_name):
