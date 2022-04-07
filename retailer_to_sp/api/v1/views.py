d10d1916980f86fff99ffd3b684a02ec1830d556
Kamal Agarwal <kamal@gramfactory.com>
2022-04-07 11:47:21 +0530

    Fix EAN code issue while verify shipment product.

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -8725,7 +8725,7 @@ class VerifyReturnShipmentProductsView(generics.GenericAPIView):
             self.queryset = self.queryset.filter(ordered_product__id=shipment_id)
 
         if product_ean_code:
-            self.queryset = self.queryset.filter(product__product_ean_code=product_ean_code)
+            self.queryset = self.queryset.filter(product__product_ean_code__startswith=product_ean_code)
 
         if batch_id:
             self.queryset = self.queryset.filter(rt_ordered_product_mapping__batch_id=batch_id)
