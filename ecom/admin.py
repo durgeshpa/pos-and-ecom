6315bc443f302f98188fd78b98b97c211f71a05a
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-04-25 17:07:21 +0530

    Delivery Time Added

--- ecom/admin.py
+++ ecom/admin.py
@@ -114,7 +114,7 @@ class EcomOrderProductAdmin(admin.ModelAdmin):
 
         (_('Order Details'), {
             'fields': ('id', 'order_no', 'invoice_no', 'order_status', 'order_cancellation_reason', 'buyer',
-                       'buyer_address')}),
+                       'buyer_address', 'delivered_at')}),
 
         (_('Amount Details'), {
             'fields': ('sub_total', 'offer_discount', 'reward_discount', 'order_amount')}),
@@ -151,6 +151,9 @@ class EcomOrderProductAdmin(admin.ModelAdmin):
 
     def order_no(self, obj):
         return obj.order_no
+
+    def delivered_at(self, obj):
+        return obj.delivered_at
 
     def download_invoice(self, obj):
         try:
