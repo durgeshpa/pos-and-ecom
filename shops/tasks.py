8bca75d1ea365b41e6019f3c6b75269e0a8824e0
nikiforniket <nikeetkumar7857@gmail.com>
2022-05-02 11:55:03 +0530

    added remove stale user | replace token and Chnages in device to track source app of device id

--- shops/tasks.py
+++ shops/tasks.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-import requests
 import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import messaging
 from datetime import (date, datetime,
                       timedelta)
@@ -69,9 +67,8 @@ def cancel_beat_plan(*args, **kwargs):
 
 @task
 def create_topics_on_fcm(shops=None):
-    cred = credentials.Certificate()
     try:
-        f_app = firebase_admin.initialize_app(cred)
+        f_app = firebase_admin.initialize_app()
     except Exception as err:
         f_app = firebase_admin.get_app()
     if not shops:
@@ -82,18 +79,97 @@ def create_topics_on_fcm(shops=None):
         devices = list(Device.objects.filter(Q(user__rt_buyer_order__seller_shop=shop) \
             | Q(user__user_location__shop=shop), user__rt_buyer_order__created_at__gte=start_point)\
                 .filter(is_active=True).distinct('reg_id').values_list('reg_id', flat=True))
+        old_devices = shop.fcm_topics.values_list('registration_ids', flat=True)
+        devices = [device for device in devices if device not in old_devices]
         if devices:
             topic_name = f"{shop.shop_name.split(' ')[0]}_{shop.id}"
             response = messaging.subscribe_to_topic(devices, topic_name)
             cron_logger.info(f"{response.success_count} devices subscribed for shop {shop}")
-            print(f"{response.success_count} devices subscribed for shop {shop}")
             cron_logger.info(f"{response.failure_count} devices not subscribed due to error for shop {shop}")
+            print(f"{response.success_count} devices subscribed for shop {shop}")
             print(f"{response.failure_count} devices not subscribed due to error for shop {shop}")
             print([f"{err.index} - {err.reason}" for err in response.errors])
-            # try:
-            #     shop_topic = ShopFcmTopic.objects.get(shop=shop)
-            #     shop_topic.registration_ids.append(devices)
-            #     shop_topic.save()
-            # except ShopFcmTopic.DoesNotExist:
-            #     shop_topic = ShopFcmTopic(shop=shop, topic_name=topic_name, registration_ids=devices)
-            #     shop_topic.save()
+            try:
+                shop_topic = ShopFcmTopic.objects.get(shop=shop)
+                shop_topic.registration_ids.append(devices)
+                shop_topic.save()
+            except ShopFcmTopic.DoesNotExist:
+                shop_topic = ShopFcmTopic(shop=shop, topic_name=topic_name, registration_ids=devices)
+                shop_topic.save()
+    firebase_admin.delete_app(f_app)
+
+
+@task
+def remove_stale_users():
+    try:
+        f_app = firebase_admin.initialize_app()
+    except Exception as err:
+        f_app = firebase_admin.get_app()
+    shops = Shop.objects.filter(status=True)
+    for shop in shops:
+        stale_limit = int(get_config('topic_order_day_limit', 90))
+        dead_point = datetime.today() - timedelta(days=stale_limit)
+        days_limit = int(get_config('topic_order_day_limit', 90))
+        start_point = datetime.today() - timedelta(days=days_limit)
+        old_devices = Device.objects.filter(user__rt_buyer_order__seller_shop=shop,
+                                            user__rt_buyer_order__created_at__lte=dead_point)
+        unsubscribed_devices = list(
+            Device.objects.filter(id__in=Subquery(old_devices.values('id')))\
+                .exclude(user__rt_buyer_order__seller_shop=shop,
+                        user__rt_buyer_order__created_at__gte=start_point)\
+                            .filter(is_active=True).distinct('reg_id').values_list('reg_id', flat=True)
+        )
+        cron_logger.info(f"{len(unsubscribed_devices)} devices unsubscribed for shop {shop}")
+        if unsubscribed_devices:
+            topic_name = f"{shop.shop_name.split(' ')[0].lower()}_{shop.id}"
+            response = messaging.unsubscribe_from_topic(unsubscribed_devices, topic_name)
+            cron_logger.info(f"{response.success_count} devices unsubscribed for shop {shop}")
+            cron_logger.info(f"{response.failure_count} devices not unsubscribed due to error for shop {shop}")
+            print(f"{response.failure_count} devices not unsubscribed due to error for shop {shop}")
+            print([f"{err.index} - {err.reason}" for err in response.errors])
+            print(f"{response.success_count} devices unsubscribed for shop {shop}")
+            try:
+                shop_topic = ShopFcmTopic.objects.get(shop=shop)
+                left_devices = [device for device in shop_topic.registration_ids if device not in unsubscribed_devices]
+                shop_topic.registration_ids = left_devices
+                shop_topic.save()
+            except ShopFcmTopic.DoesNotExist:
+                pass
+    firebase_admin.delete_app(f_app)
+
+
+@task
+def replace_device_token(old, new):
+    topics = ShopFcmTopic.objects.filter(registration_ids__contains=[old])
+    try:
+        f_app = firebase_admin.initialize_app()
+    except Exception as err:
+        f_app = firebase_admin.get_app()
+    for topic in topics:
+        topic_name = topic.topic_name
+        unsub_response = messaging.unsubscribe_from_topic([old], topic_name)
+        topic.registration_ids.pop(old)
+        cron_logger.info(f"{unsub_response.success_count} devices unsubscribed for topic {topic}")
+        cron_logger.info(f"{unsub_response.failure_count} devices not unsubscribed due to error for topic {topic}")
+        sub_response = messaging.subscribe_to_topic([new], topic_name)
+        topic.registration_ids.append(new)
+        cron_logger.info(f"{sub_response.success_count} devices subscribed for topic {topic}")
+        cron_logger.info(f"{sub_response.failure_count} devices not subscribed due to error for topic {topic}")
+    firebase_admin.delete_app(f_app)
+
+
+@task
+def unsubscribe_inactive_token(token):
+    topics = ShopFcmTopic.objects.filter(registration_ids__contains=[token])
+    try:
+        f_app = firebase_admin.initialize_app()
+    except Exception as err:
+        f_app = firebase_admin.get_app()
+    for topic in topics:
+        topic_name = topic.topic_name
+        unsub_response = messaging.unsubscribe_from_topic([token], topic_name)
+        topic.registration_ids.pop(token)
+        topic.save()
+        cron_logger.info(f"{unsub_response.success_count} devices unsubscribed for topic {topic}")
+        cron_logger.info(f"{unsub_response.failure_count} devices not unsubscribed due to error for topic {topic}")
+    firebase_admin.delete_app(f_app)
