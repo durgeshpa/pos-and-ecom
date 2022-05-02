d5c8f393ab88176616585e2ead5ba16b6f935199
nikiforniket <nikeetkumar7857@gmail.com>
2022-05-02 16:06:27 +0530

    removed cancelation option for offline orders from pos app

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -2977,19 +2977,20 @@ class OrderCentral(APIView):
                 cart_products = CartProductMapping.objects.filter(cart=order.ordered_cart)
                 if order.ordered_cart.cart_type == 'BASIC':
                     # Unprocessed orders can be cancelled
-                    if order.order_status != Order.ORDERED:
-                        return api_response('This order cannot be cancelled!')
+                    # if order.order_status != Order.ORDERED:
+                    #     return api_response('This order cannot be cancelled!')
 
-                    # cancel shipment pos order
-                    ordered_product = OrderedProduct.objects.filter(order=order).last()
-                    ordered_product.shipment_status = 'CANCELLED'
-                    ordered_product.last_modified_by = self.request.user
-                    ordered_product.save()
-                    # Update inventory
-                    for cp in cart_products:
-                        PosInventoryCls.order_inventory(cp.retailer_product.id, PosInventoryState.SHIPPED,
-                                                        PosInventoryState.AVAILABLE, cp.qty, self.request.user,
-                                                        order.order_no, PosInventoryChange.CANCELLED)
+                    # # cancel shipment pos order
+                    # ordered_product = OrderedProduct.objects.filter(order=order).last()
+                    # ordered_product.shipment_status = 'CANCELLED'
+                    # ordered_product.last_modified_by = self.request.user
+                    # ordered_product.save()
+                    # # Update inventory
+                    # for cp in cart_products:
+                    #     PosInventoryCls.order_inventory(cp.retailer_product.id, PosInventoryState.SHIPPED,
+                    #                                     PosInventoryState.AVAILABLE, cp.qty, self.request.user,
+                    #                                     order.order_no, PosInventoryChange.CANCELLED)
+                    return api_response('Offline Orders cannot be cancelled from pos app.')
                 else:
                     # delivered orders can not be cancelled
                     if order.order_status == Order.DELIVERED:
