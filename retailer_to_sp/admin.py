0cb395709dfa50d5c7fa3934ca25ec5570eee73f
Kamal Agarwal <kamal@gramfactory.com>
2022-05-12 13:54:57 +0530

    resolve conflicts

--- retailer_to_sp/admin.py
+++ retailer_to_sp/admin.py
@@ -2857,6 +2857,32 @@ class SearchKeywordLogAdmin(admin.ModelAdmin):
         return False
 
 
+
+class PickerUserFilter(AutocompleteFilter):
+    title = 'Picker User'
+    field_name = 'final_user'
+    autocomplete_url = 'all-picker-users-autocomplete'
+
+
+class PickerUserAssignmentLogAdmin(admin.ModelAdmin):
+    list_display = ('picker_dashboard', 'initial_user', 'final_user', 'created_by', 'created_at', )
+    list_filter = [PickerUserFilter, 'created_at']
+    list_per_page = 50
+    ordering = ('-picker_dashboard',)
+    readonly_fields = ('picker_dashboard', 'initial_user', 'final_user', 'created_by', 'created_at', )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    class Media:
+        pass
+
 admin.site.register(Cart, CartAdmin)
 admin.site.register(BulkOrder, BulkOrderAdmin)
 admin.site.register(Order, OrderAdmin)
@@ -2879,3 +2905,4 @@ admin.site.register(ShipmentPackagingMapping, ShipmentPackagingMappingAdmin)
 admin.site.register(EInvoiceData, EInvoiceAdmin)
 admin.site.register(ENoteData, ENoteAdmin)
 admin.site.register(SearchKeywordLog, SearchKeywordLogAdmin)
+admin.site.register(PickerUserAssignmentLog, PickerUserAssignmentLogAdmin)
