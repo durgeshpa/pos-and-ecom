4332bf85c1d9799b679bd4fa87b2aa866e90a221
Shalinee Bhawnani <shalinee@gramfactory.com>
2022-05-23 22:24:36 +0530

    logger for otp issue

--- otp/models.py
+++ otp/models.py
@@ -13,7 +13,6 @@ import logging
 error_logger = logging.getLogger('otp_issue_log_file')
 
 info_logger = logging.getLogger('file-info')
-error_logger = logging.getLogger('file-error')
 debug_logger = logging.getLogger('file-debug')
 
 
@@ -41,12 +40,12 @@ class PhoneOTP(models.Model):
         try:
             otp = cls.generate_otp(length=getattr(settings, 'OTP_LENGTH', 6),
                                    allowed_chars=getattr(settings, 'OTP_CHARS', '0123456789'))
-            info_logger.info(f"create_otp_for_number otp.{otp} for number {number}")
+            info_logger.info(f"create_otp_for_number|otp {otp} for number {number}")
         except Exception as e:
             otp = '895674'
             error_logger.error(e)
         phone_otp = PhoneOTP.objects.create(phone_number=number, otp=otp)
-        info_logger.info(f"phone_otp obj {phone_otp} with number & otp.{number} - {otp}")
+        info_logger.info(f"create_otp_for_number|{number} - {otp}|phone_otp {phone_otp}")
         return phone_otp, otp
 
     @classmethod
@@ -63,7 +62,7 @@ class PhoneOTP(models.Model):
         user.attempts = 0
         user.created_at = timezone.now()
         user.save()
-        info_logger.error(f"user & otp {user} - {otp}")
+        info_logger.info(f"user {user} & otp {otp}")
         return user, otp
 
     @classmethod
