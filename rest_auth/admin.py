0565a3f670b8841cf8199ac53740efcb78e7a5a5
Sanket Kamta <sanket@gramfactory.com>
2022-05-25 13:36:53 +0530

    Token filter by user

--- rest_auth/admin.py
+++ rest_auth/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import *
+from admin_auto_filters.filters import AutocompleteFilter
+from django.db.models import Q
+
+
+class TokenFilter(AutocompleteFilter):
+    title = 'User'  # display title
+    field_name = 'user'  # name of the foreign key field
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            return queryset.filter(
+                Q(user_id=self.value())
+            )
 
 
 class TokenAdmin(admin.ModelAdmin):
@@ -8,6 +21,7 @@ class TokenAdmin(admin.ModelAdmin):
     ]
     fields = ['key', 'user']
     readonly_fields = ['key']
+    list_filter = [TokenFilter, ]
 
 # Register your models here.
 admin.site.register(Token, TokenAdmin)
