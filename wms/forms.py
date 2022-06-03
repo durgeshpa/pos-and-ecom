73bf4ecd1da507159527c87efa18be015bd6dc23
Deepti <deepti@gramfactory.com>
2022-06-03 13:19:25 +0530

    2epx6dh:QCArea Assignment Issue Fix

--- wms/forms.py
+++ wms/forms.py
@@ -12,10 +12,11 @@ from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth.models import Permission, Group
 from tempus_dominus.widgets import DateTimePicker
 
+import retailer_to_sp
 from accounts.models import User
 from .models import Bin, In, Putaway, PutawayBinInventory, BinInventory, Out, Pickup, StockMovementCSVUpload, \
     InventoryType, InventoryState, BIN_TYPE_CHOICES, Audit, Zone, WarehouseAssortment, QCArea, Crate, \
-    ZonePickerUserAssignmentMapping, QCDeskQCAreaAssignmentMapping, QCDesk
+    ZonePickerUserAssignmentMapping, QCDeskQCAreaAssignmentMapping, QCDesk, GROCERY, SUPERSTORE
 from products.models import Product, ProductPrice, ParentProduct
 from shops.models import Shop
 from gram_to_brand.models import GRNOrderProductMapping
@@ -24,7 +25,8 @@ from django.core.exceptions import ValidationError
 from django.db.models import Sum, Q
 from .common_functions import create_batch_id
 from global_config.views import get_config
-from retailer_to_sp.models import OrderedProduct, PickerDashboard
+from retailer_to_sp.models import OrderedProduct, PickerDashboard, Order, Cart, SUPERSTORE, CART_TYPES, \
+    GROCERY_CART_TYPES, SUPERSTORE_CART_TYPES
 from django.db import transaction
 from .common_functions import cancel_ordered, cancel_shipment, cancel_returned, putaway_repackaging
 from dal import autocomplete
@@ -1786,6 +1788,16 @@ class QCDeskForm(forms.ModelForm):
             for qc_area in self.cleaned_data['qc_areas']:
                 if qc_area.warehouse_id != int(self.data['warehouse']):
                     raise ValidationError(_("Invalid QC Area " + str(qc_area) + " selected."))
+            new_added_qcarea = self.cleaned_data['qc_areas'].difference(self.instance.qc_areas.all())
+            cart_type = GROCERY_CART_TYPES if self.instance.desk_type != SUPERSTORE else SUPERSTORE_CART_TYPES
+            for new_area in new_added_qcarea:
+                if new_area.qc_area_shipment.filter(~Q(order__ordered_cart__cart_type__in=cart_type,
+                                                       order__order_status__in=[Order.PARTIAL_MOVED_TO_QC, Order.MOVED_TO_QC,
+                                                                         Order.PICKING_COMPLETE,
+                                                                         Order.PICKING_PARTIAL_COMPLETE])).exists():
+                    raise ValidationError(_("QC Area " + str(qc_area) + " has non " + self.instance.desk_type + " Orders."
+                                            "This QC Area can not be assigned to this QC Desk yet."))
+
         return self.cleaned_data['qc_areas']
 
     def clean_alternate_desk(self):
