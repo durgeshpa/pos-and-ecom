bae06cc500525786452257d7afc9c986bda1e12c
Sanket Kamta <sanket@gramfactory.com>
2022-06-22 19:19:39 +0530

    sa_download_debug

--- common/common_utils.py
+++ common/common_utils.py
@@ -68,6 +68,7 @@ def merge_pdf_files(file_path_list, merge_pdf_name):
     try:
         info_logger.info("API2PDF_KEY :: {}".format(config('API2PDF_KEY')))
         a2p_client = Api2Pdf(config('API2PDF_KEY'))
+        info_logger.info("a2p_client.api_key :: {}".format(a2p_client.api_key))
         merge_result = a2p_client.merge(file_path_list, file_name=merge_pdf_name)
         info_logger.info("Merge result :: {}".format(merge_result.result))
         return merge_result.result['pdf']
