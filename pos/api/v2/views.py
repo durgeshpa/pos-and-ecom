e388faa28cfd4cb6a7d6981d9633813769def44a
durgesh patel <durgesh@gramfactory.com>
2022-06-29 20:03:52 +0530

    stage bug .

--- pos/api/v2/views.py
+++ pos/api/v2/views.py
@@ -572,6 +572,8 @@ class AdminOffers(GenericAPIView):
                                            expiry_date)
         coupon = OffersCls.rule_set_cart_mapping(coupon_obj.id, 'catalog', combo_offer_name, combo_code, shop,
                                                  start_date, expiry_date, data.get('limit_of_usages_per_customer',None))
+        if data.get('coupon_type_name') == 'superstore':
+                data.update['coupon_enable_on'] = 'superstore'
         coupon.coupon_enable_on = data.get('coupon_enable_on') if data.get('coupon_enable_on') else 'all'
         coupon.coupon_shop_type = data.get('coupon_shop_type') if data.get(
             'coupon_shop_type') else coupon.coupon_shop_type
@@ -620,6 +622,8 @@ class AdminOffers(GenericAPIView):
             return api_response(coupon_obj)
         coupon = OffersCls.rule_set_cart_mapping(coupon_obj.id, 'cart', coupon_name, coupon_code, shop, start_date,
                                                  expiry_date, data.get('limit_of_usages_per_customer',None))
+        if data.get('coupon_type_name') == 'superstore':
+                data.update['coupon_enable_on'] = 'superstore'
         coupon.coupon_enable_on = data.get('coupon_enable_on') if data.get('coupon_enable_on') else 'all'
         coupon.coupon_shop_type = data.get('coupon_shop_type') if data.get(
             'coupon_shop_type') else coupon.coupon_shop_type
