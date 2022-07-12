31aeb009b890c0b5b778ca7bb84fd745736703c7
Sanket Kamta <sanket@gramfactory.com>
2022-07-12 19:03:47 +0530

    Email recipient global variable change

--- retailer_to_sp/api/v1/views.py
+++ retailer_to_sp/api/v1/views.py
@@ -6474,7 +6474,8 @@ def send_notification_ecom_user(order, message_title, message_body):
 
 def sendemailforsuperstoreorder(order):
     subject = 'A new SuperStore order has been placed.'
-    url = 'https://{}.gramfactory.com/admin/retailer_to_sp/order/{}'.format(config('ENVIRONMENT_URL'),order.id)
+    platform = 'seller' if config('ENVIRONMENT') == 'production' else config('ENVIRONMENT')
+    url = 'https://{}.gramfactory.com/admin/retailer_to_sp/order/{}'.format(platform, order.id)
     body = 'Click here for more details - {}'.format(url)
     sender = GlobalConfig.objects.get(key='sender')
     receiver = GlobalConfig.objects.get(key='superstore_order_internal_email_recipient')
